a11bbd30b802c5f9e84350eb1424d50d738256a1
Ashoka Finley <ashoka.finley@example.com>
2020-05-27 13:40:35 -0400

    move layout file to layouts

---
+++ components/layouts/PrimaryLayout.tsx
@@ -0,0 +1,19 @@
+import { Box, Container } from 'theme-ui';
+
+import Header from './Header';
+
+type props = {};
+
+const PrimaryLayout: React.FC<React.PropsWithChildren<props>> = ({
+  children
+}) => {
+  return (
+    <Container mx="auto" px={4}>
+      <Header />
+      <Box as="main">{children}</Box>
+      {/* <Footer /> */}
+    </Container>
+  );
+};
+
+export default PrimaryLayout;
