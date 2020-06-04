b89cc3a1310da91efc0039ed25e0e4c3ca5f239e
Josh Levine <joshuapark95@gmail.com>
2020-06-04 00:42:18 -0400

    add basic date range filter for polls

--- pages/polling.tsx
+++ pages/polling.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useMemo } from 'react';
-import { NavLink, Heading, Checkbox, Label, Box } from 'theme-ui';
+import { NavLink, Heading, Checkbox, Label, Box, Flex, Input } from 'theme-ui';
 import Link from 'next/link';
 
 import { getNetwork, isDefaultNetwork } from '../lib/maker';
@@ -9,8 +9,8 @@ import Poll from '../types/poll';
 
 function isActivePoll(poll) {
   const hasStarted = new Date(poll.startDate).getTime() <= Date.now();
-  const hasntEnded = new Date(poll.endDate).getTime() >= Date.now();
-  return hasStarted && hasntEnded;
+  const hasNotEnded = new Date(poll.endDate).getTime() >= Date.now();
+  return hasStarted && hasNotEnded;
 }
 
 interface Props {
@@ -18,7 +18,8 @@ interface Props {
 }
 
 const PollingOverview = ({ polls }: Props) => {
-  const [filterInactive, setFilterInactive] = useState(true);
+  const [dateFilter, setDateFilter] = useState<(Date | null)[]>([null, null]);
+  const [filterInactive, setFilterInactive] = useState(false);
   const network = getNetwork();
 
   const pollsToShow = useMemo(
@@ -28,18 +29,50 @@ const PollingOverview = ({ polls }: Props) => {
         if (filterInactive) {
           _show = _show && isActivePoll(poll);
         }
+
+        const [filterPollsBeforeTime, filterPollsAfterTime] = dateFilter;
+        if (filterPollsBeforeTime) {
+          _show = _show && new Date(poll.startDate).getTime() >= filterPollsBeforeTime.getTime();
+        }
+        if (filterPollsAfterTime) {
+          _show = _show && new Date(poll.startDate).getTime() <= filterPollsAfterTime.getTime();
+        }
+
         return _show;
       }),
-    [polls, filterInactive]
+    [polls, filterInactive, dateFilter]
   );
 
   return (
     <PrimaryLayout>
       <Heading as="h1">Polling Votes</Heading>
-      <Label>
-        <Checkbox checked={filterInactive} onChange={() => setFilterInactive(b => !b)} />
-        Show only active polls
-      </Label>
+      <Flex sx={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+        <Box>
+          <Label>
+            <Checkbox checked={filterInactive} onChange={() => setFilterInactive(b => !b)} />
+            Show only active polls
+          </Label>
+        </Box>
+
+        <Flex>
+          <Label sx={{ whiteSpace: 'nowrap', alignItems: 'center' }}>
+            Show only polls after
+            <Input
+              ml="3"
+              type="date"
+              onChange={e => setDateFilter([new Date(e.target.value), dateFilter[1]])}
+            />
+          </Label>
+          <Label ml="3" sx={{ whiteSpace: 'nowrap', alignItems: 'center' }}>
+            Show only polls before
+            <Input
+              ml="3"
+              type="date"
+              onChange={e => setDateFilter([dateFilter[0], new Date(e.target.value)])}
+            />
+          </Label>
+        </Flex>
+      </Flex>
 
       {pollsToShow.map(poll => (
         <Box key={poll.multiHash}>
