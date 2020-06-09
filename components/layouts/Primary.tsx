77c97f3ad100521a2b60b7fb1752867bd344c0ff
Ethan <ebennett19@gmail.com>
2020-06-09 01:36:55 -0400

    Remove mobile branch from primary layout

--- components/layouts/Primary.tsx
+++ components/layouts/Primary.tsx
@@ -1,4 +1,4 @@
-import { Container, Box } from 'theme-ui';
+import { Container } from 'theme-ui';
 
 import Header from '../Header';
 import Footer from '../Footer';
@@ -9,25 +9,11 @@ type Props = {
 
 const PrimaryLayout = ({ children, shortenFooter }: React.PropsWithChildren<Props>) => {
   return (
-    <>
-      {/* Desktop */}
-      <Box sx={{ display: ['none', 'block'] }}>
-        <Container mx="auto" px={4} sx={{ px: [0, 4], background: 'url(/assets/heroVisualTransparent.png) no-repeat', backgroundSize: '100%', backgroundPosition: '0 0' }}>
-          <Header />
-          <main>{children}</main>
-          <Footer shorten={shortenFooter || false} />
-        </Container>
-      </Box>
-
-      {/* Mobile */}
-      <Box sx={{ display: ['block', 'none'] }}>
-        <Container mx="auto" sx={{ background: 'url(/assets/heroVisualTransparent.png) no-repeat', backgroundSize: '100%', backgroundPosition: '0 0' }}>
-          <Header />
-          <main>{children}</main>
-          <Footer shorten={shortenFooter || false} />
-        </Container>
-      </Box>
-    </>
+    <Container mx="auto" px={4} sx={{ px: [0, 4], background: 'url(/assets/heroVisualTransparent.png) no-repeat', backgroundSize: '100%', backgroundPosition: '0 0' }}>
+      <Header />
+      <main>{children}</main>
+      <Footer shorten={shortenFooter || false} />
+    </Container>
   );
 };
 
