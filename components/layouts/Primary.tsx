d9742f40b79485211988492c4b48e4006dc684ea
Ethan Bennett <ebennett19@gmail.com>
2020-06-09 01:27:31 -0400

    Update components/layouts/Primary.tsx
    Co-authored-by: Josh Levine <24902242+jparklev@users.noreply.github.com>

--- components/layouts/Primary.tsx
+++ components/layouts/Primary.tsx
@@ -12,7 +12,7 @@ const PrimaryLayout = ({ children, shortenFooter }: React.PropsWithChildren<Prop
     <>
       {/* Desktop */}
       <Box sx={{ display: ['none', 'block'] }}>
-        <Container mx="auto" px={4} sx={{ background: 'url(/assets/heroVisualTransparent.png) no-repeat', backgroundSize: '100%', backgroundPosition: '0 0' }}>
+        <Container mx="auto" px={4} sx={{ px: [0, 4], background: 'url(/assets/heroVisualTransparent.png) no-repeat', backgroundSize: '100%', backgroundPosition: '0 0' }}>
           <Header />
           <main>{children}</main>
           <Footer shorten={shortenFooter || false} />
