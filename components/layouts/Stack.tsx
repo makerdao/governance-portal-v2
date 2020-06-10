48363c6606669140deba2b45ae466322d8f260c3
Lawrence Wang <l@lw.io>
2020-06-10 15:35:43 -0700

    parameterize stack gap

--- components/layouts/Stack.tsx
+++ components/layouts/Stack.tsx
@@ -3,15 +3,20 @@ import { jsx } from 'theme-ui';
 import { Flex } from 'theme-ui';
 import theme from '../../lib/theme';
 
-type Props = {};
+type Props = {
+  gap: number;
+};
 
-const StackLayout = ({ children }: React.PropsWithChildren<Props>) => {
+/**
+ * Usage note: children should not specify their own margins
+ */
+const StackLayout = ({ children, gap = 4 }: React.PropsWithChildren<Props>) => {
   return (
     <Flex
       sx={{ width: '100%', flexDirection: 'column', alignItems: 'stretch' }}
       css={`
         > * + * {
-          margin-top: ${theme.sizes[4]}px;
+          margin-top: ${theme.sizes[gap]}px;
         }
       `}
     >
