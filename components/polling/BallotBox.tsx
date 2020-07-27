cefe0b01da8f7db2c0516e611de263bc69507a73
Ashoka Finley <ashoka.finley@example.com>
2020-07-27 12:16:28 -0700

    update the question mark

--- components/polling/BallotBox.tsx
+++ components/polling/BallotBox.tsx
@@ -4,18 +4,21 @@ import { Card, Heading, Box, Flex, Button, Text } from 'theme-ui';
 import { Icon } from '@makerdao/dai-ui-icons';
 
 export default function ({ ...props }) {
-  const ballot = useBallotStore(state => state.ballot);
-  const submitBallot = useBallotStore(state => state.submitBallot)
-
+  const ballot = props.ballot
+  const submitBallot = props.submitBallot
+  const activePolls = props.activePolls
+  const inReview = props.inReview
+  const setInReview = props.setInReview
   const ballotLength = () => {
     return Object.keys(ballot).length
   }
   const [votingWeightTotal, setVotingWeighTotal] = useState(0)
-  const activePolls = props.activePolls
+
+
   return (
     <Box>
       <Heading mb={3} as='h4'>
-        Your Ballot
+        {inReview ? 'Submit Ballot' : 'Your Ballot'}
       </Heading>
       <Card variant="compact" p={0}>
         <Box p={3} sx={{ borderBottom: '1px solid #D4D9E1'}}>
@@ -55,10 +58,12 @@ export default function ({ ...props }) {
           </Flex>
         </Box>
         <Flex p={3}  sx={{ borderBottom: '1px solid #D4D9E1', justifyContent: 'space-between', flexDirection: 'row'}}>
-          <Text color="onSurface">
-            Voting weight for all polls
-            <Icon name='question' mt={1} ml={1} />
-          </Text>
+          <Flex sx={{ flexDirection: 'row' }}>
+            <Text color='onSurface'>
+              Voting weight for all polls
+            </Text>
+            <Icon name='question' ml={1} mt={1} sx={{ paddingTop: '3px' }} />
+          </Flex>
           <Text>
             {`${votingWeightTotal.toFixed(2)} MKR`}
           </Text>
@@ -80,8 +85,8 @@ export default function ({ ...props }) {
               {`Confirm Time`}
             </Text>
           </Flex>
-          <Button onClick={submitBallot} disabled={ballotLength() < 1} variant='primary' sx={{width: '100%'}}>
-            Review & Submit Your Ballot
+          <Button onClick={inReview ? submitBallot : () => setInReview(true)}  variant='primary' sx={{width: '100%'}}>
+            {inReview ? `Submit Your Ballot (${ballotLength()} Votes)` : 'Review & Submit Your Ballot'}
           </Button>
         </Flex>
       </Card>
