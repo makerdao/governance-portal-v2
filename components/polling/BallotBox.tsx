483adfaf4213edd6773ef4f9f960780f1c176d2f
Lawrence Wang <l@lw.io>
2020-07-27 14:16:20 -0700

    fix formatting, remove duplicate in BallotBox.jsx

--- components/polling/BallotBox.tsx
+++ components/polling/BallotBox.tsx
@@ -3,21 +3,20 @@ import useBallotStore from '../../stores/ballot';
 import { Card, Heading, Box, Flex, Button, Text } from 'theme-ui';
 import { Icon } from '@makerdao/dai-ui-icons';
 
-export default function ({ ...props }) {
-  const ballot = props.ballot
-  const submitBallot = props.submitBallot
-  const activePolls = props.activePolls
-  const inReview = props.inReview
-  const setInReview = props.setInReview
+export default function({ ...props }) {
+  const ballot = props.ballot;
+  const submitBallot = props.submitBallot;
+  const activePolls = props.activePolls;
+  const inReview = props.inReview;
+  const setInReview = props.setInReview;
   const ballotLength = () => {
-    return Object.keys(ballot).length
-  }
-  const [votingWeightTotal, setVotingWeighTotal] = useState(0)
-
+    return Object.keys(ballot).length;
+  };
+  const [votingWeightTotal, setVotingWeighTotal] = useState(0);
 
   return (
     <Box>
-      <Heading mb={3} as='h4'>
+      <Heading mb={3} as="h4">
         {inReview ? 'Submit Ballot' : 'Your Ballot'}
       </Heading>
       <Card variant="compact" p={[0, 0]}>
@@ -52,7 +51,8 @@ export default function ({ ...props }) {
             ))}
           </Flex>
         </Box>
-        <Flex p={3} 
+        <Flex
+          p={3}
           sx={{
             borderBottom: '1px solid #D4D9E1',
             justifyContent: 'space-between',
@@ -60,42 +60,26 @@ export default function ({ ...props }) {
           }}
         >
           <Flex sx={{ flexDirection: 'row' }}>
-            <Text color='onSurface'>
-              Voting weight for all polls
-            </Text>
-            <Icon name='question' ml={1} mt={1} sx={{ paddingTop: '3px' }} />
+            <Text color="onSurface">Voting weight for all polls</Text>
+            <Icon name="question" ml={1} mt={1} sx={{ paddingTop: '3px' }} />
           </Flex>
-          <Text>
-            {`${votingWeightTotal.toFixed(2)} MKR`}
-          </Text>
           <Text>{`${votingWeightTotal.toFixed(2)} MKR`}</Text>
         </Flex>
-        <Flex p={3} sx={{ flexDirection: 'column'}}>
-          <Flex pb={3} sx={{ justifyContent: 'space-between', flexDirection: 'row'}}>
-            <Text color="onSurface">
-              Estimated Gas Cost
-            </Text>
-            <Text>
-              {`Gas Cost`}
-            </Text>
+        <Flex p={3} sx={{ flexDirection: 'column' }}>
+          <Flex pb={3} sx={{ justifyContent: 'space-between', flexDirection: 'row' }}>
+            <Text color="onSurface">Estimated Gas Cost</Text>
+            <Text>{`Gas Cost`}</Text>
           </Flex>
-          <Flex pb={4} sx={{ justifyContent: 'space-between', flexDirection: 'row'}}>
-            <Text color="onSurface">
-              Estimated Confirmation Time
-            </Text>
-            <Text>
-              {`Confirm Time`}
-            </Text>
+          <Flex pb={4} sx={{ justifyContent: 'space-between', flexDirection: 'row' }}>
+            <Text color="onSurface">Estimated Confirmation Time</Text>
+            <Text>{`Confirm Time`}</Text>
           </Flex>
           <Button
             onClick={inReview ? submitBallot : () => setInReview(true)}
-            variant='primary'
-            sx={{width: '100%'}}
+            variant="primary"
+            sx={{ width: '100%' }}
           >
-            {inReview 
-              ? `Submit Your Ballot (${ballotLength()} Votes)`
-              : 'Review & Submit Your Ballot'
-            }
+            {inReview ? `Submit Your Ballot (${ballotLength()} Votes)` : 'Review & Submit Your Ballot'}
           </Button>
         </Flex>
       </Card>
