910dd6ef7e348fdb766e08184f7a3f01a73eef17
Lawrence Wang <l@lw.io>
2020-07-28 19:14:49 -0700

    handle Abstain in ballot store
    the only significant change here is using isNil instead of truthiness checking, but the whole file changed due to formatting

--- stores/ballot.ts
+++ stores/ballot.ts
@@ -1,30 +1,39 @@
 import create from 'zustand';
 import { devtools } from 'zustand/middleware';
+import isNil from 'lodash/isNil';
+import omit from 'lodash/omit';
 
-const [useBallotStore] = create(devtools((set, get) => ({
-  ballot: {},
-  addToBallot: (pollId, option: number | number[]) => {
-    set(state => ({ ballot: { ...state.ballot, [pollId]: { ...state.ballot[pollId], option } } }), 'addToBallot');
-  },
-  removeFromBallot: (pollId, option) => {
-    set(state => {
-      const { pollId, ...updatedBallot } = state.ballot;
-      return updatedBallot;
-    }, 'removeFromBallot');
-  },
-  clearBallot: () => {
-    set({ ballot: {} }, 'clearBallot');
-  },
-  submitBallot: () => {
-    set(state => {
-      let newBallotObj = {};
-      Object.keys(state.ballot).forEach(key => {
-        if (state.ballot[key].option)
-          newBallotObj[key] = { ...state.ballot[key], submitted: state.ballot[key].option };
-      });
-      return { ballot: newBallotObj };
-    }, 'submitBallot');
-  }
-}), 'BallotStore'));
+const [useBallotStore] = create(
+  devtools(
+    (set, get) => ({
+      ballot: {},
+      addToBallot: (pollId, option: number | number[]) => {
+        set(
+          state => ({ ballot: { ...state.ballot, [pollId]: { ...state.ballot[pollId], option } } }),
+          'addToBallot'
+        );
+      },
+      removeFromBallot: pollId => {
+        set(state => {
+          return omit(state.ballot, pollId);
+        }, 'removeFromBallot');
+      },
+      clearBallot: () => {
+        set({ ballot: {} }, 'clearBallot');
+      },
+      submitBallot: () => {
+        set(state => {
+          const newBallot = {};
+          Object.keys(state.ballot).forEach(key => {
+            if (!isNil(state.ballot[key].option))
+              newBallot[key] = { ...state.ballot[key], submitted: state.ballot[key].option };
+          });
+          return { ballot: newBallot };
+        }, 'submitBallot');
+      }
+    }),
+    'BallotStore'
+  )
+);
 
 export default useBallotStore;
