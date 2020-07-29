22301130aa0befb49cb3abc98c94d5ae9dfdae96
Lawrence Wang <l@lw.io>
2020-07-29 16:39:16 -0700

    fix jest coverage calculations

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,11 @@ module.exports = {
   collectCoverageFrom: [
     '**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
-    '!**/node_modules/**'
+    '!**/{node_modules,coverage}/**',
   ],
+  coverageReporters: ['json', 'lcov', 'text-summary'],
   setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/', '/setup'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
-  },
   transformIgnorePatterns: ['/node_modules/'],
   moduleDirectories: ['node_modules', 'test/helpers'],
   globals: {
