26dfe4e20dd1bf4eefda3c0c16b1cf70e45739b4
Lawrence Wang <l@lw.io>
2020-08-05 16:37:27 -0700

    style fixes on ballot review page

--- pages/polling/review.tsx
+++ pages/polling/review.tsx
@@ -30,6 +30,22 @@ const PollingReview = ({ polls }: { polls: Poll[] }) => {
   const account = useAccountsStore(state => state.currentAccount);
   const ballotLength = Object.keys(ballot).length;
   const activePolls = polls.filter(poll => isActivePoll(poll));
+
+  const SubmitButton = props => (
+    <Flex sx={{ flexDirection: 'column', width: '100%' }} {...props}>
+      <Flex sx={{ flexDirection: 'column' }}>
+        <Button
+          onClick={submitBallot}
+          variant="primary"
+          disabled={!ballotLength || !!txId}
+          sx={{ width: '100%' }}
+        >
+          Submit Your Ballot ({ballotLength} vote{ballotLength === 1 ? '' : 's'})
+        </Button>
+      </Flex>
+    </Flex>
+  );
+
   return (
     <PrimaryLayout shortenFooter={true}>
       <Stack gap={3}>
@@ -45,26 +61,37 @@ const PollingReview = ({ polls }: { polls: Poll[] }) => {
                     Back To All Polls
                   </Button>
                 </Link>
-                {bpi < 2 && (
-                  <Flex sx={{ flexDirection: 'column', width: '100%' }}>
-                    <Flex sx={{ flexDirection: 'column' }}>
-                      <Button
-                        onClick={submitBallot}
-                        variant="primary"
-                        disabled={!ballotLength || !!txId}
-                        sx={{ width: '100%' }}
-                      >
-                        Submit Your Ballot ({ballotLength}) Votes
-                      </Button>
-                    </Flex>
-                  </Flex>
-                )}
+                {bpi < 2 && <SubmitButton sx={{ mb: 3 }} />}
                 <Stack sx={{ mb: 4, display: activePolls.length ? null : 'none' }}>
-                  {Object.keys(ballot).map(pollId => {
+                  {Object.keys(ballot).map((pollId, index) => {
                     const poll = findPollById(polls, pollId);
-                    invariant(poll !== undefined, 'Unkown poll found on voter ballot');
+                    invariant(poll !== undefined, 'Unknown poll found on voter ballot');
                     return (
-                      <PollOverviewCard key={poll.multiHash} poll={poll} reviewing={true} sending={txId} />
+                      <PollOverviewCard
+                        key={poll.multiHash}
+                        poll={poll}
+                        reviewing={true}
+                        sending={txId}
+                        sx={
+                          index === 0
+                            ? {
+                                borderBottomLeftRadius: '0 !important',
+                                borderBottomRightRadius: '0 !important',
+                                borderBottom: '0 !important'
+                              }
+                            : index === ballotLength - 1
+                            ? {
+                                borderTopLeftRadius: '0 !important',
+                                borderTopRightRadius: '0 !important',
+                                mt: '0 !important'
+                              }
+                            : {
+                                borderRadius: '0 !important',
+                                borderBottom: '0 !important',
+                                mt: '0 !important'
+                              }
+                        }
+                      />
                     );
                   })}
                 </Stack>
@@ -72,20 +99,7 @@ const PollingReview = ({ polls }: { polls: Poll[] }) => {
             </Stack>
           </Box>
           <Stack gap={3}>{!!account && <ReviewBox activePolls={activePolls} />}</Stack>
-          {bpi < 2 && (
-            <Flex sx={{ flexDirection: 'column', width: '100%' }}>
-              <Flex sx={{ flexDirection: 'column' }}>
-                <Button
-                  onClick={submitBallot}
-                  variant="primary"
-                  disabled={!ballotLength || !!txId}
-                  sx={{ width: '100%' }}
-                >
-                  Submit Your Ballot ({ballotLength}) Votes
-                </Button>
-              </Flex>
-            </Flex>
-          )}
+          {bpi < 2 && <SubmitButton />}
         </SidebarLayout>
       </Stack>
     </PrimaryLayout>
