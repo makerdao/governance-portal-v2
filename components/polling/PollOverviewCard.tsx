91c468e1691241907a67cb39bbfb7d3f5185cae2
Lawrence Wang <l@lw.io>
2020-08-05 16:56:59 -0700

    refactor ChoiceSummary
    deduplicate container and button so that new styles apply to both vote types

--- components/polling/PollOverviewCard.tsx
+++ components/polling/PollOverviewCard.tsx
@@ -161,42 +161,32 @@ const QuickVote = ({ poll, sending }: { poll: Poll; sending: null | string }) =>
 };
 
 const ChoiceSummary = ({ choice: { option }, poll, edit, sending, ...props }) => {
-  if (typeof option === 'number') {
-    return (
-      <Box {...props}>
+  const isSingleSelect = typeof option === 'number';
+  return (
+    <Box {...props}>
+      {isSingleSelect ? (
         <Box bg="background" sx={{ p: 3, mb: 2 }}>
           <Text>{option === ABSTAIN ? 'Abstain' : poll.options[option]}</Text>
         </Box>
-        <Button
-          onClick={edit}
-          variant="smallOutline"
-          sx={{ display: sending ? 'none' : 'inline-flex', flexDirection: 'row', alignItems: 'center' }}
-        >
-          <Icon name="edit" size={3} mr={1} />
-          Edit choice
-        </Button>
-      </Box>
-    );
-  }
-  return (
-    <Box {...props}>
-      {option.map((id, index) => (
-        <Flex sx={{ backgroundColor: 'background', py: 2, px: 3, mb: 2 }} key={index}>
-          <Flex sx={{ flexDirection: 'column' }}>
-            <Text sx={{ textTransform: 'uppercase', fontSize: 1, fontWeight: 'bold' }}>
-              {getNumberWithOrdinal(index + 1)} choice
-            </Text>
-            <Text>{poll.options[id]}</Text>
+      ) : (
+        option.map((id, index) => (
+          <Flex sx={{ backgroundColor: 'background', py: 2, px: 3, mb: 2 }} key={index}>
+            <Flex sx={{ flexDirection: 'column' }}>
+              <Text sx={{ textTransform: 'uppercase', fontSize: 1, fontWeight: 'bold' }}>
+                {getNumberWithOrdinal(index + 1)} choice
+              </Text>
+              <Text>{poll.options[id]}</Text>
+            </Flex>
           </Flex>
-        </Flex>
-      ))}
+        ))
+      )}
       <Button
         onClick={edit}
         variant="smallOutline"
-        sx={{ display: 'inline-flex', flexDirection: 'row', alignItems: 'center', mt: 3 }}
+        sx={{ display: sending ? 'none' : 'inline-flex', flexDirection: 'row', alignItems: 'center' }}
       >
         <Icon name="edit" size={3} mr={1} />
-        Edit choices
+        Edit choice{isSingleSelect ? '' : 's'}
       </Button>
     </Box>
   );
