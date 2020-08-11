03c5893f121dc8ea1d1b8788c297c8af4302cc72
Josh Levine <joshuapark95@gmail.com>
2020-08-11 13:11:21 -0400

    make pages/index test a basic render check

--- __tests__/pages/index.js
+++ __tests__/pages/index.js
@@ -5,14 +5,10 @@ import { ThemeProvider } from 'theme-ui';
 import theme from '../../lib/theme';
 import Index from '../../pages/index';
 
-test('renders header', () => {
+test('renders', async () => {
   const { getByText } = render(
     <ThemeProvider theme={theme}>
       <Index />
     </ThemeProvider>
   );
-  const heading = getByText(/Executive/);
-  const polling = getByText(/Polling/);
-  expect(heading).toBeInTheDocument();
-  expect(polling).toBeInTheDocument();
 });
