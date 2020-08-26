782cb90119d433832adb4d1bb905ee2de9adb1f1
Lawrence Wang <l@lw.io>
2020-08-25 18:51:24 -0700

    partial implementation of ranked choice tooltip
    it shows up in the upper left corner of the screen for me...

--- components/polling/QuickVote.tsx
+++ components/polling/QuickVote.tsx
@@ -24,7 +24,18 @@ type Props = {
   account?: Account;
 };
 
-const QuickVote = ({ poll, showHeader, account, ...props }: Props) => {
+const rankedChoiceBlurb = (
+  <>
+    Voters can rank options in order of preference. <br />
+    If no option receives more than 50% of the vote <br />
+    based on first choices, the option with the fewest <br />
+    votes is eliminated and its votes are redistributed <br />
+    to their voters&apos; second choices until one option <br />
+    achieves a majority.
+  </>
+);
+
+const QuickVote = ({ poll, showHeader, account, ...props }: Props): JSX.Element => {
   const { data: allUserVotes } = useSWR<PollVote[]>(
     account?.address ? ['/user/voting-for', account.address] : null,
     (_, address) => getMaker().then(maker => maker.service('govPolling').getAllOptionsVotingFor(address)),
@@ -53,14 +64,28 @@ const QuickVote = ({ poll, showHeader, account, ...props }: Props) => {
     setEditing(false);
   };
 
-  const gap = 3;
+  const gap = 2;
   return (
     <Stack gap={gap} {...props}>
-      <Flex sx={{ flexDirection: 'row', justifyContent: 'flex-start', display: showHeader ? null : 'none' }}>
+      <Flex
+        sx={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          display: showHeader ? null : 'none'
+        }}
+      >
         <Text variant="caps" color="mutedAlt">
           Your Vote
         </Text>
-        {isRankedChoicePoll(poll) && <Icon name="stackedVotes" size={3} ml={2} />}
+        {isRankedChoicePoll(poll) && (
+          <Tooltip label={rankedChoiceBlurb}>
+            <Box sx={{ position: 'relative' }}>
+              {/* this is here because tooltip needs a child that can be passed a ref */}
+              <Icon name="stackedVotes" size={3} ml={2} />
+            </Box>
+          </Tooltip>
+        )}
       </Flex>
 
       {(!!addedChoice || currentVote !== null) && !editing ? (
