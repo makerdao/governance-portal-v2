5b905ffe207163e97373de2c4a90a6043e5ed248
Ethan Bennett <ebennett19@gmail.com>
2020-10-14 21:28:39 -0400

    Fix voting weight tooltip

--- components/polling/VotingWeight.tsx
+++ components/polling/VotingWeight.tsx
@@ -32,6 +32,16 @@ export default function (props): JSX.Element {
   }
   votingWeightDescription = votingWeightDescription.slice(0, -2);
 
+  const tooltipLabel = (
+    <>
+      {votingWeightDescription && (
+        <div style={{ fontWeight: 600, paddingBottom: '10px' }}>{votingWeightDescription}</div>
+      )}
+      Your voting weight is made up of MKR in your wallet, vote proxy, and voting contract. <br />
+      This amount is applied to all polls you vote on.
+    </>
+  );
+
   return (
     <Flex
       {...props}
@@ -45,17 +55,15 @@ export default function (props): JSX.Element {
     >
       <Flex sx={{ flexDirection: 'row' }}>
         <Text color="textSecondary">Voting weight</Text>
-        {votingWeightDescription ? (
-          <Tooltip
-            sx={{ mt: -1 }}
-            style={{ backgroundColor: 'white', fontSize: 15, borderRadius: 5 }}
-            label={votingWeightDescription}
-          >
-            <Box>
-              <Icon name="question" ml={2} mt={'6px'} />
-            </Box>
-          </Tooltip>
-        ) : null}
+        <Tooltip
+          sx={{ mt: -1 }}
+          style={{ backgroundColor: 'white', fontSize: 15, borderRadius: 5 }}
+          label={tooltipLabel}
+        >
+          <Box>
+            <Icon name="question" ml={2} mt={'6px'} />
+          </Box>
+        </Tooltip>
       </Flex>
       <Text sx={{ color: 'text' }}>
         {votingWeight ? `${votingWeight.total.toBigNumber().toFormat(2)} MKR` : '--'}
