3c95ec0293fb3de9e315dc40256faefb97b309a5
Lawrence Wang <l@lw.io>
2020-10-16 12:29:31 -0700

    cleanup variables that get passed to frontend

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
+  // everything in here gets exposed to the frontend.
+  // prefer NEXT_PUBLIC_* instead, which makes this behavior more explicit
   env: {
     INFURA_KEY: process.env.INFURA_KEY || '84842078b09946638c03157f83405213', // ethers default infura key
     ALCHEMY_KEY: process.env.ALCHEMY_KEY || '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC', // ethers default alchemy key
-    TRACING_RPC_NODE: process.env.TRACING_RPC_NODE, // foundation parity node with tracing enabled
-    MONGODB_URI: process.env.MONGODB_URI, // mongodb uri (currently only used for vote comments)
-    MONGODB_COMMENTS_DB: process.env.MONGODB_COMMENTS_DB, // mongodb db name for vote comments
     USE_PROD_SPOCK: process.env.USE_PROD_SPOCK // use production spock instance if true, otherwise use staging
   }
 };
