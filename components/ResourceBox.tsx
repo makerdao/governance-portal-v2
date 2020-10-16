700e50f690dc6186b396fb8e426a2bdfce9995dd
Josh Levine <joshuapark95@gmail.com>
2020-10-16 01:16:30 -0400

    limit link target area to resource box text

--- components/ResourceBox.tsx
+++ components/ResourceBox.tsx
@@ -1,4 +1,5 @@
-import { Box, Heading, Card, Link as ExternalLink, Flex, Text } from 'theme-ui';
+/** @jsx jsx */
+import { Box, Heading, Card, Link as ExternalLink, Flex, Text, jsx } from 'theme-ui';
 import { Icon } from '@makerdao/dai-ui-icons';
 
 export default function (props): JSX.Element {
@@ -8,60 +9,65 @@ export default function (props): JSX.Element {
         Resources
       </Heading>
       <Card variant="compact">
-        <ExternalLink href="https://forum.makerdao.com" target="_blank">
-          <Flex sx={{ alignItems: 'center' }}>
+        <Flex sx={{ alignItems: 'center' }}>
+          <ExternalLink href="https://forum.makerdao.com" target="_blank">
             <Text sx={{ color: 'accentBlue', fontSize: [2, 3], ':hover': { color: 'blueLinkHover' } }}>
               Maker Forum
               <Icon ml={2} name="arrowTopRight" size={2} />
             </Text>
-          </Flex>
-        </ExternalLink>
-        <ExternalLink href="https://community-development.makerdao.com/governance/governance" target="_blank">
-          <Flex sx={{ alignItems: 'center', pt: 3 }}>
+          </ExternalLink>
+        </Flex>
+        <Flex sx={{ alignItems: 'center', pt: 3 }}>
+          <ExternalLink
+            href="https://community-development.makerdao.com/governance/governance"
+            target="_blank"
+          >
             <Text sx={{ color: 'accentBlue', fontSize: [2, 3], ':hover': { color: 'blueLinkHover' } }}>
               Governance FAQs
               <Icon ml={2} name="arrowTopRight" size={2} />
             </Text>
-          </Flex>
-        </ExternalLink>
-        <ExternalLink href="https://blog.makerdao.com/makerdao-governance-risk-framework/" target="_blank">
-          <Flex sx={{ alignItems: 'center', pt: 3 }}>
+          </ExternalLink>
+        </Flex>
+        <Flex sx={{ alignItems: 'center', pt: 3 }}>
+          <ExternalLink href="https://blog.makerdao.com/makerdao-governance-risk-framework/" target="_blank">
             <Text sx={{ color: 'accentBlue', fontSize: [2, 3], ':hover': { color: 'blueLinkHover' } }}>
               Governance Risk Framework
               <Icon ml={2} name="arrowTopRight" size={2} />
             </Text>
-          </Flex>
-        </ExternalLink>
-        <ExternalLink href="https://github.com/makerdao/awesome-makerdao" target="_blank">
-          <Flex sx={{ alignItems: 'center', pt: 3 }}>
+          </ExternalLink>
+        </Flex>
+        <Flex sx={{ alignItems: 'center', pt: 3 }}>
+          <ExternalLink href="https://github.com/makerdao/awesome-makerdao" target="_blank">
             <Text sx={{ color: 'accentBlue', fontSize: [2, 3], ':hover': { color: 'blueLinkHover' } }}>
               Awesome MakerDAO
               <Icon ml={2} name="arrowTopRight" size={2} />
             </Text>
-          </Flex>
-        </ExternalLink>
-        <ExternalLink
-          href="https://community-development.makerdao.com/contribute/governance_and_risk_meetings/"
-          target="_blank"
-        >
-          <Flex sx={{ alignItems: 'center', pt: 3 }}>
+          </ExternalLink>
+        </Flex>
+
+        <Flex sx={{ alignItems: 'center', pt: 3 }}>
+          <ExternalLink
+            href="https://community-development.makerdao.com/contribute/governance_and_risk_meetings/"
+            target="_blank"
+          >
             <Text sx={{ color: 'accentBlue', fontSize: [2, 3], ':hover': { color: 'blueLinkHover' } }}>
               Governance call schedule
               <Icon ml={2} name="arrowTopRight" size={2} />
             </Text>
-          </Flex>
-        </ExternalLink>
-        <ExternalLink
-          href="https://calendar.google.com/calendar/embed?src=makerdao.com_3efhm2ghipksegl009ktniomdk%40group.calendar.google.com&ctz=America%2FLos_Angeles"
-          target="_blank"
-        >
-          <Flex sx={{ alignItems: 'center', pt: 3 }}>
+          </ExternalLink>
+        </Flex>
+
+        <Flex sx={{ alignItems: 'center', pt: 3 }}>
+          <ExternalLink
+            href="https://calendar.google.com/calendar/embed?src=makerdao.com_3efhm2ghipksegl009ktniomdk%40group.calendar.google.com&ctz=America%2FLos_Angeles"
+            target="_blank"
+          >
             <Text sx={{ color: 'accentBlue', fontSize: [2, 3], ':hover': { color: 'blueLinkHover' } }}>
               MakerDAO events calendar
               <Icon ml={2} name="arrowTopRight" size={2} />
             </Text>
-          </Flex>
-        </ExternalLink>
+          </ExternalLink>
+        </Flex>
       </Card>
     </Box>
   );
