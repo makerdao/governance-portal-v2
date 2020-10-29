fb1f9939140b627196896632dae9fc221a4ccf49
Lawrence Wang <l@lw.io>
2020-10-29 15:11:09 -0700

    render mock proposals & polls in landing page test

--- __tests__/pages/index.js
+++ __tests__/pages/index.js
@@ -4,11 +4,22 @@ import { ThemeProvider } from 'theme-ui';
 
 import theme from '../../lib/theme';
 import Index from '../../pages/index';
+import { getPostsAndPhotos, getExecutiveProposals, getPolls } from '../../lib/api';
+
+let blogPosts, proposals, polls;
+
+beforeAll(async () => {
+  [blogPosts, proposals, polls] = await Promise.all([
+    getPostsAndPhotos(),
+    getExecutiveProposals(),
+    getPolls()
+  ]);
+});
 
 test('renders', async () => {
   const { debug } = render(
     <ThemeProvider theme={theme}>
-      <Index blogPosts={[]} polls={[]} proposals={[]} />
+      <Index blogPosts={blogPosts} polls={polls} proposals={proposals} />
     </ThemeProvider>
   );
 });
