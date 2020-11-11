16fe90df73046e3882aa7dc35ad7db1bde72c5f7
Josh Levine <joshuapark95@gmail.com>
2020-11-11 05:23:30 -0500

    switch to all alchemy

--- lib/maker/network.ts
+++ lib/maker/network.ts
@@ -1,14 +1,23 @@
-import { SupportedNetworks } from "../constants";
+import { SupportedNetworks } from '../constants';
 
-export function networkToRpc(network: SupportedNetworks) {
+export function networkToRpc(network: SupportedNetworks, nodeProvider?: 'infura' | 'alchemy') {
   switch (network) {
     case SupportedNetworks.MAINNET:
+      if (nodeProvider === 'alchemy') {
+        return `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`;
+      }
       return `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`;
     case SupportedNetworks.KOVAN:
+      if (nodeProvider === 'alchemy') {
+        return `https://eth-kovan.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`;
+      }
       return `https://kovan.infura.io/v3/${process.env.INFURA_KEY}`;
     case SupportedNetworks.TESTNET:
       return `http://localhost:2000`;
     default:
+      if (nodeProvider === 'alchemy') {
+        return `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`;
+      }
       return `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`;
   }
 }
