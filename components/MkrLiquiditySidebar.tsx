2486aceea2a3ea2e5d54645b464afe04ba47236e
Tyler Sorensen <tyler@makerdao.com>
2020-11-16 12:48:50 -1000

    add uniswap and balancer, cleanup

--- components/MkrLiquiditySidebar.tsx
+++ components/MkrLiquiditySidebar.tsx
@@ -1,51 +1,68 @@
 /** @jsx jsx */
 import { Card, Flex, Link as ExternalLink, Text, Box, Heading, jsx } from 'theme-ui';
-import useSWR, { mutate } from 'swr';
+import useSWR from 'swr';
 import Skeleton from 'react-loading-skeleton';
 
 import Stack from './layouts/Stack';
 import getMaker, { DAI } from '../lib/maker';
-import { bigNumberKFormat } from '../lib/utils';
 import CurrencyObject from '../types/currency';
 import { MKR } from '../lib/maker';
+const axios = require('axios');
 
-const aaveMkrReserveAddress = "0x3dfd23A6c5E8BbcFc9581d2E864a68feb6a076d3";
-const aaveMkrReserveAbi = [
-  {"constant":true,"inputs":[{"internalType":"address","name":"_reserve","type":"address"}],"name":"getReserveAvailableLiquidity","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}
-  ];
+const aaveLendingPoolCore = '0x3dfd23A6c5E8BbcFc9581d2E864a68feb6a076d3';
+const uniswapV2MkrPool = '0xC2aDdA861F89bBB333c90c492cB837741916A225';
+
+async function getBalancerMkr() {
+  let balancerNum = 0;
+  return axios({
+    url: 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer',
+    method: 'post',
+    data: {
+      query: `
+        query PostsForPools {
+          pools(where: {tokensList_contains: ["0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2"], publicSwap: true}) {
+                  tokens {
+                    address
+                    balance
+                    symbol
+                  }
+              }
+        }
+        `
+    }
+  }).then((result) => {
+    result.data.data.pools.forEach(function (pool) {
+      pool.tokens.forEach(function (token) {
+         if (token.symbol == "MKR") {
+          balancerNum = balancerNum + parseFloat(token.balance)
+         }
+      })
+    });
+    return MKR(balancerNum);
+  });
+}
 
 async function getMkrLiquidity() {
   const maker = await getMaker();
-  const scs = maker.service('smartContract');
-  const aaveMkrReserve = scs.getContractByAddressAndAbi(
-    aaveMkrReserveAddress, aaveMkrReserveAbi);
   return Promise.all([
-    aaveMkrReserve.getReserveAvailableLiquidity(scs.getContractAddress('MCD_GOV')),
-    Promise.resolve(1),
-    Promise.resolve(1)
+    maker.service('token').getToken(MKR).balanceOf(aaveLendingPoolCore),
+    getBalancerMkr(),
+    maker.service('token').getToken(MKR).balanceOf(uniswapV2MkrPool)
   ]);
-}
-
-// if we are on the browser, trigger a prefetch as soon as possible
-if (typeof window !== 'undefined') {
-  getMkrLiquidity().then(liq => {
-    mutate('/mkr-liquidiy', liq, false);
-  });
 }
 
 type StatField = 'MKR in Aave' | 'MKR in Balancer' | 'MKR in Uniswap';
 
 export default function SystemStatsSidebar({ fields = [], ...props }: { fields: StatField[] }): JSX.Element {
-  const { data } = useSWR<CurrencyObject[]>('/mkr-liquidity', getMkrLiquidity);
+  const { data } = useSWR<CurrencyObject[]>('/mkr-liquidity', getMkrLiquidity, { refreshInterval: 60000 });
   const [aave, balancer, uniswap] = data || [];
-  console.log(MKR(aave));
   const statsMap = {
     'MKR in Aave': key => (
       <Flex key={key} sx={{ justifyContent: 'space-between', flexDirection: 'row' }}>
         <Text sx={{ fontSize: 3, color: 'textSecondary' }}>MKR in Aave</Text>
         <Text variant="h2" sx={{ fontSize: 3 }}>
-          {data ? (
-            `${MKR(aave).div(10**18).toFixed()} MKR`
+          {aave ? (
+            `${aave.toBigNumber().toFormat(0)} MKR`
           ) : (
             <Box sx={{ width: 6 }}>
               <Skeleton />
@@ -59,8 +76,8 @@ export default function SystemStatsSidebar({ fields = [], ...props }: { fields: 
       <Flex key={key} sx={{ justifyContent: 'space-between', flexDirection: 'row' }}>
         <Text sx={{ fontSize: 3, color: 'textSecondary' }}>MKR in Balancer</Text>
         <Text variant="h2" sx={{ fontSize: 3 }}>
-          {data ? (
-            `${balancer} MKR`
+          {balancer ? (
+            `${balancer.toBigNumber().toFormat(0)} MKR`
           ) : (
             <Box sx={{ width: 6 }}>
               <Skeleton />
@@ -72,10 +89,10 @@ export default function SystemStatsSidebar({ fields = [], ...props }: { fields: 
 
     'MKR in Uniswap': key => (
       <Flex key={key} sx={{ justifyContent: 'space-between', flexDirection: 'row' }}>
-        <Text sx={{ fontSize: 3, color: 'textSecondary' }}>MKR in Uniswap</Text>
+        <Text sx={{ fontSize: 3, color: 'textSecondary' }}>MKR in Uniswap V2</Text>
         <Text variant="h2" sx={{ fontSize: 3 }}>
-          {data ? (
-            `${uniswap} MKR`
+          {uniswap ? (
+            `${uniswap.toBigNumber().toFormat(0)} MKR`
           ) : (
             <Box sx={{ width: 6 }}>
               <Skeleton />
