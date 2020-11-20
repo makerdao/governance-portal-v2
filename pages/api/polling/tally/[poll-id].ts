a7e8144c1c4ba0d7d2d34b18e91d3ad024e5c509
Josh Levine <joshuapark95@gmail.com>
2020-11-19 21:58:22 -0500

    bump dai-plugin-gov, fix rcv progress bar stacks, convert a few currency objs to bns

--- pages/api/polling/tally/[poll-id].ts
+++ pages/api/polling/tally/[poll-id].ts
@@ -39,5 +39,5 @@ function createPollTallyRoute({ cacheType }: { cacheType: string }) {
 export { createPollTallyRoute };
 
 export default createPollTallyRoute({
-  cacheType: 's-maxage=30, stale-while-revalidate'
+  cacheType: 's-maxage=5, stale-while-revalidate'
 });
