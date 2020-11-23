c028e28b084ec2e88b40518d4a7aeb2cc97f2a63
Ashoka Finley <ashoka.finley@gmail.com>
2020-11-23 10:29:25 -0800

    continue testing

--- __tests__/pages/esmodule.js
+++ __tests__/pages/esmodule.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { renderWithTheme } from '../helpers';
-import { act, cleanup, fireEvent, render, wait, waitForElement, screen, waitFor } from '@testing-library/react';
+import { act, cleanup, fireEvent, render, wait, screen, waitFor } from '@testing-library/react';
 
 import { ESM } from '@makerdao/dai-plugin-governance/dist/utils/constants';
-
+import waitForExpect from 'wait-for-expect'
 import { MKR } from '@makerdao/dai';
 import { TestAccountProvider } from '@makerdao/test-helpers';
 import userEvent from '@testing-library/user-event'
 import { cache, SWRConfig } from "swr";
-import waitForExpect from 'wait-for-expect';
 import ESModule from '../../pages/esmodule';
 import getMaker from '../../lib/maker';
 import { accountsApi } from '../../stores/accounts';
+import ilkList from '../../lib/references'
+import BigNumber from 'bignumber.js';
 
-// const { click } = fireEvent;
-// // import configureMockStore from 'redux-mock-store';
-// // import thunk from 'redux-thunk';
-// const middlewares = [thunk];
-// const mockStore = configureMockStore(middlewares);
+export const WAD = new BigNumber('1e18');
+
+const ilks = ilkList.map(i => [i.symbol, i.currency])
+  .filter(i => i[0] !== 'MANA-A');
 
 const SWRESModule = () => (
     <SWRConfig value={{ dedupingInterval: 0}}>
@@ -87,9 +87,9 @@ describe('emergency shutdown render', () => {
     await findByText('Burn Your MKR', {}, {timeout: 5000})
   });
 
-  test.only('Burn MKR Modal Flow', async () => {
-    jest.setTimeout(10000)
-    const { getByTestId, getAllByTestId, getByText, getByRole, findByText, debug } = renderWithTheme(
+  test('Burn MKR Modal Flow', async () => {
+    jest.setTimeout(60000)
+    const { getByTestId, getAllByTestId, getByText, getByRole, findByText, findByTestId, debug } = renderWithTheme(
       <ESModule />
     );
 
@@ -122,84 +122,107 @@ describe('emergency shutdown render', () => {
     // Set Max Check
     fireEvent.click(getByText('Set max'));
     // debug()
-    waitFor(() => expect(getByRole('spinbutton').value).toEqual('2.0000'));
+    await waitFor(() => expect(getByRole('spinbutton').value).toEqual('2.0000'), {timeout: 5000});
 
     // MKR is Chief Check
     // getByTestId('voting-power');
 
     // Valid Amount Check
     fireEvent.change(getByRole('spinbutton'), { target: { value: amount - 2 } });
-    waitFor(() => expect(continueButton.disabled).toBeFalsy());
+    await waitFor(() => expect(continueButton.disabled).toBeFalsy());
     fireEvent.click(continueButton);
 
     // Third Step Render
-    waitFor(() => getByText('Burn amount'));
+    // await waitFor(() => getByText('Burn amount'))
     // waitFor(() => getByText('New ESM total'))
-    await findByText('New ESM total');
-    // let confirmInput;
-    // await act(() => {
-    //   confirmInput = getAllByTestId('confirm-input')[2];
-    // });
-    // let burnMKRbutton;
-    // await act(() => {
-    //   burnMKRbutton = getByText('Continue');
-    // });
-    // expect(burnMKRbutton.disabled).toBeTruthy();
+    await findByText('Burn amount')
+    await findByText('New ESM total')
+    // debug()
 
-    // // click the terms of service
-    // const tos = getByTestId('tosCheck');
-    // click(tos);
-    // expect(tos.checked).toBeTruthy();
+    let confirmInput = await findByTestId('confirm-input')
+    let burnMKRbutton = await findByText('Continue')
+    await waitFor(() => expect(burnMKRbutton.disabled).toBeTruthy());
 
-  //   // click the unlock mkr
-  //   const allowanceBtn = getByTestId('allowance-toggle').children[0];
-  //   await waitForElement(() => !allowanceBtn.disabled);
-  //   click(allowanceBtn);
-  //   await waitForElement(() => allowanceBtn.disabled);
+    // click the terms of service
+    let tos = await findByTestId('tosCheck')
+    fireEvent.click(tos);
+    await waitFor(() => expect(tos.checked).toBeTruthy());
 
-  //   // Incorrect Input Check
-  //   fireEvent.change(confirmInput, { target: { value: 'I am burning 2 MKR' } });
-  //   expect(burnMKRbutton.disabled).toBeTruthy();
+    // click the unlock mkr
+    const allowanceBtn = getByTestId('allowance-toggle');
+    await waitFor(() => expect(!allowanceBtn.disabled).toBeTruthy());
+    fireEvent.click(allowanceBtn);
+    await waitFor(() => expect(allowanceBtn.disabled).toBeTruthy());
 
-  //   // Correct Input Check
-  //   fireEvent.change(confirmInput, { target: { value: 'I am burning 1 MKR' } });
-  //   const step2 = await waitForElement(() => getByTestId('step2'));
+    // Incorrect Input Check
+    fireEvent.change(confirmInput, { target: { value: 'I am burning 2 MKR' } });
+    await waitFor(() => expect(burnMKRbutton.disabled).toBeTruthy());
 
-  //   await waitForElement(() => !burnMKRbutton.disabled);
-  //   click(burnMKRbutton);
+    // Correct Input Check
+    fireEvent.change(confirmInput, { target: { value: 'I am burning 1 MKR' } });
+    await waitFor(() => expect(!burnMKRbutton.disabled).toBeTruthy());
 
-  //   // Third Step Render
-  //   await wait(() => getByText('Your MKR is being burned'));
+    fireEvent.click(burnMKRbutton);
 
-  //   // Fourth Step Success Render
-  //   await wait(() => getByText('MKR Burned'));
+    // Third Step Render
+    await findByText('Sign Transaction');
+
+    // Fourth Step Success Render
+    await findByText('MKR successfully burned in ESM');
   });
 
 });
+describe('initiate emergency shutdown', () => {
 
-// test('show Initiate Shutdown button on threshold reached', async () => {
-//   let newESM = Object.assign({}, { ...esm, canFire: true, totalStaked: MKR(50000), thresholdAmount: 50000 });
-//   let subStore = mockStore({ accounts, esm: newESM });
-//   const { getByText } = await render(<Modules store={subStore} />);
-//   await wait(() => getByText('Initiate Emergency Shutdown'));
-// });
+  beforeAll(async () => {
+    maker = await getMaker();
+    // const nextAccount = TestAccountProvider.nextAccount();
+    // maker.useAccount('test-account');
+    // await waitForExpect(() => {
+    //   const currentAccount = accountsApi.getState().currentAccount;
+    //   expect(currentAccount.address).toBe(nextAccount.address);
+    //   expect(currentAccount.name).toBe('test-account');
+    // });
+    const token = maker.service('smartContract').getContract('MCD_GOV');
+    await token['mint(uint256)'](WAD.times(50000).toFixed());
+    const esm = maker.service('smartContract').getContract('MCD_ESM');
+    await token.approve(esm.address, -1); //approve unlimited
+    await esm.join(WAD.times(50000).toFixed());
+    const end = maker.service('smartContract').getContract('MCD_END');
+    for (let ilkInfo of ilks) {
+        const [ilk] = ilkInfo;
+        await end['cage(bytes32)'](stringToBytes(ilk));
+    }
+    const migVault = maker
+      .service('migration')
+      .getMigration('global-settlement-collateral-claims');
 
-// test('show disabled Initiate Shutdown button on shutdown initiated', async () => {
-//   let newESM = Object.assign(
-//     {},
-//     {
-//       ...esm,
-//       canFire: false,
-//       totalStaked: MKR(50000),
-//       thresholdAmount: 50000,
-//       // hack to allow for bigNumber
-//       cageTime: MKR(1580326770128)
-//     }
-//   );
-//   let subStore = mockStore({ accounts, esm: newESM });
-//   const { getByText, getByTestId, debug } = await render(<Modules store={subStore} />);
-//   const initiateButton = getByText('Initiate Emergency Shutdown');
-//   expect(initiateButton.disabled).toBeTruthy();
-//   await wait(() => getByTestId('shutdown-initiated'));
-// });
+    for (let vault of Object.keys(vaults)) {
+      await migVault.free(vaults[vault].id, vault);
+    }
+
+    await end.thaw();
+
+    for (let ilkInfo of ilks) {
+      const [ilk] = ilkInfo;
+      await end.flow(stringToBytes(ilk));
+    }
+  });
+
+  test('show Initiate Shutdown button on threshold reached', async () => {
+    const { findByText } = await renderWithTheme(<ESModule />);
+    await findByText('Initiate Emergency Shutdown');
+  });
+  
+  test('show disabled Initiate Shutdown button on shutdown initiated', async () => {
+    const esm = maker.service('smartContract').getContract('MCD_ESM');
+    await esm.fire();
+    const { findByText, getByTestId, debug } = await renderWithTheme(<ESModule store={subStore} />);
+    const initiateButton = await findByText('Initiate Emergency Shutdown');
+    waitFor(() => expect(initiateButton.disabled).toBeTruthy());
+    // await wait(() => getByTestId('shutdown-initiated'));
+  });
+})
+
+
 
