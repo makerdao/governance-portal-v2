68965b1c722a64f5112fe04622ec8f82c7ad2db2
Ethan Bennett <ebennett19@gmail.com>
2020-11-25 16:43:21 -0500

    Add assertions to test account connection

--- __tests__/components/header/AccountSelect.tsx
+++ __tests__/components/header/AccountSelect.tsx
@@ -9,8 +9,17 @@ beforeAll(() => {
 });
 
 test('can connect an account', async () => {
-    const { findByText } = render(<WrappedAccountSelect />);
-    click(await findByText('Connect wallet'));
+    const { findByText, findAllByText } = render(<WrappedAccountSelect />);
+    const connectButton = await findByText('Connect wallet');
+    expect(connectButton).toBeDefined();
+    
+    click(connectButton);
     click(await findByText('MetaMask'));
-    await findByText('0x16F', { exact: false });
-});
+
+    const copyButton = await findByText('Copy Address');
+    expect(copyButton).toBeDefined();
+    const etherscanButton = await findByText('etherscan', { exact: false });
+    expect(etherscanButton).toBeDefined();
+    const displayedAddress = await findAllByText('0x16F', { exact: false });
+    expect(displayedAddress.length).toBe(2);
+  });
