4327769096bdd6b3a946e33974fb62cc9411475b
Lawrence Wang <l@lw.io>
2020-12-11 16:37:59 -0800

    fix test

--- __tests__/pages/executive.tsx
+++ __tests__/pages/executive.tsx
@@ -1,8 +1,8 @@
-import ExecutiveOverviewPage from '../../pages/executive';
+import { ExecutiveOverview } from '../../pages/executive';
 import proposals from '../../mocks/proposals.json';
 import { renderWithTheme as render } from '../helpers';
 
 test('basic rendering', async () => {
-  const { getByText } = render(<ExecutiveOverviewPage proposals={proposals} />);
+  const { getByText } = render(<ExecutiveOverview proposals={proposals} />);
   getByText('Raise the Test Coverage');
 });
