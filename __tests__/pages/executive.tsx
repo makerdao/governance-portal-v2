722908b74612da9209dab805210894913a9805d6
Tyler Sorensen <tyler@makerdao.com>
2021-01-06 18:12:39 -1000

    more exec testing

--- __tests__/pages/executive.tsx
+++ __tests__/pages/executive.tsx
@@ -1,7 +1,9 @@
 import { ExecutiveOverview } from '../../pages/executive';
 import proposals from '../../mocks/proposals.json';
 import { injectProvider, connectAccount, renderWithAccountSelect as render } from '../helpers'; 
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, cleanup } from '@testing-library/react';
+import mixpanel from 'mixpanel-browser';
+import { cache } from 'swr';
 
 const { click } = fireEvent;
 let component;
@@ -14,6 +16,7 @@ async function setup() {
 
 beforeAll(async () => {
   injectProvider();
+  mixpanel.track = () => {};
   // temporary hack to hide spam errors and warnings from dependencies
   console.error = () => {};
   console.warn = () => {};
@@ -23,13 +26,19 @@ beforeEach(async () => {
   component = await setup();
 });
 
-test('basic rendering', async () => {
-  component.getByText('Raise the Test Coverage');
+test('can deposit', async () => {
+  const depositButton = await component.findByTestId('deposit-button');
+  click(depositButton);
+  const t = await component.findByText('Approve voting contract');
+  const approveButton = component.getByTestId('deposit-approve-button');
+  click(approveButton);
+  //TODO
 });
 
 test('can vote', async () => {
-  const voteButton = await component.findByTestId('vote-button-exec-overview-card');
-  expect(voteButton).toBeDefined();
-  click(voteButton);
-  expect(await component.findByText('Confirm Vote')).toBeDefined();
+  const [voteButtonOne, ] = await component.findAllByTestId('vote-button-exec-overview-card');
+  click(voteButtonOne);
+  const submitButton = await component.findByText('Submit Vote');
+  click(submitButton);
+  //FIXME: test that UI updates accordingly, for some reason the votedProposals isn't being updated even though the tx succeeds
 });
