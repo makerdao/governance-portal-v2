808fa4a51c95ee163a4cc3f26d35dade13352709
Ashoka Finley <ashoka.finley@gmail.com>
2021-01-24 22:37:57 -0700

    render plain text of parsed value

--- pages/polling/create.tsx
+++ pages/polling/create.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-
+import { useState } from 'react';
 import { Heading, Box, jsx, Button, Flex, Input, Label, Textarea, Select, Radio } from 'theme-ui';
 import Head from 'next/head';
 import Link from 'next/link';
@@ -13,9 +13,17 @@ import Tabs from '../../components/Tabs';
 import SystemStatsSidebar from '../../components/SystemStatsSidebar';
 import MkrLiquiditySidebar from '../../components/MkrLiquiditySidebar';
 import ResourceBox from '../../components/ResourceBox';
+import { validateUrl } from '../../lib/polling/validator';
+import Poll from '../../types/poll';
 
 const PollingCreate = () => {
   const bpi = useBreakpointIndex();
+  const [pollUrl, setPollUrl] = useState('');
+  const [parsedPoll, setParsedPoll] = useState<Poll | undefined>();
+  const urlValidation = async url => {
+    const result = await validateUrl(url);
+    if (result.valid) setParsedPoll(result.parsedData);
+  };
 
   return (
     <PrimaryLayout shortenFooter={true} sx={{ maxWidth: 'dashboard' }}>
@@ -49,35 +57,37 @@ const PollingCreate = () => {
                   tabTitles={['Poll Creator', 'Poll Explorer']}
                   tabPanels={[
                     <div key={1} sx={{ p: [3, 4] }}>
-                      {/* <Box as="form" onSubmit={e => e.preventDefault()}> */}
                       <Box>
                         <Label htmlFor="url">URL</Label>
                         <Flex sx={{ flexDirection: 'row' }}>
-                          <Input name="url" mb={3} />
-                          <Button variant="smallOutline" sx={{ height: '42px', width: '80px', ml: 3 }}>
+                          <Input name="url" mb={3} onChange={e => setPollUrl(e.target.value)} />
+                          <Button
+                            variant="smallOutline"
+                            onClick={() => urlValidation(pollUrl)}
+                            sx={{ height: '42px', width: '80px', ml: 3 }}
+                          >
                             Validate
                           </Button>
                         </Flex>
                       </Box>
-
+                      <Label htmlFor="pollId">Poll ID</Label>
+                      <Input name="pollId" mb={3} disabled value={parsedPoll?.pollId} />
                       <Label htmlFor="title">Title</Label>
-                      <Input name="title" mb={3} />
+                      <Input name="title" mb={3} disabled value={parsedPoll?.title} />
                       <Label htmlFor="summary">Summary</Label>
-                      <Input name="summary" mb={3} />
+                      <Input name="summary" mb={3} disabled value={parsedPoll?.summary} />
                       <Label htmlFor="options">Vote Options</Label>
-                      <Input name="options" mb={3} />
+                      <Input name="options" mb={3} disabled value={JSON.stringify(parsedPoll?.options)} />
                       <Label htmlFor="type">Vote Type</Label>
-                      <Select name="type" mb={3}>
-                        <option>Plurality Voting</option>
-                      </Select>
+                      <Input name="type" disabled mb={3} value={parsedPoll?.voteType} />
                       <Label htmlFor="category">Category</Label>
-                      <Select name="category" mb={3}>
-                        <option>Systemic Risk Variables</option>
-                      </Select>
+                      <Input name="category" disabled value={parsedPoll?.categories.join(', ')} />
                       <Label htmlFor="start">Poll Start Time (UTC)</Label>
                       <Input
                         name="start"
-                        defaultValue={new Date(Date.now()).toLocaleString()}
+                        value={
+                          parsedPoll && new Date(parseInt(parsedPoll?.startDate) * 1000).toLocaleDateString()
+                        }
                         mb={3}
                         disabled
                       />
@@ -85,13 +95,15 @@ const PollingCreate = () => {
                       <Input
                         name="end"
                         mb={3}
-                        defaultValue={new Date(Date.now() + 12096e5).toLocaleString()}
+                        value={
+                          parsedPoll && new Date(parseInt(parsedPoll?.endDate) * 1000).toLocaleDateString()
+                        }
                         disabled
                       />
-                      <Label htmlFor="duration">Poll Duration</Label>
-                      <Input name="duration" mb={3} defaultValue={'14 Days'} disabled />
+                      {/* <Label htmlFor="discussion">Discussion Link</Label>
+                      <Input name="discussion" mb={3} disabled value={parsedPoll && parsedPoll.discussionLink } /> */}
                       <Label htmlFor="proposal">Proposal</Label>
-                      <Textarea name="proposal" mb={3} />
+                      <Textarea name="proposal" mb={3} disabled value={parsedPoll?.content} />
                       <Flex>
                         <Button variant="primary">Create Poll</Button>
                         <Button variant="outline" sx={{ ml: 4 }}>
@@ -103,24 +115,28 @@ const PollingCreate = () => {
                     </div>,
                     <div key={1} sx={{ p: [3, 4] }}>
                       <table sx={{ width: '100%', textAlign: 'center', border: '1px solid black' }}>
-                        <tr>
-                          <th>Poll ID</th>
-                          <th>Link</th>
-                          <th>Status</th>
-                          <th>Poll Title</th>
-                          <th>Select</th>
-                        </tr>
-                        <tr>
-                          <td>411</td>
-                          <td>Icon</td>
-                          <td>Pending</td>
-                          <td sx={{ textAlign: 'left' }}>Increase System Surplus Buffer</td>
-                          <td>
-                            <Flex sx={{ justifyContent: 'center' }}>
-                              <Radio name="pollSelect" />
-                            </Flex>
-                          </td>
-                        </tr>
+                        <thead>
+                          <tr>
+                            <th>Poll ID</th>
+                            <th>Link</th>
+                            <th>Status</th>
+                            <th>Poll Title</th>
+                            <th>Select</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>411</td>
+                            <td>Icon</td>
+                            <td>Pending</td>
+                            <td sx={{ textAlign: 'left' }}>Increase System Surplus Buffer</td>
+                            <td>
+                              <Flex sx={{ justifyContent: 'center' }}>
+                                <Radio name="pollSelect" />
+                              </Flex>
+                            </td>
+                          </tr>
+                        </tbody>
                       </table>
                     </div>
                   ]}
@@ -144,3 +160,10 @@ const PollingCreate = () => {
 };
 
 export default PollingCreate;
+
+// multiHash: 'QmWPAu5zvDkBeVKqq9MGy4sYBgQfm5H1BtrYENMmq9J7xA',
+// url:
+//   'https://raw.githubusercontent.com/makerdao/community/master/governance/polls/Adjust%20the%20Dust%20Parameter%20-%20January%2018%2C%202021.md',
+// slug: 'QmWPAu5z',
+// options: { '0': 'Abstain', '1': 'Yes', '2': 'No' },
+// discussionLink: 'https://forum.makerdao.com/t/signal-request-increasing-dust-parameter/5963',
