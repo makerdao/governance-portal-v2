42185d907f04f8a5ba01316e3ffc7d9eb17930ca
Ashoka Finley <ashoka.finley@gmail.com>
2021-03-01 13:50:50 -0700

    update import for Theme UI"

--- components/header/AccountSelect.tsx
+++ components/header/AccountSelect.tsx
@@ -2,7 +2,8 @@
 
 import React, { useEffect, useState } from 'react';
 import { useBreakpointIndex } from '@theme-ui/match-media';
-import { jsx, Box, Flex, Text, Spinner, Button, Close, ThemeUICSSObject } from 'theme-ui';
+import { jsx, Box, Flex, Text, Spinner, Button, Close } from 'theme-ui';
+import { ThemeUICSSObject } from '@theme-ui/css';
 import { Icon } from '@makerdao/dai-ui-icons';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 import mixpanel from 'mixpanel-browser';
