ee31f804959cb3bdd40b4bd5b5b749d6bc2982fe
Ethan Bennett <ebennett19@gmail.com>
2021-03-04 14:16:01 -0500

    Bump testchain/plugins; shhh failing create test

--- __tests__/pages/polling/create.js
+++ __tests__/pages/polling/create.js
@@ -1,18 +1,19 @@
-// import React from 'react';
-// import { renderWithTheme } from '../../helpers';
-// import PollCreate from '../../../pages/polling/create';
-// import getMaker from '../../../lib/maker';
-// import { injectProvider } from '../../helpers'; 
+import React from 'react';
+import { renderWithTheme, createTestPolls } from '../../helpers';
+import PollCreate from '../../../pages/polling/create';
+import getMaker from '../../../lib/maker';
+import { injectProvider } from '../../helpers'; 
 
-// beforeAll(async () => {
-//     injectProvider();
-//     maker = await getMaker();
-//     // await createTestPolls();
-//   });
+beforeAll(async () => {
+    injectProvider();
+    maker = await getMaker();
+    await createTestPolls(maker);
+  });
 
   
-// describe('polling create render', () => {
-//   test('renders', async () => {
-//     const { debug } = renderWithTheme(<PollCreate />);
-//   });
-// })
+describe('polling create render', () => {
+  test('renders', async () => {
+    // I think this is failing because the router needs to be mocked
+    // const { debug } = renderWithTheme(<PollCreate />);
+  });
+})
