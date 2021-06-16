2d09d7c9e60470358087561b61d34822090364d0
Rafael Ventura <rafinskipg@gmail.com>
2021-06-16 18:07:51 +0200

    minor refactor on types

--- lib/maker/network.ts
+++ lib/maker/network.ts
@@ -1,23 +1,24 @@
+import { config } from '../config';
 import { SupportedNetworks } from '../constants';
 
-export function networkToRpc(network: SupportedNetworks, nodeProvider?: 'infura' | 'alchemy') {
+export function networkToRpc(network: SupportedNetworks, nodeProvider?: 'infura' | 'alchemy'): string {
   switch (network) {
     case SupportedNetworks.MAINNET:
       if (nodeProvider === 'alchemy') {
-        return `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`;
+        return `https://eth-mainnet.alchemyapi.io/v2/${config.ALCHEMY_KEY}`;
       }
-      return `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`;
+      return `https://mainnet.infura.io/v3/${config.INFURA_KEY}`;
     case SupportedNetworks.KOVAN:
       if (nodeProvider === 'alchemy') {
-        return `https://eth-kovan.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`;
+        return `https://eth-kovan.alchemyapi.io/v2/${config.ALCHEMY_KEY}`;
       }
-      return `https://kovan.infura.io/v3/${process.env.INFURA_KEY}`;
+      return `https://kovan.infura.io/v3/${config.INFURA_KEY}`;
     case SupportedNetworks.TESTNET:
-      return `http://localhost:2000`;
+      return 'http://localhost:2000';
     default:
       if (nodeProvider === 'alchemy') {
-        return `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`;
+        return `https://eth-mainnet.alchemyapi.io/v2/${config.ALCHEMY_KEY}`;
       }
-      return `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`;
+      return `https://mainnet.infura.io/v3/${config.INFURA_KEY}`;
   }
 }
