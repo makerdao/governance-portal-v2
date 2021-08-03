45163a5dd375d72fcba1dbf6aa0875fd8c147928
Adam Goth <adamgoth@gmail.com>
2021-08-03 14:49:39 +0200

    Remove truncation of address on profile page

--- components/delegations/DelegateDetail.tsx
+++ components/delegations/DelegateDetail.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
-import { jsx, Box, Text, Link as ExternalLink, Divider, Flex } from 'theme-ui';
 import React from 'react';
+import { jsx, Box, Text, Link as ExternalLink, Divider, Flex } from 'theme-ui';
+import { useBreakpointIndex } from '@theme-ui/match-media';
 import { getNetwork } from 'lib/maker';
 import { getEtherscanLink } from 'lib/utils';
 import { Delegate } from 'types/delegate';
@@ -12,39 +13,40 @@ type PropTypes = {
 };
 
 export function DelegateDetail({ delegate }: PropTypes): React.ReactElement {
+  const bpi = useBreakpointIndex();
   const { voteDelegateAddress } = delegate;
   return (
     <Box sx={{ variant: 'cards.primary', p: [0, 0] }}>
-      <Box sx={{ display: 'flex', p: 3 }}>
-        <DelegatePicture delegate={delegate} key={delegate.id} />
-        <Flex sx={{ justifyContent: 'space-between', width: '100%' }}>
-          <Box sx={{ ml: 2 }}>
-            <Text as="p" variant="microHeading" sx={{ fontSize: [3, 5] }}>
-              {delegate.name}
-            </Text>
-            <ExternalLink
-              title="View on etherescan"
-              href={getEtherscanLink(getNetwork(), voteDelegateAddress, 'address')}
-              target="_blank"
-            >
-              <Text as="p">
-                {voteDelegateAddress.substr(0, 6)}...
-                {voteDelegateAddress.substr(voteDelegateAddress.length - 5, voteDelegateAddress.length - 1)}
+      <Box sx={{ p: 3 }}>
+        <Flex>
+          <DelegatePicture delegate={delegate} key={delegate.id} />
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ ml: 2 }}>
+              <Text as="p" variant="microHeading" sx={{ fontSize: [3, 5] }}>
+                {delegate.name}
+              </Text>
+              <ExternalLink
+                title="View on etherescan"
+                href={getEtherscanLink(getNetwork(), voteDelegateAddress, 'address')}
+                target="_blank"
+              >
+                <Text as="p" sx={{ fontSize: bpi > 0 ? 3 : 1 }}>
+                  {voteDelegateAddress}
+                </Text>
+              </ExternalLink>
+            </Box>
+          </Box>
+        </Flex>
+        {delegate.externalUrl && (
+          <Box sx={{ mt: 2 }}>
+            <ExternalLink title="See external profile" href={delegate.externalUrl} target="_blank">
+              <Text sx={{ fontSize: 1 }}>
+                See external profile
+                <Icon ml={2} name="arrowTopRight" size={2} />
               </Text>
             </ExternalLink>
           </Box>
-
-          {delegate.externalUrl && (
-            <Flex sx={{ alignItems: 'flex-end' }}>
-              <ExternalLink title="See external profile" href={delegate.externalUrl} target="_blank">
-                <Text sx={{ fontSize: 1 }}>
-                  See external profile
-                  <Icon ml={2} name="arrowTopRight" size={2} />
-                </Text>
-              </ExternalLink>
-            </Flex>
-          )}
-        </Flex>
+        )}
       </Box>
       <Box sx={{ p: 3 }}>
         <div
