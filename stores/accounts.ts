bb8b9c072588bad9c1be188dceeb825b7f021c11
rafinskipg <rafinskipg@gmail.com>
2021-08-11 12:39:52 +0200

    Lint

--- stores/accounts.ts
+++ stores/accounts.ts
@@ -9,7 +9,6 @@ import { VoteDelegateContract } from 'types/voteDelegateContract';
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-
 type Store = {
   currentAccount?: Account;
   proxies: Record<string, VoteProxyContract | null>;
--- types/voteDelegateContract.d.ts
+++ types/voteDelegateContract.d.ts
@@ -1,5 +1,5 @@
 export type VoteDelegateContract = {
-    getVoteDelegateAddress: () => string;
-    voteExec: (picks: string[] | string) => Promise<any>;
-    votePoll: (pollIds: string[], pollOptions: string[]) => Promise<any>;
-  };
+  getVoteDelegateAddress: () => string;
+  voteExec: (picks: string[] | string) => Promise<any>;
+  votePoll: (pollIds: string[], pollOptions: string[]) => Promise<any>;
+};
