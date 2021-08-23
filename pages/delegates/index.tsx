59ba738923c3224b5d0c93a4486e4e54d08cbdb0
Rafael Ventura <rafinskipg@gmail.com>
2021-08-23 19:00:00 +0200

    fix build

--- pages/delegates/index.tsx
+++ pages/delegates/index.tsx
@@ -24,7 +24,7 @@ import useAccountsStore from 'stores/accounts';
 import Link from 'next/link';
 import { DelegatesSystemInfo } from 'components/delegations/DelegatesSystemInfo';
 import { DelegatesAPIResponse, DelegatesAPIStats } from 'types/delegatesAPI';
-import { getExecutiveProposals } from 'lib/api';
+import { getExecutiveProposals } from 'modules/executives/api/fetchExecutives';
 import { CMSProposal } from 'types/proposal';
 
 type Props = {
