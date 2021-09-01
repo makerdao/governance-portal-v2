212ac7ccbe9e3017edd58b8367c0aeb4e864b147
Adam Goth <adamgoth@gmail.com>
2021-09-01 16:17:32 +0200

    Update delegate detail for shadows

--- modules/delegates/components/DelegateDetail.tsx
+++ modules/delegates/components/DelegateDetail.tsx
@@ -25,7 +25,7 @@ export function DelegateDetail({ delegate, stats }: PropTypes): React.ReactEleme
 
   const tabTitles = [
     delegate.status === DelegateStatusEnum.recognized ? 'Delegate Credentials' : null,
-    'Participation Metrics',
+    delegate.status === DelegateStatusEnum.recognized ? 'Participation Metrics' : null,
     'Voting History'
   ].filter(i => !!i) as string[];
 
@@ -58,22 +58,20 @@ export function DelegateDetail({ delegate, stats }: PropTypes): React.ReactEleme
               <Box sx={{ width: '100%' }}>
                 <Box sx={{ ml: 3 }}>
                   <Text as="p" variant="microHeading" sx={{ fontSize: [3, 5] }}>
-                    {delegate.name}
+                    {delegate.name !== '' ? delegate.name : 'Shadow Delegate'}
                   </Text>
                   <ExternalLink
                     title="View on etherescan"
                     href={getEtherscanLink(getNetwork(), voteDelegateAddress, 'address')}
                     target="_blank"
                   >
-                    <Text as="p" sx={{ fontSize: [1,3] }}>
+                    <Text as="p" sx={{ fontSize: [1, 3] }}>
                       Delegate contract <Icon ml={2} name="arrowTopRight" size={2} />
                     </Text>
                   </ExternalLink>
-                  <Box></Box>
                 </Box>
               </Box>
             </Flex>
-            
           </Box>
           <Box mt={[3, 0]}>
             <DelegateContractExpiration delegate={delegate} />
