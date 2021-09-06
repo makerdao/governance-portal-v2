ebfcfb8830f9752ad51b24517f2b4de87bde2f72
Rafael Ventura <rafinskipg@gmail.com>
2021-09-06 18:39:56 +0200

    Lint

--- components/polling/PollOverviewCard.tsx
+++ components/polling/PollOverviewCard.tsx
@@ -55,22 +55,16 @@ export default function PollOverviewCard({
         )}
         <Box sx={{ cursor: 'pointer' }}>
           <Box>
-            <Link
-              href={`/polling/${poll.slug}?network=${network}`}
-              passHref
-            >
-              <InternalLink  variant="nostyle">
+            <Link href={`/polling/${poll.slug}?network=${network}`} passHref>
+              <InternalLink variant="nostyle">
                 <Text variant="microHeading" sx={{ fontSize: [3, 5] }}>
                   {poll.title}
                 </Text>
               </InternalLink>
             </Link>
           </Box>
-          <Link
-            href={`/polling/${poll.slug}?network=${network}`}
-            passHref
-          >
-            <InternalLink  variant="nostyle">
+          <Link href={`/polling/${poll.slug}?network=${network}`} passHref>
+            <InternalLink variant="nostyle">
               <Text
                 sx={{
                   fontSize: [2, 3],
