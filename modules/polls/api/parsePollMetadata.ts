cd123a4e0956b3b429ba7211931507f3ea6b0a39
Adam Goth <adamgoth@gmail.com>
2021-09-13 15:31:50 +0200

    Fix poll sort

--- modules/polls/api/parsePollMetadata.ts
+++ modules/polls/api/parsePollMetadata.ts
@@ -5,56 +5,54 @@ import { backoffRetry, timeoutPromise } from 'lib/utils';
 import matter from 'gray-matter';
 import { parsePollMetadata } from '../helpers/parser';
 
-
 export async function parsePollsMetadata(pollList): Promise<Poll[]> {
-    let numFailedFetches = 0;
-    const failedPollIds: number[] = [];
-    const polls: Poll[] = [];
-  
-    for (const pollGroup of chunk(
-      uniqBy(pollList, p => p.multiHash),
-      20
-    )) {
-      // fetch polls in batches, don't fetch a new batch until the current one has resolved
-      const pollGroupWithData = await Promise.all(
-        pollGroup.map(async (p: PartialPoll) => {
-          let document = '';
-          try {
-            document = await timeoutPromise(
-              5000, // reject if it takes longer than this to fetch
-              backoffRetry(3, () => fetch(p.url))
-            ).then(resp => resp?.text());
-            if (!(document.length > 0 && Object.keys(matter(document).data?.options)?.length > 0))
-              throw new Error();
-          } catch (err) {
-            numFailedFetches += 1;
-            failedPollIds.push(p.pollId);
-            return null;
-          }
-  
-          return parsePollMetadata(p, document);
-        })
-      ).then(polls =>
-        (polls as any[])
-          .filter(p => !!p && !!p.summary && !!p.options)
-          .filter(poll => new Date(poll.startDate).getTime() <= Date.now())
-      );
-  
-      polls.push(...pollGroupWithData);
-    }
-  
-    console.log(
-      `---
+  let numFailedFetches = 0;
+  const failedPollIds: number[] = [];
+  const polls: Poll[] = [];
+
+  for (const pollGroup of chunk(
+    uniqBy(pollList, p => p.multiHash),
+    20
+  )) {
+    // fetch polls in batches, don't fetch a new batch until the current one has resolved
+    const pollGroupWithData = await Promise.all(
+      pollGroup.map(async (p: PartialPoll) => {
+        let document = '';
+        try {
+          document = await timeoutPromise(
+            5000, // reject if it takes longer than this to fetch
+            backoffRetry(3, () => fetch(p.url))
+          ).then(resp => resp?.text());
+          if (!(document.length > 0 && Object.keys(matter(document).data?.options)?.length > 0))
+            throw new Error();
+        } catch (err) {
+          numFailedFetches += 1;
+          failedPollIds.push(p.pollId);
+          return null;
+        }
+
+        return parsePollMetadata(p, document);
+      })
+    ).then(polls =>
+      (polls as any[])
+        .filter(p => !!p && !!p.summary && !!p.options)
+        .filter(poll => new Date(poll.startDate).getTime() <= Date.now())
+    );
+
+    polls.push(...pollGroupWithData);
+  }
+
+  console.log(
+    `---
     Failed to fetch docs for ${numFailedFetches}/${pollList.length} polls.
     IDs: ${failedPollIds}`
-    );
-  
-    return (
-      polls
-        // closest to expiration shown first
-        .sort((a, b) =>
-          Date.now() - new Date(a.endDate).getTime() < Date.now() - new Date(b.endDate).getTime() ? 1 : -1
-        )
-    );
-  }
-  
+  );
+
+  return (
+    polls
+      // closest to expiration shown first
+      .sort((a, b) =>
+        Date.now() - new Date(a.endDate).getTime() < Date.now() - new Date(b.endDate).getTime() ? -1 : 1
+      )
+  );
+}
