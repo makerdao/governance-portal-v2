4be391f9f5c8ffeddbb42436595160a5f1c573e1
Adam Goth <adamgoth@gmail.com>
2021-09-24 15:06:34 +0200

    Cache tally results

--- modules/polling/api/fetchPollTally.ts
+++ modules/polling/api/fetchPollTally.ts
@@ -1,9 +1,25 @@
 import { SupportedNetworks } from 'lib/constants';
 import getMaker from 'lib/maker';
+import { config } from 'lib/config';
+import { fsCacheGet, fsCacheSet } from 'lib/fscache';
 import { RawPollTally } from '../types';
 
 export async function fetchPollTally(pollId: number, network?: SupportedNetworks): Promise<RawPollTally> {
   const maker = await getMaker(network);
 
-  return maker.service('govPolling').getTallyRankedChoiceIrv(pollId);
+  const cacheKey = `tally-${pollId}`;
+  if (config.USE_FS_CACHE) {
+    const cachedTally = fsCacheGet(cacheKey, network);
+    if (cachedTally) {
+      return JSON.parse(cachedTally);
+    }
+  }
+
+  const tally = await maker.service('govPolling').getTallyRankedChoiceIrv(pollId);
+
+  if (config.USE_FS_CACHE) {
+    fsCacheSet(cacheKey, JSON.stringify(tally), network);
+  }
+
+  return tally;
 }
