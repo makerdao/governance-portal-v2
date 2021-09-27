30e0105bce7ce5cee3232b1f085142b10a0ced7b
Phil Bain <bain.pmcg@gmail.com>
2021-09-27 03:01:41 -0600

    add sentry with npx wizard (#183)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@ const path = require('path');
 
 require('dotenv').config({ path: './.env' });
 
-module.exports = {
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+const { withSentryConfig } = require('@sentry/nextjs');
+
+// Main Next.js config
+const moduleExports = {
   // everything in here gets exposed to the frontend.
   // prefer NEXT_PUBLIC_* instead, which makes this behavior more explicit
   env: {
@@ -32,8 +39,24 @@ module.exports = {
     return [
       {
         source: '/delegates/:address',
-        destination: '/address/:address',
-      },
+        destination: '/address/:address'
+      }
     ];
-  },
+  }
 };
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
