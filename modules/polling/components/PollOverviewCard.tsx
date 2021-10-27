b0451c1bfb4dd18ffc6090710cb6e0ba91929be5
Rafael Ventura <rafinskipg@gmail.com>
2021-10-27 15:09:25 +0200

    change order of tags

--- modules/polling/components/PollOverviewCard.tsx
+++ modules/polling/components/PollOverviewCard.tsx
@@ -79,11 +79,21 @@ export default function PollOverviewCard({
           </Link>
         </Box>
 
+        <Flex>
+          {poll.categories.map(c => (
+            <Box key={c} sx={{ marginRight: 2 }}>
+              <PollCategoryTag category={c} />
+            </Box>
+          ))}
+        </Flex>
+        
         {bpi > 0 && (
           <div>
             <CountdownTimer endText="Poll ended" endDate={poll.endDate} />
           </div>
         )}
+
+       
         <Flex sx={{ alignItems: 'center' }}>
           {canVote &&
             bpi === 0 &&
@@ -141,13 +151,7 @@ export default function PollOverviewCard({
           {isActivePoll(poll) ? '' : <PollOptionBadge poll={poll} sx={{ ml: 3, color: 'text' }} />}
           <VotingStatus sx={{ display: reviewPage ? 'none' : ['none', 'block'], ml: 3 }} poll={poll} />
         </Flex>
-        <Flex>
-          {poll.categories.map(c => (
-            <Box key={c} sx={{ marginRight: 2 }}>
-              <PollCategoryTag category={c} />
-            </Box>
-          ))}
-        </Flex>
+        
       </Stack>
       {showQuickVote && (
         <QuickVote poll={poll} showHeader={true} account={account} sx={{ maxWidth: 7, ml: 5 }} />
