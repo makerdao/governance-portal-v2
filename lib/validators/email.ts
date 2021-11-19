d202d9c544e76e5f4ffe1bcebc48fb1418141853
Phil Bain <bain.pmcg@gmail.com>
2021-11-19 15:15:22 -0700

    add a new string to edit title matcher

--- lib/validators/email.ts
+++ lib/validators/email.ts
@@ -1,3 +1,4 @@
+// prettier-ignore
 // eslint-disable-next-line
 const rfc2822EmailRegex = /[a-z0-9!#$%&'*+\/=?^_{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9][a-z0-9-]*[a-z0-9]/;
 
