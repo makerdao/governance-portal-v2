62095f7558dd2f1ca2fbec46dc4208a08943e9dd
Rafael Ventura <rafinskipg@gmail.com>
2021-11-29 17:47:14 +0100

    hot fix tally

--- modules/polling/helpers/getPollTaly.ts
+++ modules/polling/helpers/getPollTaly.ts
@@ -23,7 +23,6 @@ export async function getPollTally(poll: Poll, network?: SupportedNetworks): Pro
   const tally: RawPollTally = await backoffRetry(3, () =>
     fetchPollTally(poll.pollId, voteType, false, currentNetwork)
   );
-
   const maker = await getMaker(currentNetwork);
   const votesByAddress: PollTallyVote[] = (
     await maker.service('govPolling').getMkrAmtVotedByAddress(poll.pollId)
@@ -35,12 +34,26 @@ export async function getPollTally(poll: Poll, network?: SupportedNetworks): Pro
 
   const parsedTally = {
     pollVoteType: voteType,
-    options: tally.options,
+    options: Object.keys(tally.options).length > 0 ? tally.options : {
+      "0": {
+        mkrSupport:new BigNumber(0),
+        winner: false 
+      },
+      "1": {
+        mkrSupport:new BigNumber(0),
+        winner: false 
+      },
+      "2": {
+        mkrSupport:new BigNumber(0),
+        winner: false 
+      }
+    },
     winner,
     totalMkrParticipation,
     numVoters,
     votesByAddress
   } as RawPollTally;
+
 
   if ('rounds' in tally) (parsedTally as RawPollTallyRankedChoice).rounds = tally.rounds;
 
