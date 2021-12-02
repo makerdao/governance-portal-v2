cdc623427d640086d275fc81605d8de32049f374
Rafael Ventura <rafinskipg@gmail.com>
2021-12-02 16:22:27 +0100

    Test/add test empty tally (#260)
    * Add test for empty tally
    
    * do not ignore helpers tests
    
    * add explanation
    
    * Fix ignore pattern
    
    * Update getPollTally.spec.ts

---
+++ modules/polling/api/fetchVotesByAddress.ts
@@ -0,0 +1,17 @@
+import BigNumber from 'bignumber.js';
+import { SupportedNetworks } from 'lib/constants';
+import getMaker, { getNetwork } from 'lib/maker';
+import { PollTallyVote } from '../types';
+
+export async function fetchVotesByAddresForPoll(
+  pollId: number,
+  network?: SupportedNetworks
+): Promise<PollTallyVote[]> {
+  const currentNetwork = network || getNetwork();
+
+  const maker = await getMaker(currentNetwork);
+
+  return (await maker.service('govPolling').getMkrAmtVotedByAddress(pollId)).sort((a, b) =>
+    new BigNumber(a.mkrSupport).lt(new BigNumber(b.mkrSupport)) ? 1 : -1
+  );
+}
