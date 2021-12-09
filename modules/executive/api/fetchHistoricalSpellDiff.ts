1aefc2a81a9ed6ea3e99a9d60bb7089373787fe4
Phil Bain <bain.pmcg@gmail.com>
2021-12-09 16:51:24 -0700

    move hardcoded vals into constants

--- modules/executive/api/fetchHistoricalSpellDiff.ts
+++ modules/executive/api/fetchHistoricalSpellDiff.ts
@@ -1,29 +1,28 @@
 import { ethers } from 'ethers';
+import { DECODED_SPELL_ENDPOINT, SIGNATURE_CAST } from 'lib/constants';
 import { fetchJson } from 'lib/fetchJson';
 import { validateDiff } from '../helpers/spellDiffParsers';
 import { DecodedDiffAPIResponse, SpellDiff } from '../types';
 
 export async function fetchHistoricalSpellDiff(proposalAddress: string): Promise<SpellDiff[]> {
   if (!proposalAddress) return [];
-  // TODO: this sig is for 'cast()' which is what we're looking for.
-  // It could also also be retrieved by using ethers Interface.encodeFunctionData(), but we always know it, so...
-  const castSig = '0x96d373e5';
 
   // Need to find the tx of the cast spell
+  // TODO: is there a backup procedure we could use to get the hash?
   const provider = new ethers.providers.EtherscanProvider();
   const history = await provider.getHistory(proposalAddress);
-  const castTx = history.filter(h => h.data === castSig);
+  const castTx = history.filter(h => h.data === SIGNATURE_CAST);
 
   if (castTx.length > 1) {
     // TODO: Need to loop & fetch the txns from the provider to find the successful one
     console.warn('multiple matching txs', castTx);
   }
   const [{ hash }] = castTx;
-  console.log('transaction hash for decoding', hash);
+  console.log('transaction hash for decoding spell', hash);
 
   // TODO: currently the endpoint is hardcoded to only allow the following hash:
   const hcHash = '0xf91cdba571422ba3da9e7b79cbc0d51e8208244c2679e4294eec4ab5807acf7f';
-  const url = `http://18.157.179.179/api/v1/transactions/${hcHash}/diffs/decoded`;
+  const url = DECODED_SPELL_ENDPOINT(hcHash);
 
   const diffs: DecodedDiffAPIResponse[] = await fetchJson(url);
   const validated = diffs.map(diff => validateDiff(diff));
--- modules/executive/api/fetchSimulationSpellDiffs.ts
+++ modules/executive/api/fetchSimulationSpellDiffs.ts
@@ -1,22 +1,26 @@
 import { fetchJson } from 'lib/fetchJson';
 import { validateDiff } from '../helpers/spellDiffParsers';
 import { SimulationDiffAPIResponse, SpellDiff } from '../types';
+import {
+  SIMULATE_TX_ENDPOINT,
+  SIMULATE_TX_GAS,
+  SIMULATE_TX_GAS_PRICE,
+  SIGNATURE_CAST,
+  SIMULATE_TX_VALUE,
+  SIMULATE_TX_FROM
+} from 'lib/constants';
 
 type Response = Record<'diffs', SimulationDiffAPIResponse[]>;
 
 export async function fetchSimulationSpellDiffs(proposalAddress: string): Promise<SpellDiff[]> {
-  const from_address = '0x5cab1e5286529370880776461c53a0e47d74fb63'; // Think its just the caster of the spell, could be anyone?
-  const to_address = '0x82b24156f0223879aaac2dd0996a25fe1ff74e1a'; // the spell address
-  // const data = data2;
-  const data = '0x96d373e5'; // The spell signature can also be pulled from the interface but should always be "cast" right?
-  const gas = '0x1c6b9e'; // from the example, should see if we can get it dynamically
-  const gas_price = '0x23bd501f00';
+  proposalAddress = '0x82b24156f0223879aaac2dd0996a25fe1ff74e1a'; // the spell address
   const execute_on_top_of_block_number = 13624481; // from example. could be current block, eh?
-  // const timestamp = 1637047755; // 11/15 5:29pm mst
-  const value = 0; // think its always 0 in our case
+  // const timestamp = 1637047755; // 11/15 5:29pm mst doesn't appear to be required
 
-  const ip = '18.157.179.179';
-  const url = `http://${ip}/api/v1/transactions/simulation/?from_address=${from_address}&to_address=${to_address}&data=${data}&gas=${gas}&gas_price=${gas_price}&execute_on_top_of_block_number=${execute_on_top_of_block_number}&value=${value}`;
+  // May not need blocknumber once the API is fixed:
+  // If no executeOnTopOfBlockNumber is provided - transaction will be simulated on top of the head block
+
+  const url = `${SIMULATE_TX_ENDPOINT}/?from_address=${SIMULATE_TX_FROM}&to_address=${proposalAddress}&data=${SIGNATURE_CAST}&gas=${SIMULATE_TX_GAS}&gas_price=${SIMULATE_TX_GAS_PRICE}&execute_on_top_of_block_number=${execute_on_top_of_block_number}&value=${SIMULATE_TX_VALUE}`;
   const { diffs } = <Response>await fetchJson(url);
   const validated = diffs.map(diff => validateDiff(diff));
   return validated;
