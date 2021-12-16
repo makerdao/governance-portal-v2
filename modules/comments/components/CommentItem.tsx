3fa085a340668f37adf5bf8b7807e5b941029f31
Adam Goth <adamgoth@gmail.com>
2021-12-16 11:09:54 +0100

    Align text

--- modules/comments/components/CommentItem.tsx
+++ modules/comments/components/CommentItem.tsx
@@ -81,7 +81,14 @@ export default function CommentItem({
             </Box>
           )}
         </Box>
-        <Flex sx={{ flexDirection: 'column', alignItems: ['flex-start', 'flex-end'], maxWidth: ['100%', '50%'] }}>
+        <Flex
+          sx={{
+            flexDirection: 'column',
+            alignItems: ['flex-start', 'flex-end'],
+            maxWidth: ['100%', '265px'],
+            textAlign: ['left', 'right']
+          }}
+        >
           <Text as="p" variant="caps" color="textMuted" sx={{ lineHeight: '22px' }}>
             {formatDateWithTime(comment.comment.date)}
           </Text>
