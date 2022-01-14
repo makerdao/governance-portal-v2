d70ed605c78ea1cbf1f58ce861710acd57c177b0
Phil Bain <bain.pmcg@gmail.com>
2022-01-14 15:59:47 -0700

    fix signer in getContracts

--- modules/web3/hooks/useContracts.tsx
+++ modules/web3/hooks/useContracts.tsx
@@ -1,9 +1,16 @@
+import { Web3Provider } from '@ethersproject/providers';
 import { EthSdk, getContracts } from '../helpers/getContracts';
 import { useActiveWeb3React } from 'modules/web3/hooks/useActiveWeb3React';
 import { SupportedChainId } from 'modules/web3/web3.constants';
 
-export const useContracts = (): EthSdk => {
-  const { chainId, library } = useActiveWeb3React();
+type Props = {
+  chainId?: SupportedChainId;
+  library?: Web3Provider;
+  account?: string | null;
+};
 
-  return getContracts(chainId ?? SupportedChainId.MAINNET, library);
+export const useContracts = (): EthSdk => {
+  const { chainId, library, account }: Props = useActiveWeb3React();
+
+  return getContracts(chainId ?? SupportedChainId.MAINNET, library, account);
 };
