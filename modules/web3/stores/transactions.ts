46726547518c65c388af1234dcf7f2d905fcef63
Phil Bain <bain.pmcg@gmail.com>
2022-01-17 15:50:27 -0700

    handle errored tx in tx manager

--- modules/web3/stores/transactions.ts
+++ modules/web3/stores/transactions.ts
@@ -1,12 +1,10 @@
 import create from 'zustand';
 import invariant from 'tiny-invariant';
 import { v4 as uuidv4 } from 'uuid';
-// import getMaker from 'lib/maker';
+import { ContractTransaction } from 'ethers';
 import { Transaction, TXMined, TXPending, TXInitialized, TXError } from '../types/transaction';
 import { parseTxError } from '../helpers/errors';
 import { accountsApi } from '../../app/stores/accounts';
-import { ethers } from 'ethers';
-import { getNetwork } from 'lib/maker';
 
 type Hooks = {
   pending?: (txHash: string) => void;
@@ -21,7 +19,7 @@ type Store = {
   setPending: (txId: string, hash: string) => void;
   setMined: (txId: string) => void;
   setError: (txId: string, error?: { message: string }) => void;
-  track: (txCreator: () => Promise<any>, message?: string, hooks?: Hooks) => any;
+  track: (txCreator: () => Promise<ContractTransaction>, message?: string, hooks?: Hooks) => any;
 };
 
 const [useTransactionsStore, transactionsApi] = create<Store>((set, get) => ({
@@ -111,8 +109,6 @@ const [useTransactionsStore, transactionsApi] = create<Store>((set, get) => ({
   },
 
   track: async (txCreator, message = '', hooks) => {
-    const network = getNetwork();
-    const provider = ethers.getDefaultProvider(network);
     const account = accountsApi.getState().currentAccount;
 
     if (!account) return;
@@ -125,50 +121,24 @@ const [useTransactionsStore, transactionsApi] = create<Store>((set, get) => ({
 
     const txPromise = txCreator();
     const tx = await txPromise;
-    // console.log('tx', tx);
 
-    //Pending tx
+    // We are in "pending" state because the txn has now been been sent
     get().setPending(txId, tx.hash);
     if (typeof hooks?.pending === 'function') hooks.pending(tx.hash);
 
-    //Mined tx
-    provider.waitForTransaction(tx.hash).then(txn => {
-      get().setMined(txId);
-      if (typeof hooks?.mined === 'function') hooks.mined(txId, tx.hash);
-    });
+    // Handle mined or error txns
+    tx.wait()
+      .then(txn => {
+        get().setMined(txId);
+        if (typeof hooks?.mined === 'function') hooks.mined(txId, tx.hash);
+      })
+      .catch(e => {
+        get().setError(txId, e);
+        if (typeof hooks?.error === 'function') hooks.error();
+      });
 
-    // Error tx
-    //TODO...
     return txId;
   }
-  // track: async (txCreator, message = '', hooks) => {
-  //   const maker = await getMaker();
-  //   const txPromise = txCreator();
-  //   // noop catch since we handle tx errors via the manager
-  //   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  //   txPromise.catch(() => {});
-
-  //   const from = maker.currentAddress();
-  //   const txId = uuidv4();
-
-  //   get().initTx(txId, from, message);
-  //   maker.service('transactionManager').listen(txPromise, {
-  //     pending: ({ hash }) => {
-  //       get().setPending(txId, hash);
-  //       if (typeof hooks?.pending === 'function') hooks.pending(hash);
-  //     },
-  //     mined: ({ hash }) => {
-  //       get().setMined(txId);
-  //       if (typeof hooks?.mined === 'function') hooks.mined(txId, hash);
-  //     },
-  //     error: (_, error) => {
-  //       get().setError(txId, error);
-  //       if (typeof hooks?.error === 'function') hooks.error();
-  //     }
-  //   });
-
-  //   return txId;
-  // }
 }));
 
 const transactionsSelectors = {
