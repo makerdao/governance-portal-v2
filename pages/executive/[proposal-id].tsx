48d811bcfea363edb6c8e74a65ccfa4466ce0d83
Adam Goth <adamgoth@gmail.com>
2022-01-21 12:04:47 +0100

    More default network updates

--- pages/executive/[proposal-id].tsx
+++ pages/executive/[proposal-id].tsx
@@ -26,12 +26,13 @@ import { useSpellData } from 'modules/executive/hooks/useSpellData';
 import { useVotedProposals } from 'modules/executive/hooks/useVotedProposals';
 import { useHat } from 'modules/executive/hooks/useHat';
 import { useMkrOnHat } from 'modules/executive/hooks/useMkrOnHat';
-import { getNetwork, isDefaultNetwork } from 'lib/maker';
+import { getNetwork } from 'lib/maker';
 import { cutMiddle, limitString } from 'lib/string';
 import { getStatusText } from 'modules/executive/helpers/getStatusText';
 import { useAnalytics } from 'modules/app/client/analytics/useAnalytics';
 import { ANALYTICS_PAGES } from 'modules/app/client/analytics/analytics.constants';
 import { getEtherscanLink } from 'modules/web3/helpers/getEtherscanLink';
+import { isDefaultNetwork } from 'modules/web3/helpers/isDefaultNetwork';
 
 // stores
 import useAccountsStore from 'modules/app/stores/accounts';
@@ -54,6 +55,8 @@ import { Address } from 'modules/address/components/Address';
 import { ZERO_ADDRESS } from 'modules/web3/constants/addresses';
 import { useExecutiveComments } from 'modules/comments/hooks/useExecutiveComments';
 import ExecutiveComments from 'modules/comments/components/ExecutiveComments';
+import { useActiveWeb3React } from 'modules/web3/hooks/useActiveWeb3React';
+import { chainIdToNetworkName } from 'modules/web3/helpers/chain';
 
 type Props = {
   proposal: Proposal;
@@ -94,9 +97,10 @@ const ProposalView = ({ proposal }: Props): JSX.Element => {
 
   const account = useAccountsStore(state => state.currentAccount);
   const bpi = useBreakpointIndex();
+  const { chainId } = useActiveWeb3React();
 
   const { data: allSupporters, error: supportersError } = useSWR(
-    `/api/executive/supporters?network=${getNetwork()}`
+    `/api/executive/supporters?network=${chainIdToNetworkName(chainId)}`
   );
 
   const { data: votedProposals } = useVotedProposals();
@@ -398,10 +402,12 @@ export default function ProposalPage({ proposal: prefetchedProposal }: { proposa
   const [_proposal, _setProposal] = useState<Proposal>();
   const [error, setError] = useState<string>();
   const { query, isFallback } = useRouter();
+  const { chainId } = useActiveWeb3React();
 
   // fetch proposal contents at run-time if on any network other than the default
   useEffect(() => {
-    if (!isDefaultNetwork() && query['proposal-id']) {
+    if (!chainId) return;
+    if (!isDefaultNetwork(chainId) && query['proposal-id']) {
       getExecutiveProposal(query['proposal-id'] as string)
         .then(proposal => {
           if (proposal) {
@@ -412,9 +418,9 @@ export default function ProposalPage({ proposal: prefetchedProposal }: { proposa
         })
         .catch(setError);
     }
-  }, [query['proposal-id']]);
+  }, [query['proposal-id'], chainId]);
 
-  if (error || (isDefaultNetwork() && !isFallback && !prefetchedProposal?.key)) {
+  if (error || (isDefaultNetwork(chainId) && !isFallback && !prefetchedProposal?.key)) {
     return (
       <ErrorPage
         statusCode={404}
@@ -423,14 +429,14 @@ export default function ProposalPage({ proposal: prefetchedProposal }: { proposa
     );
   }
 
-  if (isFallback || (!isDefaultNetwork() && !_proposal))
+  if (isFallback || (!isDefaultNetwork(chainId) && !_proposal))
     return (
       <PrimaryLayout shortenFooter={true}>
         <p>Loading…</p>
       </PrimaryLayout>
     );
 
-  const proposal = isDefaultNetwork() ? prefetchedProposal : _proposal;
+  const proposal = isDefaultNetwork(chainId) ? prefetchedProposal : _proposal;
   return <ProposalView proposal={proposal as Proposal} />;
 }
 
