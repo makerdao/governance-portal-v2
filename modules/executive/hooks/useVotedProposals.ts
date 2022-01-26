4f56cf0d3f86c46c68c77cf733f9f03adfac21a7
Phil Bain <bain.pmcg@gmail.com>
2022-01-26 15:54:28 -0700

    update useVotedProposals

--- modules/executive/hooks/useVotedProposals.ts
+++ modules/executive/hooks/useVotedProposals.ts
@@ -1,10 +1,11 @@
-import getMaker from 'lib/maker';
 import { ZERO_SLATE_HASH } from 'modules/executive/helpers/zeroSlateHash';
 import useSWR from 'swr';
 import { useAccount } from 'modules/app/hooks/useAccount';
+import { useContracts } from 'modules/web3/hooks/useContracts';
+import { getSlateAddresses } from '../helpers/getSlateAddresses';
 
 type VotedProposalsResponse = {
-  data: any;
+  data: string[] | undefined;
   loading: boolean;
   error: Error;
   mutate: any;
@@ -12,6 +13,7 @@ type VotedProposalsResponse = {
 
 export const useVotedProposals = (passedAddress?: string): VotedProposalsResponse => {
   let addressToUse;
+  const { chief } = useContracts();
 
   // if address is passed, fetch for that
   if (passedAddress) {
@@ -28,14 +30,11 @@ export const useVotedProposals = (passedAddress?: string): VotedProposalsRespons
   }
 
   const { data, error, mutate } = useSWR<string[]>(
-    addressToUse ? ['/executive/voted-proposals', addressToUse] : null,
-    (_, address) =>
-      getMaker().then(maker =>
-        maker
-          .service('chief')
-          .getVotedSlate(address)
-          .then(slate => (slate !== ZERO_SLATE_HASH ? maker.service('chief').getSlateAddresses(slate) : []))
-      ),
+    addressToUse ? `${addressToUse}/executive/voted-proposals` : null,
+    async () => {
+      const votedSlate = await chief.votes(addressToUse);
+      return votedSlate !== ZERO_SLATE_HASH ? await getSlateAddresses(chief, votedSlate) : [];
+    },
     { revalidateOnMount: true, refreshInterval: 60000, revalidateOnFocus: false }
   );
 
