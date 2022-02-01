96a289b688155815d51560533caf5f0f5b978976
Phil Bain <bain.pmcg@gmail.com>
2022-02-01 15:54:26 -0700

    move polling create into hook

---
+++ modules/polling/hooks/usePollCreate.ts
@@ -0,0 +1,56 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+import useTransactionStore, {
+  transactionsSelectors,
+  transactionsApi
+} from 'modules/web3/stores/transactions';
+import { shallow } from 'zustand/shallow';
+import { Transaction } from 'modules/web3/types/transaction';
+import { useContracts } from 'modules/web3/hooks/useContracts';
+import { useAccount } from 'modules/app/hooks/useAccount';
+
+type CreateResponse = {
+  txId: string | null;
+  setTxId: Dispatch<SetStateAction<string | null>>;
+  createPoll: (
+    startDate: number,
+    endDate: number,
+    multiHash: string,
+    url: string,
+    callbacks?: Record<string, (id?: string) => void>
+  ) => void;
+  tx: Transaction | null;
+};
+
+export const usePollCreate = (): CreateResponse => {
+  const [txId, setTxId] = useState<string | null>(null);
+
+  const { account } = useAccount();
+  const { polling } = useContracts();
+
+  const [track, tx] = useTransactionStore(
+    state => [state.track, txId ? transactionsSelectors.getTransaction(state, txId) : null],
+    shallow
+  );
+
+  const createPoll = (startDate, endDate, multiHash, url, callbacks) => {
+    const createTxCreator = () => polling.createPoll(startDate, endDate, multiHash, url);
+    const txId = track(createTxCreator, account, 'Creating poll', {
+      initialized: () => {
+        if (typeof callbacks?.initialized === 'function') callbacks.initialized();
+      },
+      pending: () => {
+        if (typeof callbacks?.pending === 'function') callbacks.pending();
+      },
+      mined: txId => {
+        transactionsApi.getState().setMessage(txId, 'Created poll');
+        if (typeof callbacks?.mined === 'function') callbacks.mined();
+      },
+      error: () => {
+        if (typeof callbacks?.error === 'function') callbacks.error();
+      }
+    });
+    setTxId(txId);
+  };
+
+  return { txId, setTxId, createPoll, tx };
+};
