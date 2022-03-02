a4e32127e4356845d538c3c2782399d55be9cbe6
Phil Bain <bain.pmcg@gmail.com>
2022-03-02 12:44:43 -0700

    pin block in remaining tests for consistency

--- cypress/integration/account.spec.ts
+++ cypress/integration/account.spec.ts
@@ -3,10 +3,15 @@
 // If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
 // https://github.com/cypress-io/eslint-plugin-cypress
 
+import { INIT_BLOCK } from 'cypress/support/constants/blockNumbers';
 import { getTestAccountByIndex } from 'cypress/support/constants/testaccounts';
-import { setAccount, visitPage } from '../support/commons';
+import { forkNetwork, setAccount, visitPage } from '../support/commons';
 
 describe('Account Page', async () => {
+  before(() => {
+    forkNetwork(INIT_BLOCK);
+  });
+
   it('should navigate to the account page and be able to create a delegate contract', () => {
     visitPage('/account');
 
