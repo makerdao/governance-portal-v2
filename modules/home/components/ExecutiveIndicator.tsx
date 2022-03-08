52913ee7ea454fe6ca1841bbab5b20059dc1cf82
Venture <rafinskipg@gmail.com>
2022-03-08 15:40:20 +0100

    Adding pagination  for execeutives (#369)
    * Adding pagination
    
    * Added pagination loading on executives page
    
    * sort by active
    
    * fix delegates
    
    * Update loading state
    
    Co-authored-by: Adam Goth <adamgoth@gmail.com>

--- modules/home/components/ExecutiveIndicator.tsx
+++ modules/home/components/ExecutiveIndicator.tsx
@@ -58,7 +58,7 @@ const ExecutiveIndicatorComponent = ({
 
   const { account } = useAccount();
   const { data: votedProposals } = useVotedProposals();
-  
+
   const newUnvotedProposals =
     votedProposals && account
       ? unscheduledProposals.filter(
