6af3c39880644355891c524da53b5f98f92baac5
Adam Goth <adamgoth@gmail.com>
2022-03-16 13:56:53 +0100

    Linter

--- modules/home/components/GovernanceStats.tsx
+++ modules/home/components/GovernanceStats.tsx
@@ -13,27 +13,33 @@ export function GovernanceStats(): JSX.Element {
   const infoUnits = [
     {
       title: 'MKR on Hat',
-      value: true ? '76,234 MKR' : <Skeleton />
+      // value: true ? '76,234 MKR' : <Skeleton />
+      value: '76,234 MKR'
     },
     {
       title: 'Active Proposals',
-      value: true ? `9` : <Skeleton />
+      // value: true ? `9` : <Skeleton />
+      value: '9'
     },
     {
       title: 'Recognized Delegates',
-      value: true ? '17' : <Skeleton />
+      // value: true ? '17' : <Skeleton />
+      value: '17'
     },
     {
       title: 'Shadow Delegates',
-      value: true ? '9' : <Skeleton />
+      // value: true ? '9' : <Skeleton />
+      value: '9'
     },
     {
       title: 'MKR Delegated',
-      value: true ? '89,234' : <Skeleton />
+      // value: true ? '89,234' : <Skeleton />
+      value: '89,234'
     },
     {
       title: 'MKR in Chief',
-      value: true ? '132,023 MKR' : <Skeleton />
+      // value: true ? '132,023 MKR' : <Skeleton />
+      value: '132,023 MKR'
     }
   ];
 
