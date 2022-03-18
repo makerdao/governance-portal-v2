07ddb7a2ebdc7e073ec882661064d50a0521e352
Adam Goth <adamgoth@gmail.com>
2022-03-18 10:59:54 +0100

    Give tags onClick, put links on homepage

--- modules/home/components/PollCategoriesLanding.tsx
+++ modules/home/components/PollCategoriesLanding.tsx
@@ -1,5 +1,6 @@
-import { Flex, Box, Heading } from 'theme-ui';
+import { Flex, Box, Heading, Link as ThemeUILink } from 'theme-ui';
 import { PollCategoryTag } from 'modules/polling/components/PollCategoryTag';
+import Link from 'next/link';
 import { PollCategory } from 'modules/polling/types';
 
 type Props = {
@@ -12,8 +13,11 @@ export const PollCategoriesLanding = ({ pollCategories }: Props): JSX.Element =>
     <Flex sx={{ flexWrap: 'wrap', justifyContent: 'center', maxWidth: '1000px', margin: 'auto' }}>
       {pollCategories.map(category => (
         <Box key={category.name} sx={{ my: 3, mx: 4 }}>
-          {/* TODO make these clickable links */}
-          <PollCategoryTag category={category.name} clickable={false} />
+          <Link href={{ pathname: '/polling', query: { category: category.name } }} passHref>
+            <ThemeUILink title={`${category.name} polls`}>
+              <PollCategoryTag category={category.name} />
+            </ThemeUILink>
+          </Link>
         </Box>
       ))}
     </Flex>
