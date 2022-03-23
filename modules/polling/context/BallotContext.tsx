99b7662afb8a517215280dc762732eea23fa2137
Phil Bain <bain.pmcg@gmail.com>
2022-03-23 16:56:47 -0600

    very WIP POC of using arweave to submit votes

--- modules/polling/context/BallotContext.tsx
+++ modules/polling/context/BallotContext.tsx
@@ -1,3 +1,4 @@
+import { ethers } from 'ethers';
 import { fetchJson } from 'lib/fetchJson';
 import { useAccount } from 'modules/app/hooks/useAccount';
 import { PollComment, PollsCommentsRequestBody } from 'modules/comments/types/pollComments';
@@ -13,6 +14,11 @@ import React, { ReactNode, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import shallow from 'zustand/shallow';
 import { Ballot, BallotVote } from '../types/ballot';
+import Arweave from 'arweave';
+import JSONWebKey from 'json-web-key';
+import myKey from '../../../arweave-key-5cRlmK5gd0h1G7XVeFpS2yyGdxUgRg5V_NXgdy-jWhY.json';
+import { bundleAndSignData, createData } from 'arbundles';
+import { ArweaveSigner } from 'arbundles/src/signing';
 
 interface ContextProps {
   ballot: Ballot;
@@ -156,7 +162,7 @@ export const BallotProvider = ({ children }: PropTypes): React.ReactElement => {
   const { account, voteDelegateContract } = useAccount();
   const { polling } = useContracts();
 
-  const submitBallot = () => {
+  const submitBallot = async () => {
     const pollIds: string[] = [];
     const pollOptions: string[] = [];
 
@@ -167,48 +173,140 @@ export const BallotProvider = ({ children }: PropTypes): React.ReactElement => {
       }
     });
 
-    const voteTxCreator = voteDelegateContract
-      ? () => voteDelegateContract['votePoll(uint256[],uint256[])'](pollIds, pollOptions)
-      : () => polling['vote(uint256[],uint256[])'](pollIds, pollOptions);
+    if (!account) {
+      return;
+    }
 
-    const txId = track(voteTxCreator, account, `Voting on ${Object.keys(ballot).length} polls`, {
-      pending: txHash => {
-        // if comment included, add to comments db
-        if (getComments().length > 0) {
-          const commentsRequest: PollsCommentsRequestBody = {
-            voterAddress: account?.toLowerCase() || '',
-            comments: getComments(),
-            signedMessage: commentsSignature,
-            txHash
-          };
+    // useEffect(() => {
+    //   (async () => {
+    //     // const data = await fetchJson('/api/comments/nonce', {
+    //     //   method: 'POST',
+    //     //   body: JSON.stringify({
+    //     //     voterAddress: account
+    //     //   })
+    //     // });
+    //     // console.log('nonce', data.nonce);
 
-          fetchJson(`/api/comments/polling/add?network=${network}`, {
-            method: 'POST',
-            body: JSON.stringify(commentsRequest)
-          })
-            .then(() => {
-              // console.log('comment successfully added');
-            })
-            .catch(() => {
-              console.error('failed to add comment');
-              toast.error('Unable to store comments');
-            });
-        }
-      },
-      mined: txId => {
-        // Set votes
-        setPreviousBallot({
-          ...previousBallot,
-          ...ballot
-        });
-        clearBallot();
-        transactionsApi.getState().setMessage(txId, `Voted on ${Object.keys(ballot).length} polls`);
-      },
-      error: () => {
-        toast.error('Error submitting ballot');
-      }
+    //   })();
+    // }, []);
+
+    const uploadVote = false;
+    let arTxId;
+
+    const signedMessage = await sign(account, JSON.stringify({ pollIds, pollOptions }), library);
+
+    console.log('pollIds options', pollIds, pollOptions);
+    console.log('pollIds options stringify', JSON.stringify({ pollIds, pollOptions }));
+
+    console.log('signed msg', signedMessage);
+
+    const pollString = JSON.stringify({ pollIds, pollOptions });
+    const v = ethers.utils.verifyMessage(pollString, signedMessage);
+    console.log('verify message', v);
+
+    // const key = JSONWebKey.fromJSON(myKey);
+    // console.log('jwk', key);
+
+    const arweave = Arweave.init({
+      host: 'arweave.net',
+      port: 443,
+      protocol: 'https'
     });
-    setTxId(txId);
+
+    const arAddress = await arweave.wallets.getAddress(myKey);
+    console.log('my address', arAddress);
+
+    if (uploadVote) {
+      // const signer = new ArweaveSigner(myKey);
+      // const tags = [{ name: 'polling', value: 'makerpollingvalue' }];
+      // const dataItem = [createData(pollString, signer)];
+
+      // const myBundle = await bundleAndSignData(dataItem, signer);
+      // const txA = await myBundle.toTransaction({}, arweave, myKey);
+
+      const txA = await arweave.createTransaction(
+        {
+          data: JSON.stringify({ pollIds, pollOptions, account, signedMessage })
+        },
+        myKey
+      );
+      txA.addTag('Content-Type', 'text/html');
+      txA.addTag('polling', 'makerdux');
+
+      // console.log('tx before sign', txA);
+      await arweave.transactions.sign(txA, myKey);
+
+      console.log('tx after sign', txA);
+      arTxId = txA.id;
+
+      const response = await arweave.transactions.post(txA);
+
+      console.log('sent tx response', response);
+
+      console.log(response.status);
+    }
+
+    // // const mytx = 'q45_e4PmYafAynudOP9XAvTPuRQmkusYnacimmEbDZw';
+    const txAId = 'UGNrbK27ai-ZzA3OcQ4DLO6DshNJ9vO_WlNEqG_YeRI';
+    const status = await arweave.transactions.getStatus(txAId);
+    console.log('tx status', status);
+
+    const ddd = (await arweave.transactions.getData(txAId, { decode: true, string: true })) as string;
+    console.log('string data', ddd);
+    const returnedVoteDO = JSON.parse(ddd);
+    console.log('ddd', returnedVoteDO);
+    const pollString2 = JSON.stringify({
+      pollIds: returnedVoteDO.pollIds,
+      pollOptions: returnedVoteDO.pollOptions
+    });
+
+    const ve2 = ethers.utils.verifyMessage(pollString2, returnedVoteDO.signedMessage);
+    console.log('Returned message matches user?', ve2 === account);
+
+    // await arweave.transactions.sign(transaction, key);
+
+    // const voteTxCreator = voteDelegateContract
+    //   ? () => voteDelegateContract['votePoll(uint256[],uint256[])'](pollIds, pollOptions)
+    //   : () => polling['vote(uint256[],uint256[])'](pollIds, pollOptions);
+
+    // const txId = track(voteTxCreator, account, `Voting on ${Object.keys(ballot).length} polls`, {
+    //   pending: txHash => {
+    //     // if comment included, add to comments db
+    //     if (getComments().length > 0) {
+    //       const commentsRequest: PollsCommentsRequestBody = {
+    //         voterAddress: account?.toLowerCase() || '',
+    //         comments: getComments(),
+    //         signedMessage: commentsSignature,
+    //         txHash
+    //       };
+
+    //       fetchJson(`/api/comments/polling/add?network=${network}`, {
+    //         method: 'POST',
+    //         body: JSON.stringify(commentsRequest)
+    //       })
+    //         .then(() => {
+    //           // console.log('comment successfully added');
+    //         })
+    //         .catch(() => {
+    //           console.error('failed to add comment');
+    //           toast.error('Unable to store comments');
+    //         });
+    //     }
+    //   },
+    //   mined: txId => {
+    //     // Set votes
+    //     setPreviousBallot({
+    //       ...previousBallot,
+    //       ...ballot
+    //     });
+    //     clearBallot();
+    //     transactionsApi.getState().setMessage(txId, `Voted on ${Object.keys(ballot).length} polls`);
+    //   },
+    //   error: () => {
+    //     toast.error('Error submitting ballot');
+    //   }
+    // });
+    // setTxId(txId);
   };
 
   return (
