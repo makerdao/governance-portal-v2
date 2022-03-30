4c4c38e2292b931420ea41f5cf16a02e86c36497
Phil Bain <bain.pmcg@gmail.com>
2022-03-30 09:03:34 -0600

    Spell Effects 2 (#374)
    * copy over files
    
    * fix build
    
    * update spell effects with working URL, improve parsers and design
    
    * clean up spell diffs page, fix simulation endpoint
    
    * add loading and error handling to spell effects
    
    * estimate block to execute using nextCastTime of spell
    
    * Reuse server side data and avoid doing extra request
    
    * speed up build by only pre-fetching spells <2 months old
    
    * remove blocknumber estimate and use timestamp to simulate spells
    
    * fix types, unique key issue
    
    Co-authored-by: Rafael Ventura <rafinskipg@gmail.com>

--- pages/api/executive/state-diff/[address].ts
+++ pages/api/executive/state-diff/[address].ts
@@ -1,104 +1,27 @@
 import invariant from 'tiny-invariant';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { ethers } from 'ethers';
-import { fetchJson } from 'lib/fetchJson';
-import { config } from 'lib/config';
-import { ETH_TX_STATE_DIFF_ENDPOINT, SupportedNetworks } from 'modules/web3/constants/networks';
-import { getTrace } from 'modules/web3/helpers/getTrace';
+import { SupportedNetworks } from 'modules/web3/constants/networks';
 import withApiHandler from 'modules/app/api/withApiHandler';
-import { isSupportedNetwork } from 'modules/web3/helpers/networks';
-import { DEFAULT_NETWORK } from 'modules/web3/constants/networks';
-import { networkNameToChainId } from 'modules/web3/helpers/chain';
-import { getContracts } from 'modules/web3/helpers/getContracts';
-import { getDefaultProvider } from 'modules/web3/helpers/getDefaultProvider';
+import { fetchSimulationSpellDiffs } from 'modules/executive/api/fetchSimulationSpellDiff';
+import { fetchHistoricalSpellDiff } from 'modules/executive/api/fetchHistoricalSpellDiff';
 
 export default withApiHandler(async (req: NextApiRequest, res: NextApiResponse) => {
   const spellAddress: string = req.query.address as string;
+  const nextCastTime: string = req.query.nextCastTime as string;
+  const hasBeenCast: string = req.query.hasBeenCast as string;
+
   invariant(spellAddress && ethers.utils.isAddress(spellAddress), 'valid spell address required');
 
-  const network = (req.query.network as string) || DEFAULT_NETWORK.network;
-  invariant(isSupportedNetwork(network), `unsupported network ${network}`);
+  invariant(
+    !req.query.network || req.query.network === SupportedNetworks.MAINNET,
+    `unsupported network ${req.query.network}`
+  );
 
-  const chainId = networkNameToChainId(network);
-  const contracts = getContracts(chainId);
-
-  const pauseAddress = contracts['pause'].address;
-  const pauseProxyAddress = contracts['pauseProxy'].address;
-
-  const provider = getDefaultProvider(network);
-
-  console.log(config.INFURA_KEY, config.ALCHEMY_KEY, 'infura and alchemy keys state diff');
-  const encoder = new ethers.utils.Interface([
-    'function sig() returns (bytes)',
-    'function action() returns (address)',
-    'function done() returns (bool)',
-    'function exec(address, bytes)',
-    'function actions()'
-  ]);
-
-  async function ethCall(method, to = spellAddress) {
-    const calldata = {
-      to,
-      data: encoder.encodeFunctionData(method)
-    };
-
-    return encoder.decodeFunctionResult(method, await provider.call(calldata));
-  }
-
-  const [hasBeenCast] = await ethCall('done');
-  const [usr] = await ethCall('action');
-
-  let trace;
-  let executedOn: number | null = null;
-
-  if (hasBeenCast) {
-    const pauseExecSelector = `${ethers.utils
-      .id('exec(address,bytes32,bytes,uint256)')
-      .slice(0, 10)}${Array.from({ length: 56 })
-      .map(() => '0')
-      .join('')}`;
-
-    const spellAddressBytes32 = `0x${Array.from({ length: 24 })
-      .map(() => '0')
-      .join('')}${spellAddress.replace('0x', '')}`;
-
-    const usrBytes32 = `0x${Array.from({ length: 24 })
-      .map(() => '0')
-      .join('')}${usr.replace('0x', '')}`;
-
-    const [{ transactionHash, blockNumber }] = await new ethers.providers.InfuraProvider(
-      network,
-      config.INFURA_KEY
-    ).getLogs({
-      address: pauseAddress,
-      fromBlock: 0,
-      toBlock: 'latest',
-      topics: [pauseExecSelector, spellAddressBytes32, usrBytes32]
-    });
-
-    invariant(transactionHash, `Unable to find cast transaction for spell ${spellAddress}`);
-    trace = await getTrace('trace_replayTransaction', transactionHash, network);
-    executedOn = blockNumber;
-  } else {
-    trace = await getTrace(
-      'trace_call',
-      {
-        from: pauseAddress,
-        to: pauseProxyAddress,
-        data: encoder.encodeFunctionData('exec', [usr, encoder.encodeFunctionData('actions')])
-      },
-      network
-    );
-  }
-
-  console.log(trace, 'trace');
-  invariant(trace, `Unable to fetch trace for spell ${spellAddress}`);
-  const decodedDiff = await fetchJson(ETH_TX_STATE_DIFF_ENDPOINT(SupportedNetworks.MAINNET), {
-    method: 'POST',
-    body: JSON.stringify({ trace })
-  });
-  console.log(decodedDiff, 'decodedDiff');
+  const diffs = hasBeenCast
+    ? await fetchHistoricalSpellDiff(spellAddress)
+    : await fetchSimulationSpellDiffs(spellAddress, parseInt(nextCastTime));
 
   res.setHeader('Cache-Control', 's-maxage=600, stale-while-revalidate');
-  res.status(200).json({ hasBeenCast, executedOn, decodedDiff });
+  res.status(200).json(diffs);
 });
