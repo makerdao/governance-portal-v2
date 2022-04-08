6157385c0e41d63d61a3586c7751b41718a80009
Adam Goth <adamgoth@gmail.com>
2022-04-08 14:53:20 +0200

    Update resources, add images, icons

--- modules/home/components/ResourcesLanding.tsx
+++ modules/home/components/ResourcesLanding.tsx
@@ -1,5 +1,6 @@
-import { Flex, Heading, Card, Grid, Text, Image } from 'theme-ui';
+import { Box, Flex, Heading, Card, Text, Image } from 'theme-ui';
 import { resources } from 'modules/home/helpers/resources';
+import { ExternalLink } from 'modules/app/components/ExternalLink';
 
 export const ResourcesLanding = (): JSX.Element => {
   return (
@@ -9,20 +10,65 @@ export const ResourcesLanding = (): JSX.Element => {
         <Flex>Button thing here</Flex>
       </Flex>
       <Flex sx={{ justifyContent: 'center' }}>
-        <Grid gap={4} columns={[1, 1, 2, 3]}>
+        <Flex sx={{ flexWrap: 'wrap', justifyContent: ['center', 'space-evenly', 'space-between'] }}>
           {resources.map(resource => (
-            <Card key={resource.title} sx={{ width: '405px', height: '300px' }}>
-              <Text>{resource.title}</Text>
-              <Image
-                src={resource.bg}
-                sx={{
-                  objectFit: 'cover',
-                  width: '100%'
-                }}
-              />
-            </Card>
+            <ExternalLink href={resource.url} title={resource.title} key={resource.title}>
+              <Card key={resource.title} sx={{ width: '380px', height: '300px', mb: 4 }}>
+                <Box sx={{ position: 'relative' }}>
+                  <Box sx={{ position: 'absolute', width: '100%' }}>
+                    <Flex sx={{ width: '100%', justifyContent: 'space-between', p: 3 }}>
+                      <Flex sx={{ flexDirection: 'column', width: '60%' }}>
+                        <Flex>
+                          <Box
+                            sx={{
+                              backgroundColor: 'background',
+                              borderRadius: '12px',
+                              padding: '4px 8px',
+                              display: 'flex',
+                              alignItems: 'center'
+                            }}
+                          >
+                            <Text sx={{ color: resource.color, fontSize: '10px' }}>{resource.tag}</Text>
+                          </Box>
+                        </Flex>
+                        <Text
+                          sx={{
+                            mt: 2,
+                            fontSize: '28px',
+                            color: 'background',
+                            fontWeight: 'semiBold',
+                            lineHeight: '32px'
+                          }}
+                        >
+                          {resource.title}
+                        </Text>
+                      </Flex>
+                      <Flex
+                        sx={{
+                          width: '40%',
+                          justifyContent: 'flex-end',
+                          alignItems: 'flex-start',
+                          mr: 3,
+                          mt: 3
+                        }}
+                      >
+                        <Image src={resource.logo} sx={{ width: '64px' }} />
+                      </Flex>
+                    </Flex>
+                  </Box>
+                  <Image
+                    src={resource.bg}
+                    sx={{
+                      objectFit: 'cover',
+                      width: '100%'
+                    }}
+                  />
+                </Box>
+                <Text sx={{ fontSize: 2, color: 'secondaryEmphasis' }}>{resource.summary}</Text>
+              </Card>
+            </ExternalLink>
           ))}
-        </Grid>
+        </Flex>
       </Flex>
     </Flex>
   );
--- modules/home/helpers/resources.ts
+++ modules/home/helpers/resources.ts
@@ -4,11 +4,24 @@ enum ResourceCategory {
   DEVELOPERS = 'Developers'
 }
 
+enum ResourceBackground {
+  GOVERNANCE = '/assets/resource_card_green.png',
+  PRODUCTS_AND_TOOLS = '/assets/resource_card_purple.png',
+  DEVELOPERS = '/assets/resource_card_pink.png'
+}
+
+enum ResourceColor {
+  GOVERNANCE = '#1ACCA7',
+  PRODUCTS_AND_TOOLS = '#4B68FF',
+  DEVELOPERS = '#E64BFF'
+}
+
 type LandingResource = {
   title: string;
   url: string;
   tag: ResourceCategory;
   bg: string;
+  color: string;
   logo: string;
   summary: string;
 };
@@ -18,8 +31,9 @@ export const resources: LandingResource[] = [
     title: 'Maker Governance Forum',
     url: 'https://forum.makerdao.com/',
     tag: ResourceCategory.GOVERNANCE,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
+    bg: ResourceBackground.GOVERNANCE,
+    color: ResourceColor.GOVERNANCE,
+    logo: '/assets/resource_icon_1.svg',
     summary:
       'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
   },
@@ -27,62 +41,69 @@ export const resources: LandingResource[] = [
     title: 'Maker Operation Manual',
     url: 'https://manual.makerdao.com',
     tag: ResourceCategory.GOVERNANCE,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
+    bg: ResourceBackground.GOVERNANCE,
+    color: ResourceColor.GOVERNANCE,
+    logo: '/assets/resource_icon_1.svg',
     summary:
       'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
   },
-  {
-    title: 'Governance FAQ',
-    url: 'https://makerdao.world/en/learn/governance/governance-faq',
-    tag: ResourceCategory.GOVERNANCE,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
-    summary:
-      'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
-  },
+  // {
+  //   title: 'Governance FAQ',
+  //   url: 'https://makerdao.world/en/learn/governance/governance-faq',
+  //   tag: ResourceCategory.GOVERNANCE,
+  //   bg: ResourceBackground.GOVERNANCE,
+  //   color: ResourceColor.GOVERNANCE,
+  //   logo: '/assets/resource_icon_1.svg',
+  //   summary:
+  //     'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
+  // },
   {
     title: 'Governance Tracking Sheet',
     url: 'https://docs.google.com/spreadsheets/d/1LWNlv6hr8oXebk8rvXZBPRVDjN-3OrzI0IgLwBVk0vM/edit#gid=0',
     tag: ResourceCategory.GOVERNANCE,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
+    bg: ResourceBackground.GOVERNANCE,
+    color: ResourceColor.GOVERNANCE,
+    logo: '/assets/resource_icon_1.svg',
     summary:
       'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
   },
-  {
-    title: 'Monthly Governance Cycle',
-    url: 'https://manual.makerdao.com/governance/governance-cycle/monthly-governance-cycle',
-    tag: ResourceCategory.GOVERNANCE,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
-    summary:
-      'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
-  },
-  {
-    title: 'Weekly Governance Cycle',
-    url: 'https://manual.makerdao.com/governance/governance-cycle/weekly-governance-cycle',
-    tag: ResourceCategory.GOVERNANCE,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
-    summary:
-      'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
-  },
-  {
-    title: 'Service Status',
-    url: 'https://makerdao.statuspage.io/',
-    tag: ResourceCategory.PRODUCTS_AND_TOOLS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
-    summary:
-      'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
-  },
+  // {
+  //   title: 'Monthly Governance Cycle',
+  //   url: 'https://manual.makerdao.com/governance/governance-cycle/monthly-governance-cycle',
+  //   tag: ResourceCategory.GOVERNANCE,
+  //   bg: ResourceBackground.GOVERNANCE,
+  //   color: ResourceColor.GOVERNANCE,
+  //   logo: '/assets/resource_icon_1.svg',
+  //   summary:
+  //     'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
+  // },
+  // {
+  //   title: 'Weekly Governance Cycle',
+  //   url: 'https://manual.makerdao.com/governance/governance-cycle/weekly-governance-cycle',
+  //   tag: ResourceCategory.GOVERNANCE,
+  //   bg: ResourceBackground.GOVERNANCE,
+  //   color: ResourceColor.GOVERNANCE,
+  //   logo: '/assets/resource_icon_1.svg',
+  //   summary:
+  //     'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
+  // },
+  // {
+  //   title: 'Service Status',
+  //   url: 'https://makerdao.statuspage.io/',
+  //   tag: ResourceCategory.PRODUCTS_AND_TOOLS,
+  //   bg: ResourceBackground.PRODUCTS_AND_TOOLS,
+  //   color: ResourceColor.PRODUCTS_AND_TOOLS,
+  //   logo: '/assets/resource_icon_1.svg',
+  //   summary:
+  //     'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
+  // },
   {
     title: 'Oasis',
     url: 'https://oasis.app/',
     tag: ResourceCategory.PRODUCTS_AND_TOOLS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
+    bg: ResourceBackground.PRODUCTS_AND_TOOLS,
+    color: ResourceColor.PRODUCTS_AND_TOOLS,
+    logo: '/assets/resource_icon_1.svg',
     summary:
       'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
   },
@@ -90,44 +111,59 @@ export const resources: LandingResource[] = [
     title: 'Auctions Dashboard',
     url: 'http://auctions.makerdao.network',
     tag: ResourceCategory.PRODUCTS_AND_TOOLS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
+    bg: ResourceBackground.PRODUCTS_AND_TOOLS,
+    color: ResourceColor.PRODUCTS_AND_TOOLS,
+    logo: '/assets/resource_icon_1.svg',
     summary:
       'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
   },
+  // {
+  //   title: 'Migrate Dashboard',
+  //   url: 'http://migrate.makerdao.com',
+  //   tag: ResourceCategory.PRODUCTS_AND_TOOLS,
+  //   bg: ResourceBackground.PRODUCTS_AND_TOOLS,
+  //   color: ResourceColor.PRODUCTS_AND_TOOLS,
+  //   logo: '/assets/resource_icon_1.svg',
+  //   summary:
+  //     'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
+  // },
   {
     title: 'MakerBurn',
     url: 'https://makerburn.com/#/',
     tag: ResourceCategory.PRODUCTS_AND_TOOLS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
+    bg: ResourceBackground.PRODUCTS_AND_TOOLS,
+    color: ResourceColor.PRODUCTS_AND_TOOLS,
+    logo: '/assets/resource_icon_1.svg',
     summary:
       'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
   },
-  {
-    title: 'Dai Stats',
-    url: 'https://daistats.com/',
-    tag: ResourceCategory.PRODUCTS_AND_TOOLS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
-    summary:
-      'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
-  },
-  {
-    title: 'Whitepaper',
-    url: 'https://makerdao.com/en/whitepaper/',
-    tag: ResourceCategory.DEVELOPERS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
-    summary:
-      'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
-  },
+  // {
+  //   title: 'Dai Stats',
+  //   url: 'https://daistats.com/',
+  //   tag: ResourceCategory.PRODUCTS_AND_TOOLS,
+  //   bg: ResourceBackground.PRODUCTS_AND_TOOLS,
+  //   color: ResourceColor.PRODUCTS_AND_TOOLS,
+  //   logo: '/assets/resource_icon_1.svg',
+  //   summary:
+  //     'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
+  // },
+  // {
+  //   title: 'Whitepaper',
+  //   url: 'https://makerdao.com/en/whitepaper/',
+  //   tag: ResourceCategory.DEVELOPERS,
+  //   bg: ResourceBackground.DEVELOPERS,
+  //   color: ResourceColor.DEVELOPERS,
+  //   logo: '/assets/resource_icon_1.svg',
+  //   summary:
+  //     'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
+  // },
   {
     title: 'Technical Docs',
     url: 'https://docs.makerdao.com/',
     tag: ResourceCategory.DEVELOPERS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
+    bg: ResourceBackground.DEVELOPERS,
+    color: ResourceColor.DEVELOPERS,
+    logo: '/assets/resource_icon_1.svg',
     summary:
       'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
   },
@@ -135,44 +171,49 @@ export const resources: LandingResource[] = [
     title: 'API Docs',
     url: 'https://vote.makerdao.com/api-docs',
     tag: ResourceCategory.DEVELOPERS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
+    bg: ResourceBackground.DEVELOPERS,
+    color: ResourceColor.DEVELOPERS,
+    logo: '/assets/resource_icon_1.svg',
     summary:
       'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
   },
-  {
-    title: 'Developer Guides and Tutorials',
-    url: 'https://github.com/makerdao/developerguides',
-    tag: ResourceCategory.DEVELOPERS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
-    summary:
-      'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
-  },
-  {
-    title: 'Brand Assets',
-    url: '/ac517c82ff9a43089d0db5bb2ee045a4',
-    tag: ResourceCategory.DEVELOPERS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
-    summary:
-      'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
-  },
-  {
-    title: 'Oracle Feeds',
-    url: 'https://makerdao.com/en/feeds/',
-    tag: ResourceCategory.DEVELOPERS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
-    summary:
-      'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
-  },
+  // {
+  //   title: 'Developer Guides and Tutorials',
+  //   url: 'https://github.com/makerdao/developerguides',
+  //   tag: ResourceCategory.DEVELOPERS,
+  //   bg: ResourceBackground.DEVELOPERS,
+  //   color: ResourceColor.DEVELOPERS,
+  //   logo: '/assets/resource_icon_1.svg',
+  //   summary:
+  //     'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
+  // },
+  // {
+  //   title: 'Brand Assets',
+  //   url: '/ac517c82ff9a43089d0db5bb2ee045a4',
+  //   tag: ResourceCategory.DEVELOPERS,
+  //   bg: ResourceBackground.DEVELOPERS,
+  //   color: ResourceColor.DEVELOPERS,
+  //   logo: '/assets/resource_icon_1.svg',
+  //   summary:
+  //     'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
+  // },
+  // {
+  //   title: 'Oracle Feeds',
+  //   url: 'https://makerdao.com/en/feeds/',
+  //   tag: ResourceCategory.DEVELOPERS,
+  //   bg: ResourceBackground.DEVELOPERS,
+  //   color: ResourceColor.DEVELOPERS,
+  //   logo: '/assets/resource_icon_1.svg',
+  //   summary:
+  //     'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
+  // },
   {
     title: 'MakerDAO GitHub',
     url: 'https://github.com/makerdao/',
     tag: ResourceCategory.DEVELOPERS,
-    bg: '/assets/gov-cycle-bg.png',
-    logo: 'resourcesGovernance',
+    bg: ResourceBackground.DEVELOPERS,
+    color: ResourceColor.DEVELOPERS,
+    logo: '/assets/resource_icon_1.svg',
     summary:
       'The Monthly Governance Cycle is defined in MIP51 and provides a predictable monthly cadence by which governance decisions are made.'
   }
