b86973f1fb087b99ce9d44c0bb0e32429a57b4e5
Adam Goth <adamgoth@gmail.com>
2022-04-12 06:32:39 +0200

    Cache parsed tally

--- modules/polling/api/fetchRawPollTally.ts
+++ modules/polling/api/fetchRawPollTally.ts
@@ -1,35 +1,20 @@
 import { SupportedNetworks } from 'modules/web3/constants/networks';
-// import { config } from 'lib/config';
-// import { fsCacheGet, fsCacheSet } from 'lib/fscache';
 import { PollVoteType, RawPollTally } from 'modules/polling/types';
 import { POLL_VOTE_TYPE } from 'modules/polling/polling.constants';
 import { fetchTallyPlurality } from './fetchTallyPlurality';
 import { fetchTallyRankedChoice } from './fetchTallyRankedChoice';
 
-export async function fetchPollTally(
+export async function fetchRawPollTally(
   pollId: number,
   voteType: PollVoteType,
-  useCache: boolean,
   network: SupportedNetworks
 ): Promise<RawPollTally> {
-  // const cacheKey = `tally-${pollId}`;
-  // if (config.USE_FS_CACHE && useCache) {
-  //   const cachedTally = fsCacheGet(cacheKey, network);
-  //   if (cachedTally) {
-  //     return JSON.parse(cachedTally);
-  //   }
-  // }
-
   let tally;
   if (voteType === POLL_VOTE_TYPE.PLURALITY_VOTE) {
     tally = await fetchTallyPlurality(pollId, network);
   } else {
     tally = await fetchTallyRankedChoice(pollId, network);
   }
-
-  // if (config.USE_FS_CACHE && useCache) {
-  //   fsCacheSet(cacheKey, JSON.stringify(tally), network);
-  // }
 
   return tally;
 }
--- modules/polling/helpers/getPollTally.ts
+++ modules/polling/helpers/getPollTally.ts
@@ -1,20 +1,29 @@
 import { SupportedNetworks } from 'modules/web3/constants/networks';
 import { backoffRetry } from 'lib/utils';
-import { fetchPollTally } from '../api/fetchPollTally';
-import { fetchVotesByAddressForPoll } from '../api/fetchVotesByAddress';
-import { POLL_VOTE_TYPE } from '../polling.constants';
-import { Poll, PollTally, PollVoteType, RawPollTally, RawPollTallyRankedChoice } from '../types';
-import { parseRawPollTally } from './parseRawTally';
 import BigNumber from 'bignumber.js';
+import { config } from 'lib/config';
+import { fsCacheGet, fsCacheSet } from 'lib/fscache';
+import { fetchRawPollTally } from 'modules/polling/api/fetchRawPollTally';
+import { fetchVotesByAddressForPoll } from 'modules/polling/api/fetchVotesByAddress';
+import { POLL_VOTE_TYPE } from 'modules/polling/polling.constants';
+import { Poll, PollTally, PollVoteType, RawPollTally, RawPollTallyRankedChoice } from 'modules/polling/types';
+import { parseRawPollTally } from 'modules/polling/helpers/parseRawTally';
+import { isActivePoll } from 'modules/polling/helpers/utils';
 
 export async function getPollTally(poll: Poll, network: SupportedNetworks): Promise<PollTally> {
+  const cacheKey = `${network}-parsed-tally-${poll.pollId}`;
+  if (config.USE_FS_CACHE) {
+    const cachedTally = fsCacheGet(cacheKey, network);
+    if (cachedTally) {
+      return JSON.parse(cachedTally);
+    }
+  }
+
   // if poll vote type is unknown, treat as ranked choice
   const voteType: PollVoteType = poll.voteType || POLL_VOTE_TYPE.RANKED_VOTE;
 
   // Builds raw poll tally
-  const tally: RawPollTally = await backoffRetry(3, () =>
-    fetchPollTally(poll.pollId, voteType, false, network)
-  );
+  const tally: RawPollTally = await backoffRetry(3, () => fetchRawPollTally(poll.pollId, voteType, network));
 
   const endUnix = new Date(poll.endDate).getTime() / 1000;
   const votesByAddress = await fetchVotesByAddressForPoll(poll.pollId, endUnix, network);
@@ -23,7 +32,7 @@ export async function getPollTally(poll: Poll, network: SupportedNetworks): Prom
   const winner: string = tally.winner || '';
   const numVoters = tally.numVoters;
 
-  const parsedTally = {
+  const tallyObject = {
     pollVoteType: voteType,
     options:
       Object.keys(tally.options).length > 0
@@ -48,8 +57,13 @@ export async function getPollTally(poll: Poll, network: SupportedNetworks): Prom
     votesByAddress
   } as RawPollTally;
 
-  if ('rounds' in tally) (parsedTally as RawPollTallyRankedChoice).rounds = tally.rounds;
+  if ('rounds' in tally) (tallyObject as RawPollTallyRankedChoice).rounds = tally.rounds;
 
-  // Return parsed tally
-  return parseRawPollTally(parsedTally, poll);
+  const parsedTally = parseRawPollTally(tallyObject, poll);
+
+  if (config.USE_FS_CACHE && !isActivePoll(poll)) {
+    fsCacheSet(cacheKey, JSON.stringify(parsedTally), network);
+  }
+
+  return parsedTally;
 }
