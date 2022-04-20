6a269805b99d252cff36ca7a648b91ab04649340
Adam Goth <adamgoth@gmail.com>
2022-04-20 13:16:54 +0200

    Mock remark rehype

--- __tests__/setup.tsx
+++ __tests__/setup.tsx
@@ -10,7 +10,12 @@ jest.mock('modules/web3/helpers/ens');
 
 jest.mock('remark-gfm', () => () => null);
 jest.mock('remark-html', () => () => null);
+jest.mock('remark-parse', () => () => null);
+jest.mock('remark-rehype', () => () => null);
+jest.mock('unified', () => () => null);
 jest.mock('remark', () => () => null);
+jest.mock('rehype-stringify', () => () => null);
+jest.mock('rehype-sanitize', () => () => null);
 
 jest.mock('modules/address/components/AddressIcon', () => {
   return {
