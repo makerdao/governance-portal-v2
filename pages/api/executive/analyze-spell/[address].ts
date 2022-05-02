24012ae6d5b41482de741d8a8eafced5f5cc089e
Rafael Ventura <rafinskipg@gmail.com>
2022-05-02 15:46:19 +0200

    Api analyze spell

--- pages/api/executive/analyze-spell/[address].ts
+++ pages/api/executive/analyze-spell/[address].ts
@@ -1,11 +1,12 @@
 import invariant from 'tiny-invariant';
-import { ethers } from 'ethers';
+import { ethers, providers } from 'ethers';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import withApiHandler from 'modules/app/api/withApiHandler';
 import { DEFAULT_NETWORK } from 'modules/web3/constants/networks';
 import { isSupportedNetwork } from 'modules/web3/helpers/networks';
 import { analyzeSpell } from 'modules/executive/api/analyzeSpell';
+import { config } from 'lib/config';
 // In memory result cache
 const results = {};
 
@@ -25,7 +26,10 @@ export default withApiHandler(async (req: NextApiRequest, res: NextApiResponse) 
   if (results[network][spellAddress]) {
     analysis = results[network][spellAddress];
   } else {
-    analysis = await analyzeSpell(spellAddress, network);
+    const batchProvider = new providers.JsonRpcBatchProvider(
+      `https://mainnet.infura.io/v3/${config.INFURA_KEY}`
+    );
+    analysis = await analyzeSpell(batchProvider, spellAddress, network);
     if (analysis.hasBeenCast) {
       results[network][spellAddress] = analysis;
     }
