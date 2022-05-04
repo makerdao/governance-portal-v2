ca345366c737d85c8f8d9f42edb288793f0b1485
Adam Goth <adamgoth@gmail.com>
2022-05-04 14:28:19 +0200

    Fix build

--- modules/delegates/types/delegate.d.ts
+++ modules/delegates/types/delegate.d.ts
@@ -42,7 +42,7 @@ export type Delegate = {
   mkrDelegated: string;
   proposalsSupported: number;
   execSupported: CMSProposal | undefined;
-  mkrLockedDelegate?: MKRLockedDelegateAPIResponse[];
+  mkrLockedDelegate: MKRLockedDelegateAPIResponse[];
   blockTimestamp: string;
 };
 
