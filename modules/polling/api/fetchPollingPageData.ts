3ad65971606cd2361f869882fef5a54e6c2dd0bf
Adam Goth <adamgoth@gmail.com>
2022-05-06 11:08:29 +0200

    Refactor review page fetching

--- modules/polling/api/fetchPollingPageData.ts
+++ modules/polling/api/fetchPollingPageData.ts
@@ -2,6 +2,10 @@ import { getPolls } from 'modules/polling/api/fetchPolls';
 import { Poll, PollCategory } from 'modules/polling/types';
 import { SupportedNetworks } from 'modules/web3/constants/networks';
 
+export type PollingReviewPageData = {
+  polls: Poll[];
+};
+
 export type PollingPageData = {
   polls: Poll[];
   categories: PollCategory[];
