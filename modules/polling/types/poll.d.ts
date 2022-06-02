296ab1338cf6aca7fc55ec02dcb8b21ac24c6168
Rafael Ventura <rafinskipg@gmail.com>
2022-06-02 15:10:42 +0200

    define poll metadata type

--- modules/polling/types/poll.d.ts
+++ modules/polling/types/poll.d.ts
@@ -1,6 +1,28 @@
 import { Tag } from 'modules/app/types/tag';
 import { PollVoteType } from './pollVoteType';
 
+enum PollInputFormat {
+  singleChoice = 'single-choice',
+  rankFree = 'rank-free',
+}
+
+enum PollVictoryConditions {
+  majority = 'majority',
+  plurality = 'plurality'
+}
+
+enum PollResultDisplay {
+  singleVoteBreakdown = 'singleVoteBreakdown',
+  instantRunoffBreakdown = 'instant-runoff-breakdown',
+  conditionSummary = 'condition-summary'
+}
+
+type PollMetadata = {
+  inputFormat: PollInputFormat,
+  victoryConditions: PollVictoryConditions,
+  resultDisplay: PollResultDisplay
+}
+
 export type Poll = {
   title: string;
   multiHash: string;
@@ -12,6 +34,7 @@ export type Poll = {
   startDate: Date;
   discussionLink: string | null;
   voteType: PollVoteType;
+  metadata: PollMetadata;
   tags: Tag[];
   slug: string;
   ctx: {
