0b052a1d65b5f1e5e7a2c6b0ec07a1da6b26daee
Adam Goth <adamgoth@gmail.com>
2022-06-22 13:47:23 +0200

    Remove default provider from getENS

--- modules/address/components/Address.tsx
+++ modules/address/components/Address.tsx
@@ -1,9 +1,17 @@
 import { limitString } from 'lib/string';
 import { formatAddress } from 'lib/utils';
 import { getENS } from 'modules/web3/helpers/ens';
+import { useActiveWeb3React } from 'modules/web3/hooks/useActiveWeb3React';
 import React, { useEffect, useState } from 'react';
 
-export function Address({ address, maxLength }: { address: string; maxLength?: number }): React.ReactElement {
+export const Address = React.memo(function Address({
+  address,
+  maxLength
+}: {
+  address: string;
+  maxLength?: number;
+}): React.ReactElement {
+  const { library } = useActiveWeb3React();
   const [addressFormated, setAddressFormatted] = useState(formatAddress(address || '').toLowerCase());
 
   async function fetchENSName(address: string) {
@@ -11,7 +19,7 @@ export function Address({ address, maxLength }: { address: string; maxLength?: n
       return;
     }
 
-    const ens = await getENS(address);
+    const ens = await getENS({ address, library });
 
     ens ? setAddressFormatted(ens) : setAddressFormatted(formatAddress(address).toLowerCase());
   }
@@ -26,4 +34,4 @@ export function Address({ address, maxLength }: { address: string; maxLength?: n
       {maxLength ? limitString(addressFormated, maxLength, '...') : addressFormated}
     </React.Fragment>
   );
-}
+});
