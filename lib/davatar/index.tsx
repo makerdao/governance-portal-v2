cf12aee6cfe5b185209f62848c7db6e4638f5693
Adam Goth <adamgoth@gmail.com>
2022-06-22 14:16:21 +0200

    Use active library for davatar

--- lib/davatar/index.tsx
+++ lib/davatar/index.tsx
@@ -1,5 +1,5 @@
-import { Web3Provider, getDefaultProvider, BaseProvider } from '@ethersproject/providers';
 import { SupportedChainId } from 'modules/web3/constants/chainID';
+import { useActiveWeb3React } from 'modules/web3/hooks/useActiveWeb3React';
 import { useEffect, useState, ReactChild, CSSProperties } from 'react';
 
 import Image from './Image';
@@ -9,48 +9,20 @@ export { default as Image } from './Image';
 export interface DavatarProps {
   size: number;
   address: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  provider?: any;
-  // deprecated
-  graphApiKey?: string;
   defaultComponent?: ReactChild | ReactChild[];
   style?: CSSProperties;
 }
 
-export default function Davatar({
-  size,
-  address,
-  provider,
-  graphApiKey,
-  defaultComponent,
-  style
-}: DavatarProps) {
+export default function Davatar({ size, address, defaultComponent, style }: DavatarProps): JSX.Element {
+  const { chainId, library } = useActiveWeb3React();
   const [avatarUri, setAvatarUri] = useState<string | null>(null);
-  const [ethersProvider, setEthersProvider] = useState<BaseProvider | null>(null);
 
   useEffect(() => {
-    let eth = getDefaultProvider();
-    let chainId;
-    let isEthers = false;
-
-    if (provider) {
-      if (provider.currentProvider?.chainId) {
-        chainId = parseInt(provider.currentProvider.chainId);
-      } else if (provider.network?.chainId) {
-        isEthers = true;
-        chainId = provider.network.chainId;
-      }
-
-      if ([1, 3, 4, 5].includes(chainId)) {
-        eth = isEthers ? (provider as BaseProvider) : new Web3Provider(provider.currentProvider);
-      }
-
-      setEthersProvider(eth);
-
+    if (library) {
       if (chainId !== SupportedChainId.GOERLIFORK) {
-        eth.lookupAddress(address).then(ensName => {
+        library.lookupAddress(address).then(ensName => {
           if (ensName) {
-            eth.getResolver(ensName).then(resolver => {
+            library.getResolver(ensName).then(resolver => {
               resolver &&
                 resolver.getText('avatar').then(avatar => {
                   if (avatar && avatar.length > 0) {
@@ -62,15 +34,14 @@ export default function Davatar({
         });
       }
     }
-  }, [address, provider]);
+  }, [address, library]);
 
   return (
     <Image
       size={size}
       address={address}
       uri={avatarUri}
-      graphApiKey={graphApiKey}
-      provider={ethersProvider}
+      provider={library}
       defaultComponent={defaultComponent}
       style={style}
     />
