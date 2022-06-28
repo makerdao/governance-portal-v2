7260f12c05a45ed2618fd6167f8529d3bf482f4d
Rafael Ventura <rafinskipg@gmail.com>
2022-06-28 10:55:16 +0200

    Cache executive supporters 10 minutes

--- pages/api/executive/supporters.ts
+++ pages/api/executive/supporters.ts
@@ -7,12 +7,24 @@ import { isSupportedNetwork } from 'modules/web3/helpers/networks';
 import { getContracts } from 'modules/web3/helpers/getContracts';
 import { networkNameToChainId } from 'modules/web3/helpers/chain';
 import { fetchExecutiveVoteTally } from 'modules/executive/api/fetchExecutiveVoteTally';
+import { cacheGet, cacheSet } from 'lib/cache';
 
 export default withApiHandler(async (req: NextApiRequest, res: NextApiResponse) => {
   const network = (req.query.network as string) || DEFAULT_NETWORK.network;
   invariant(isSupportedNetwork(network), `unsupported network ${network}`);
 
   const chief = getContracts(networkNameToChainId(network), undefined, undefined, true).chief;
+
+  const cacheKey = `executive-vote-tally-${chief.address}`;
+
+  const cached = await cacheGet(cacheKey, network);
+
+  res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
+
+  if (cached) {
+    res.status(200).json(cached);
+    return;
+  }
 
   const allSupporters = await fetchExecutiveVoteTally(chief);
 
@@ -23,6 +35,7 @@ export default withApiHandler(async (req: NextApiRequest, res: NextApiResponse) 
     });
   });
 
-  res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
+  const tenMinutesInMs = 10 * 60 * 1000;
+  cacheSet(cacheKey, JSON.stringify(allSupporters), network, tenMinutesInMs);
   res.status(200).json(allSupporters);
 });
