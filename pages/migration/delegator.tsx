ce1a2e0a7b8b9ba62f2beedcf17991ac3876e451
rafinskipg <rafinskipg@gmail.com>
2022-07-05 10:16:42 +0200

    Adding empty states

--- pages/migration/delegator.tsx
+++ pages/migration/delegator.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Text } from 'theme-ui';
+import { Box, Button, Heading, Text } from 'theme-ui';
 import PrimaryLayout from 'modules/app/components/layout/layouts/Primary';
 import Stack from 'modules/app/components/layout/layouts/Stack';
 import { HeadComponent } from 'modules/app/components/layout/Head';
@@ -10,6 +10,8 @@ import { useMemo } from 'react';
 import { Delegate } from 'modules/delegates/types';
 import { useDelegatedTo } from 'modules/delegates/hooks/useDelegatedTo';
 import { DelegateExpirationOverviewCard } from 'modules/migration/components/DelegateExpirationOverviewCard';
+import Icon from 'modules/app/components/Icon';
+import Link from 'next/link';
 
 export default function DelegateMigrationPage(): React.ReactElement {
   const { isDelegatedToExpiringContract, isDelegatedToExpiredContract } = useMigrationStatus();
@@ -37,7 +39,7 @@ export default function DelegateMigrationPage(): React.ReactElement {
   }, [delegatesData, delegatedTo.data]);
 
   const delegatesThatAreNotExpired: Delegate[] = useMemo(() => {
-    if (!delegatesData) {
+    if (!delegatesData || true) {
       return [];
     }
 
@@ -58,9 +60,9 @@ export default function DelegateMigrationPage(): React.ReactElement {
       )}
 
       {account && (
-        <Stack gap={3}>
+        <Stack gap={4} sx={{ maxWidth: '872px', margin: '0 auto' }}>
           <Heading mb={2} as="h4" sx={{ textAlign: 'center' }}>
-          ACTION REQUIRED: Migrate your delegated MKR.
+            ACTION REQUIRED: Migrate your delegated MKR.
           </Heading>
 
           {(isDelegatedToExpiredContract || isDelegatedToExpiringContract) && (
@@ -72,17 +74,17 @@ export default function DelegateMigrationPage(): React.ReactElement {
             </Text>
           )}
 
-          <Box sx={{ textAlign: 'center'}}>
-            <Text as="p" sx={{color: 'onSecondary'}}>
+          <Box sx={{ textAlign: 'center' }}>
+            <Text as="p" sx={{ color: 'onSecondary' }}>
               Maker delegate contracts expire after 1 year.
             </Text>
-            <Text  sx={{color: 'onSecondary'}}>
+            <Text sx={{ color: 'onSecondary' }}>
               Please migrate your MKR by undelegating from the expiring/expired contracts and redelegating to
               the new contracts.
             </Text>
           </Box>
 
-          <Box sx={{ textAlign: 'center'}}>
+          <Box sx={{ textAlign: 'center' }}>
             <Text>
               Please find below any expiring/expired delegate contracts you delegated MKR to, and requires
               migration.
@@ -92,7 +94,7 @@ export default function DelegateMigrationPage(): React.ReactElement {
           <Box>
             <Box>
               <Text as="h2">Expired/about to expire contracts you delegated MKR to</Text>
-              <Text as="p" variant="body" sx={{ color: 'onSecondary'}}>
+              <Text as="p" variant="body" sx={{ color: 'onSecondary' }}>
                 Please undelegate your MKR from old contracts, one by one.
               </Text>
             </Box>
@@ -109,16 +111,35 @@ export default function DelegateMigrationPage(): React.ReactElement {
               </Box>
             )}
             {delegatesThatAreAboutToExpiry.length === 0 && (
-              <Box sx={{
-                background: '#FBFBFB',
-                textAlign: 'center',
-                padding: '50px',
-                marginTop: 2,
-                marginBottom: 2,
-                border: '1px dashed #E3E9F0',
-                borderRadius: 1
-              }}>
-                <Text as="p" sx={{ color: 'onSecondary'}}>None of your delegates contracts are expired or about to expire</Text>
+              <Box
+                sx={{
+                  background: '#FBFBFB',
+                  textAlign: 'center',
+                  padding: '50px',
+                  marginTop: 2,
+                  marginBottom: 2,
+                  border: '1px dashed #E3E9F0',
+                  borderRadius: 1
+                }}
+              >
+                <Box
+                  sx={{
+                    borderRadius: '100%',
+                    background: 'background',
+                    border: '1px dashed #E3E9F0',
+                    display: 'flex',
+                    margin: '0 auto',
+                    width: '54px',
+                    height: '54px',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                  }}
+                >
+                  <Icon name="calendar" />
+                </Box>
+                <Text as="p" sx={{ color: 'onSecondary' }}>
+                  None of your delegates contracts are expired or about to expire.
+                </Text>
                 <Text>No further action needed!</Text>
               </Box>
             )}
@@ -127,19 +148,43 @@ export default function DelegateMigrationPage(): React.ReactElement {
           <Box>
             <Box>
               <Text as="h2">Renewed contracts by your previous delegates</Text>
-              <Text as="p" variant="body" sx={{ color: 'onSecondary'}}>
+              <Text as="p" variant="body" sx={{ color: 'onSecondary' }}>
                 Please delegate your MKR to the new contracts, one by one.
               </Text>
             </Box>
 
             <Box>
-              {delegatesThatAreNotExpired.map(delegate => {
-                return (
-                  <Box key={`delegated-about-to-expiry-${delegate.address}`} sx={{ mb: 3 }}>
-                    <DelegateExpirationOverviewCard delegate={delegate} />
-                  </Box>
-                );
-              })}
+              {delegatesThatAreNotExpired.length > 0 &&
+                delegatesThatAreNotExpired.map(delegate => {
+                  return (
+                    <Box key={`delegated-about-to-expiry-${delegate.address}`} sx={{ mb: 3 }}>
+                      <DelegateExpirationOverviewCard delegate={delegate} />
+                    </Box>
+                  );
+                })}
+              {delegatesThatAreNotExpired.length === 0 && (
+                <Box
+                  sx={{
+                    background: '#FBFBFB',
+                    textAlign: 'center',
+                    padding: '50px',
+                    marginTop: 2,
+                    marginBottom: 2,
+                    border: '1px dashed #E3E9F0',
+                    borderRadius: 1
+                  }}
+                >
+                  <Text as="p" sx={{ color: 'onSecondary' }}>
+                    None of your delegates have renewed their contract yet.
+                  </Text>
+                  <Text as="p">
+                    Check back here later, or visit the delegates page and pick a delegate manually.
+                  </Text>
+                  <Link href="/delegates">
+                    <Button sx={{ mt: 2 }}>Go to delegates page</Button>
+                  </Link>
+                </Box>
+              )}
             </Box>
           </Box>
         </Stack>
