f5561641133d102e7dd8ee875ac9d106839831f8
Adam Goth <adamgoth@gmail.com>
2022-07-11 13:59:47 +0200

    Update step three with gif and instructions

--- modules/migration/components/ConnectWallet.tsx
+++ modules/migration/components/ConnectWallet.tsx
@@ -1,21 +1,36 @@
 import { Flex, Text, Label } from 'theme-ui';
+import { Icon } from '@makerdao/dai-ui-icons';
 import { useLinkedDelegateInfo } from 'modules/migration/hooks/useLinkedDelegateInfo';
+import { ExternalLink } from 'modules/app/components/ExternalLink';
 
 export function ConnectWallet(): JSX.Element {
   const { newOwnerAddress } = useLinkedDelegateInfo();
   return (
-    <Flex>
-      <Flex sx={{ flexDirection: 'column', width: '50%' }}>
+    <Flex sx={{ flexDirection: 'column' }}>
+      <Flex sx={{ flexDirection: 'column' }}>
         <Text as="h3" sx={{ fontWeight: 'semiBold' }}>
-          Connect to your new address to continue
+          Please switch from your old delegate address to your new delegate address
         </Text>
-        <Label sx={{ mt: 4 }}>Enter new address</Label>
+        <Label sx={{ mt: 4 }}>Your new address</Label>
         <Text variant="secondary">{newOwnerAddress}</Text>
       </Flex>
-      <Flex sx={{ width: '50%', flexDirection: 'column' }}>
-        {/* <Text as="h3" sx={{ fontWeight: 'semiBold' }}>
-          But first, remember to disconnect your current wallet address
-        </Text> */}
+      <Flex sx={{ width: '100%', my: 4 }}>
+        <img src="/assets/switch-account-metamask.gif" />
+      </Flex>
+      <ExternalLink href={'#'} title="Read more">
+        <Flex sx={{ alignItems: 'center' }}>
+          <Text>Read more how to switch accounts in MetaMask</Text>
+          <Icon ml={2} name="arrowTopRight" size={2} />
+        </Flex>
+      </ExternalLink>
+      <Flex sx={{ alignItems: 'center' }}>
+        <Text variant="secondary" sx={{ my: 3 }}>
+          If you are having trouble switching accounts, please reach out for support on our{' '}
+          <ExternalLink href="https://discord.gg/tQ5wnN6Ms4" title="Discord" styles={{ color: 'accentBlue' }}>
+            <Text>Discord</Text>
+          </ExternalLink>
+        </Text>
+        <Icon name={'discord'} sx={{ ml: 2 }} />
       </Flex>
     </Flex>
   );
