bf74e74dbd113029b78d760ef9ea85c84b2112a2
Rafael Ventura <rafinskipg@gmail.com>
2022-07-15 16:46:54 +0200

    fix lint

--- modules/cache/constants/cache-keys.ts
+++ modules/cache/constants/cache-keys.ts
@@ -6,6 +6,6 @@ export const githubExecutivesCacheKey = 'executives-github';
 
 export const delegatesCacheKey = 'delegates';
 
-export const getAllPollsCacheKey = (filters?: object) => `polls-${filters ? JSON.stringify(filters) : 'all'}`;
+export const getAllPollsCacheKey = (filters?: any) => `polls-${filters ? JSON.stringify(filters) : 'all'}`;
 
 export const pollTagsMappingJSONCacheKey = 'poll-tags-mapping';
