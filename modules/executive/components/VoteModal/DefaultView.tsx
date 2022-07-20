537e8c2bad72bc47472d063190d0589e848de4c1
Tyler Sorensen <tyler@dux.makerdao.network>
2022-07-20 14:37:58 -0700

    wait a little longer before invalidating cache after exec voting

--- modules/executive/components/VoteModal/DefaultView.tsx
+++ modules/executive/components/VoteModal/DefaultView.tsx
@@ -164,7 +164,7 @@ export default function DefaultVoteModalView({
         setTimeout(() => {
           invalidateCache(executiveSupportersCacheKey, network);
           invalidateCache('proposals-', network);
-        }, 30000);
+        }, 45000);
       },
       error: () => onTransactionFailed()
     };
