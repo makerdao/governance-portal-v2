6ae618304d92867add6550ad7e31f92509b82f35
b-pmcg <bain.pmcg@gmail.com>
2022-07-29 12:24:10 -0600

    add next-axiom for fullstack logs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 require('dotenv').config({ path: './.env' });
 
+//Send Web-Vitals and logs from Next.js to Axiom.
+//https://github.com/axiomhq/next-axiom
+const { withAxiom } = require('next-axiom');
+
 // This file sets a custom webpack configuration to use your Next.js app
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
@@ -101,4 +105,4 @@ const SentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+module.exports = withSentryConfig(withAxiom(moduleExports), SentryWebpackPluginOptions);
