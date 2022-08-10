318133bae6e766b250c8f17ed32e85ff0db2a5cc
b-pmcg <bain.pmcg@gmail.com>
2022-08-10 13:39:42 +0100

    fix build error

--- modules/web3/hooks/useContracts.ts
+++ modules/web3/hooks/useContracts.ts
@@ -1,9 +1,10 @@
 import { useMemo } from 'react';
 import { Web3Provider } from '@ethersproject/providers';
-import { EthSdk, getContracts } from '../helpers/getContracts';
+import { getContracts } from '../helpers/getContracts';
 import { useWeb3 } from 'modules/web3/hooks/useWeb3';
 import { SupportedChainId } from '../constants/chainID';
 import { isSupportedChain } from 'modules/web3/helpers/chain';
+import { EthSdk } from '../types/contracts';
 
 type Props = {
   chainId?: SupportedChainId;
