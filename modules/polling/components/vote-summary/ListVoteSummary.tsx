1b08b92006b9b17e761701f3e3a1054b3d359350
Adam Goth <adamgoth@gmail.com>
2022-08-15 16:02:24 +0200

    Prevent passing number to limitString

--- modules/polling/components/vote-summary/ListVoteSummary.tsx
+++ modules/polling/components/vote-summary/ListVoteSummary.tsx
@@ -27,22 +27,25 @@ export function ListVoteSummary({
             return index < limit;
           }
         })
-        .map((choice, index) => (
-          <Box
-            key={`voter-${poll.pollId}-option-${choice}-${index}`}
-            sx={{
-              fontSize: isApproval ? 2 : index === 0 ? 2 : 1,
-              fontWeight: isApproval ? 'semiBold' : index === 0 ? 'semiBold' : 'normal',
-              color: isApproval ? '#708390' : index === 0 ? 'text' : '#708390',
-              textAlign: align
-            }}
-            title={poll.options[choice]}
-          >
-            {align === 'right'
-              ? `${limitString(poll.options[choice], 30, '...')}${showOrdinal ? ` - ${index + 1}` : ''}`
-              : `${showOrdinal ? ` - ${index + 1}` : ''}${limitString(poll.options[choice], 30, '...')}`}
-          </Box>
-        ))}
+        .map((choice, index) => {
+          const choiceText = poll.options[choice].toString();
+          return (
+            <Box
+              key={`voter-${poll.pollId}-option-${choice}-${index}`}
+              sx={{
+                fontSize: isApproval ? 2 : index === 0 ? 2 : 1,
+                fontWeight: isApproval ? 'semiBold' : index === 0 ? 'semiBold' : 'normal',
+                color: isApproval ? '#708390' : index === 0 ? 'text' : '#708390',
+                textAlign: align
+              }}
+              title={choiceText}
+            >
+              {align === 'right'
+                ? `${limitString(choiceText, 30, '...')}${showOrdinal ? ` - ${index + 1}` : ''}`
+                : `${showOrdinal ? ` - ${index + 1}` : ''}${limitString(choiceText, 30, '...')}`}
+            </Box>
+          );
+        })}
     </Box>
   );
 }
