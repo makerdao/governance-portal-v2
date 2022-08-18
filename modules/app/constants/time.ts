1d66ee7fdb5e6b5c0049848627f8ceedcc204190
Adam Goth <adamgoth@gmail.com>
2022-08-18 13:12:28 +0200

    Reduce ttl for active polls to 30 seconds

--- modules/app/constants/time.ts
+++ modules/app/constants/time.ts
@@ -1,5 +1,6 @@
-export const ONE_HOUR_IN_MS = 60 * 60 * 1000;
+export const THIRTY_SECONDS_IN_MS = 30 * 1000;
 export const ONE_MINUTE_IN_MS = 60 * 1000;
 export const FIVE_MINUTES_IN_MS = 5 * ONE_MINUTE_IN_MS;
-export const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
 export const TEN_MINUTES_IN_MS = 10 * ONE_MINUTE_IN_MS;
+export const ONE_HOUR_IN_MS = 60 * 60 * 1000;
+export const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
