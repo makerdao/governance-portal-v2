80dc3da8fb42c0fdd848670a984a19537d08ca42
Adam Goth <adamgoth@gmail.com>
2022-09-01 14:33:29 +0200

    Add ttl info to dashboard

---
+++ pages/api/cache/info.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { DEFAULT_NETWORK, SupportedNetworks } from 'modules/web3/constants/networks';
+import withApiHandler from 'modules/app/api/withApiHandler';
+import logger from 'lib/logger';
+import { getCacheInfo } from 'modules/cache/cache';
+import {
+  delegatesGithubCacheKey,
+  allDelegatesCacheKey,
+  executiveSupportersCacheKey,
+  githubExecutivesCacheKey,
+  getAllPollsCacheKey
+} from 'modules/cache/constants/cache-keys';
+
+// fetches cache info for constant keys
+export default withApiHandler(async (req: NextApiRequest, res: NextApiResponse) => {
+  const network = (req.query.network as SupportedNetworks) || DEFAULT_NETWORK.network;
+
+  // keys to check
+  const allowedCacheKeys = [
+    getAllPollsCacheKey(),
+    executiveSupportersCacheKey,
+    githubExecutivesCacheKey,
+    delegatesGithubCacheKey,
+    allDelegatesCacheKey
+  ];
+
+  try {
+    const promises = await Promise.all(allowedCacheKeys.map(key => getCacheInfo(key, network)));
+    let response = {};
+    promises.map((key, index) => {
+      response[allowedCacheKeys[index]] = key;
+    });
+
+    return res.status(200).json({
+      ...response
+    });
+  } catch (e) {
+    logger.error(`cache-info: ${e.message}`);
+    return res.status(200).json({
+      error: 'unknown'
+    });
+  }
+});
