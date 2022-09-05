ff1318685acca2ef2f24ebcd5f0fdd2581a0fc25
Adam Goth <adamgoth@gmail.com>
2022-09-05 14:47:02 +0200

    Format min display

--- modules/polling/polling.constants.ts
+++ modules/polling/polling.constants.ts
@@ -1,4 +1,5 @@
 import { BigNumber } from 'ethers';
+import { BigNumber as BigNumberJs } from 'bignumber.js';
 import { parseUnits } from 'ethers/lib/utils';
 import { WAD } from 'modules/web3/constants/numbers';
 import { PollVoteType } from './types';
@@ -6,8 +7,11 @@ import { PollVoteType } from './types';
 export const ABSTAIN = 0;
 
 export const MIN_MKR_REQUIRED_FOR_GASLESS_VOTING = BigNumber.from(parseUnits('0.1'));
-export const MIN_MKR_REQUIRED_FOR_GASLESS_VOTING_DISPLAY =
-  MIN_MKR_REQUIRED_FOR_GASLESS_VOTING.div(WAD).toNumber();
+export const MIN_MKR_REQUIRED_FOR_GASLESS_VOTING_DISPLAY = new BigNumberJs(
+  MIN_MKR_REQUIRED_FOR_GASLESS_VOTING.toString()
+)
+  .div(WAD.toString())
+  .toFormat(1);
 
 export const POLL_VOTE_TYPE = {
   PLURALITY_VOTE: 'Plurality Voting' as PollVoteType,
