d8537a3cecae063e5c292687fbd8d57130a87aec
Adam Goth <adamgoth@gmail.com>
2022-09-08 12:38:14 +0200

    Add metamask helper

--- modules/app/components/layout/header/AccountSelect.tsx
+++ modules/app/components/layout/header/AccountSelect.tsx
@@ -18,6 +18,7 @@ import { SUPPORTED_WALLETS, WalletName, ConnectionType } from 'modules/web3/cons
 import { connectorToWalletName, getConnection } from 'modules/web3/connections';
 import { AnalyticsContext } from 'modules/app/client/analytics/AnalyticsContext';
 import { isSupportedChain } from 'modules/web3/helpers/chain';
+import { getIsMetaMask } from 'modules/web3/helpers/getIsMetaMask';
 import logger from 'lib/logger';
 import useSelectedConnectionStore from 'modules/app/stores/selectedConnection';
 
@@ -33,12 +34,6 @@ const closeButtonStyle: ThemeUICSSObject = {
 const AccountSelect = (): React.ReactElement => {
   const { setUserData } = useContext(AnalyticsContext);
   const router = useRouter();
-
-  // TODO move this
-  function getIsMetaMask(): boolean {
-    return window.ethereum?.isMetaMask ?? false;
-  }
-
   const { account: address, connector, chainId } = useWeb3React();
 
   const [pending, txs] = useTransactionStore(state => [
@@ -116,6 +111,7 @@ const AccountSelect = (): React.ReactElement => {
   }, [chainId]);
 
   const bpi = useBreakpointIndex();
+  const isMetaMask = getIsMetaMask();
 
   const disconnect = () => {
     setError(null);
@@ -140,7 +136,7 @@ const AccountSelect = (): React.ReactElement => {
         variant="mutedOutline"
         key={connectionName}
         onClick={
-          (isAndroid || isIOS) && !getIsMetaMask() && SUPPORTED_WALLETS[connectionName].deeplinkUri
+          (isAndroid || isIOS) && !isMetaMask && SUPPORTED_WALLETS[connectionName].deeplinkUri
             ? () => window.location.replace(SUPPORTED_WALLETS[connectionName].deeplinkUri || '')
             : () => onClickConnection(SUPPORTED_WALLETS[connectionName].connectionType, connectionName)
         }
