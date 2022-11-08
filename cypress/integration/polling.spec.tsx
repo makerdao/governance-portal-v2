08a09f8065fd6e75c02eaa1fd199e035085a2465
b-pmcg <bain.pmcg@gmail.com>
2022-11-08 16:36:19 -0700

    work on polling & poll-vote-delegate specs, add an ended poll by blocknumber

--- cypress/integration/polling.spec.tsx
+++ cypress/integration/polling.spec.tsx
@@ -3,20 +3,20 @@
 // If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
 // https://github.com/cypress-io/eslint-plugin-cypress
 import { INIT_BLOCK } from 'cypress/support/constants/blockNumbers';
-import { TESTING_ACTIVE_POLLS_COUNT } from 'cypress/support/constants/polling';
+import {
+  TESTING_ACTIVE_POLLS_COUNT,
+  TESTING_ENDED_POLLS_COUNT,
+  TESTING_TOTAL_POLLS_COUNT
+} from 'cypress/support/constants/polling';
 import { TEST_ACCOUNTS } from 'cypress/support/constants/testaccounts';
-import { forkNetwork, setAccount, visitPage } from '../support/commons';
+import { forkNetwork, resetDatabase, setAccount, visitPage } from '../support/commons';
 
 describe('/polling page', async () => {
   before(() => {
-    forkNetwork(INIT_BLOCK);
+    forkNetwork();
+    resetDatabase();
   });
   it('renders active polls', () => {
-    // Polls keep growing and the num polls check keeps failing
-    // We should have an API method that returns number of active polls we can compare here
-    // Or we just mock the polls since the DB calls should be tested elsewhere anyway.
-    const activePolls = TESTING_ACTIVE_POLLS_COUNT;
-    // const endedPolls = 0;
     visitPage('/polling');
 
     setAccount(TEST_ACCOUNTS.normal, () => {
@@ -26,18 +26,16 @@ describe('/polling page', async () => {
         matchCase: false
       }).should('be.visible');
 
-      cy.get('[data-testid="poll-overview-card"]').its('length').should('be.eq', activePolls);
+      cy.get('[data-testid="poll-overview-card"]').its('length').should('be.eq', TESTING_ACTIVE_POLLS_COUNT);
 
       // Poll card should display poll ID
       cy.contains('Poll ID 4').should('be.visible');
 
       // Show ended polls
-      // TODO uncomment when ended polls added
-      //cy.get('[data-testid="button-view-ended-polls"]').click();
+      cy.get('[data-testid="button-view-ended-polls"]').click();
 
-      // Check that now only shows 2 polls
-      // TODO uncomment when ended polls added
-      // cy.get('[data-testid="poll-overview-card"]').its('length').should('be.eq', endedPolls);
+      // Check that now shows all polls
+      cy.get('[data-testid="poll-overview-card"]').its('length').should('be.eq', TESTING_TOTAL_POLLS_COUNT);
     });
   });
 
@@ -56,14 +54,13 @@ describe('/polling page', async () => {
     cy.get('[data-testid="your-ballot-title"]').should('not.exist');
   });
 
-  // TODO: uncomment when we have ended polls
-  xit('Filters by ended polls', () => {
+  it('Filters by ended polls', () => {
     visitPage('/polling');
     setAccount(TEST_ACCOUNTS.normal, () => {
       cy.get('[data-testid="poll-filters-status"]').click();
       cy.get('[data-testid="checkbox-show-polls-ended"]').click();
 
-      cy.get('[data-testid="poll-overview-card"]').should('have.length', 3);
+      cy.get('[data-testid="poll-overview-card"]').should('have.length', TESTING_ENDED_POLLS_COUNT);
 
       cy.contains('Ended Polls').should('be.visible');
     });
