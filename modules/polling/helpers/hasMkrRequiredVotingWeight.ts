c3156e0d760893e9a2dc888cf18b548ff5f53339
Adam Goth <adamgoth@gmail.com>
2023-01-25 12:56:23 +0100

    Fix exclude param on weight check

--- modules/polling/helpers/hasMkrRequiredVotingWeight.ts
+++ modules/polling/helpers/hasMkrRequiredVotingWeight.ts
@@ -20,7 +20,7 @@ export async function hasMkrRequiredVotingWeight(
   //verify address has a poll weight > weight param
   let hasMkrRequired = false;
   try {
-    const pollWeight: MKRVotingWeightResponse = await getMKRVotingWeight(voter, network, true);
+    const pollWeight: MKRVotingWeightResponse = await getMKRVotingWeight(voter, network, false);
     hasMkrRequired = canBeEqual ? pollWeight.total.gte(weight) : pollWeight.total.gt(weight);
   } catch (err) {
     logger.error(err);
