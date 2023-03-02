135848a570ab18998899f711c73480665c9ffd2d
hernandoagf <hernando.guzman93@gmail.com>
2023-03-02 14:15:14 -0300

    Update the hash

--- modules/app/components/layout/Head.tsx
+++ modules/app/components/layout/Head.tsx
@@ -49,13 +49,13 @@ export function HeadComponent({
         content={
           "default-src 'self' https://*.makerdao.com;" +
           'frame-src https://connect.trezor.io https://www.youtube-nocookie.com https://player.vimeo.com;' +
-          "font-src 'self';" +
+          "font-src 'self' data:;" +
           "connect-src http://localhost:8545/ http://127.0.0.1:8546/ http://127.0.0.1:8545/ http://localhost:3001/ 'self' https: wss:;" +
           "style-src 'self' 'unsafe-inline';" +
           "prefetch-src 'self' https://*.makerdao.com;" +
           `script-src 'self' https://vercel.live https://gc.zgo.at/count.js ${
             dev ? "'unsafe-eval'" : ''
-          } 'sha256-ffXIVJIqwbhUsKZuw2L+l9gEWeLEqzkH/jgc2LNrgRA=';` +
+          } 'sha256-mPj9udBbDHxqlc6SSMYyCwgoUluvJKvwI9kYlCb3uEI=';` +
           "img-src 'self' https: data:"
         }
       />
