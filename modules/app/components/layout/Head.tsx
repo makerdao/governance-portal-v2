372c21431ba5e64943a44aa9a5257b53cf1bf8a2
hernandoagf <hernando.guzman93@gmail.com>
2023-03-02 14:08:18 -0300

    Update default-src to support Firefox directives

--- modules/app/components/layout/Head.tsx
+++ modules/app/components/layout/Head.tsx
@@ -47,13 +47,13 @@ export function HeadComponent({
       <meta
         httpEquiv="Content-Security-Policy"
         content={
-          "default-src 'self';" +
+          "default-src 'self' https://*.makerdao.com;" +
           'frame-src https://connect.trezor.io https://www.youtube-nocookie.com https://player.vimeo.com;' +
           "font-src 'self';" +
           "connect-src http://localhost:8545/ http://127.0.0.1:8546/ http://127.0.0.1:8545/ http://localhost:3001/ 'self' https: wss:;" +
           "style-src 'self' 'unsafe-inline';" +
           "prefetch-src 'self' https://*.makerdao.com;" +
-          `script-src 'self' https://gc.zgo.at/count.js ${
+          `script-src 'self' https://vercel.live https://gc.zgo.at/count.js ${
             dev ? "'unsafe-eval'" : ''
           } 'sha256-ffXIVJIqwbhUsKZuw2L+l9gEWeLEqzkH/jgc2LNrgRA=';` +
           "img-src 'self' https: data:"
