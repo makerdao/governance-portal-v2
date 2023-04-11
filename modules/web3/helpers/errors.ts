2cfdebf309207b8173cca52607446c1673179d75
Rafael Ventura <rafinskipg@gmail.com>
2023-04-11 14:25:17 +0200

    errorCodes

--- modules/web3/helpers/errors.ts
+++ modules/web3/helpers/errors.ts
@@ -6,7 +6,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 */
 
-import { getMessageFromCode, ERROR_CODES } from 'eth-rpc-errors';
+import { getMessageFromCode, errorCodes } from 'eth-rpc-errors';
 
 export const TX_NOT_ENOUGH_FUNDS = "Sender doesn't have enough funds to send the transaction";
 export const USER_REJECTED = 'User rejected the transaction';
@@ -26,7 +26,7 @@ export function parseTxError(error: Error): string {
   // First check if it's a Metamask error
   if (
     error['code'] &&
-    [...Object.values(ERROR_CODES.provider), ...Object.values(ERROR_CODES.rpc)].includes(error['code'])
+    [...Object.values(errorCodes.provider), ...Object.values(errorCodes.rpc)].includes(error['code'])
   ) {
     const extracted = getMessageFromCode(error['code']);
     if (extracted) {
