d2631f0a2f4c7464df7b73e2dfb3d4ef54294606
Hernando Guzmán <hernando.guzman93@gmail.com>
2023-05-25 18:58:02 -0300

    Change aggregated polls files routes

--- modules/polling/polling.constants.ts
+++ modules/polling/polling.constants.ts
@@ -73,14 +73,14 @@ export enum PollStatusEnum {
 
 export const POLLS_HASH_FILE_URL = {
   [SupportedNetworks.MAINNET]:
-    'https://raw.githubusercontent.com/hernandoagf/community/master/governance/polls/meta/hashed-polls.json',
+    'https://raw.githubusercontent.com/makerdao/community/master/governance/polls/meta/hashed-polls.json',
   [SupportedNetworks.GOERLI]:
-    'https://raw.githubusercontent.com/hernandoagf/community/master/governance/polls/meta/hashed-polls-goerli.json'
+    'https://raw.githubusercontent.com/makerdao-dux/community/polls-aggregated/governance/polls/meta/hashed-goerli-polls.json'
 };
 
 export const AGGREGATED_POLLS_FILE_URL = {
   [SupportedNetworks.MAINNET]:
-    'https://raw.githubusercontent.com/hernandoagf/community/master/governance/polls/meta/polls.json',
+    'https://raw.githubusercontent.com/makerdao/community/master/governance/polls/meta/polls.json',
   [SupportedNetworks.GOERLI]:
-    'https://raw.githubusercontent.com/hernandoagf/community/master/governance/polls/meta/polls-goerli.json'
+    'https://raw.githubusercontent.com/makerdao-dux/community/polls-aggregated/governance/polls/meta/goerli-polls.json'
 };
