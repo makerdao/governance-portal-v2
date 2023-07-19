c085184868c6b4c2d01d4111c149e81b1052457d
hernandoagf <hernando.guzman93@gmail.com>
2023-07-19 19:52:26 -0300

    feat: Add functions and modules for AVCs page

---
+++ modules/avcs/avcs.constants.ts
@@ -0,0 +1,5 @@
+export enum AvcOrderByEnum {
+  DELEGATES = 'DELEGATES',
+  MKR_DELEGATED = 'MKR_DELEGATED',
+  RANDOM = 'RANDOM'
+}
---
+++ modules/avcs/types/avc.d.ts
@@ -0,0 +1,38 @@
+/*
+
+SPDX-FileCopyrightText: © 2023 Dai Foundation <www.daifoundation.org>
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+
+*/
+
+import { SupportedNetworks } from 'modules/web3/constants/networks';
+import { AvcOrderByEnum } from '../avcs.constants';
+
+export type Avc = {
+  id: string;
+  name: string;
+  picture?: string;
+  externalUrl: string;
+  description: string;
+  mkrDelegated: string;
+  delegateCount: number;
+};
+
+export type AvcsQueryParams = {
+  orderBy?: AvcOrderByEnum;
+  searchTerm?: string | null;
+};
+
+export type AvcsValidatedQueryParams = {
+  network: SupportedNetworks;
+  orderBy: AvcOrderByEnum;
+  searchTerm: string | null;
+};
+
+export type AvcsAPIResponse = {
+  stats: {
+    totalCount: number;
+  };
+  avcs: Avc[];
+};
