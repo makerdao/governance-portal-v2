8ee353dce371410dd811571ce0e947ce82193b53
tyler17 <tylerya@gmail.com>
2023-09-14 19:13:26 -0700

    only one option when defaulting to default value

--- modules/polling/components/PollWinningOptionBox.tsx
+++ modules/polling/components/PollWinningOptionBox.tsx
@@ -38,12 +38,6 @@ export default function PollWinningOptionBox({
       result.mkrSupport === tally.results[0].mkrSupport
   ).length;
 
-  // Winner will be null if the winning conditions are not met, but we want to display the leading option too
-  const leadingOption = typeof tally.winner === 'number' ? tally.winner : tally.results[0].optionId;
-  const leadingOptionName = `${
-    typeof tally.winner === 'number' ? tally.winningOptionName : tally.results[0].optionName
-  }${numberOfLeadingOptions > 1 ? ` & ${numberOfLeadingOptions - 1} more` : ''}`;
-
   const winningVictoryCondition = tally.parameters.victoryConditions.find(
     (v, index) => index === tally.victoryConditionMatched
   );
@@ -65,6 +59,13 @@ export default function PollWinningOptionBox({
     textWin = `No winner condition met.${comparisonText ? comparisonText : ' '}Defaulting to`;
     isDefault = true;
   }
+
+    // Winner will be null if the winning conditions are not met, but we want to display the leading option too
+    const leadingOption = typeof tally.winner === 'number' ? tally.winner : tally.results[0].optionId;
+    const leadingOptionName = `${
+      typeof tally.winner === 'number' ? tally.winningOptionName : tally.results[0].optionName
+      //don't show "& n more" if isDefault since there's only ever 1 default option
+    }${!isDefault && numberOfLeadingOptions > 1 ? ` & ${numberOfLeadingOptions - 1} more` : ''}`;
 
   return (
     <Flex sx={{ py: 2, justifyContent: 'center' }}>
