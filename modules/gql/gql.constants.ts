a064e3a6942ff0e8c441eb281f6bdcefb6c46026
tyler17 <tylerya@gmail.com>
2024-07-12 10:52:34 -0700

    add env var for private subgraph usage

--- modules/gql/gql.constants.ts
+++ modules/gql/gql.constants.ts
@@ -13,9 +13,11 @@ export const MAINNET_SPOCK_URL = 'https://pollingdb2-mainnet-prod.makerdux.com/a
 export const TENDERLY_SPOCK_URL = 'https://pollingdb2-tenderly-staging.makerdux.com/api/v1';
 
 /* Subgraph URLs */
-//NOTE: add /private to the end of the URL to make localy development easier if access denied
-export const TENDERLY_SUBGRAPH_URL = 'https://query-subgraph-staging.sky.money/subgraphs/name/jetstreamgg/subgraph-testnet/public';
-export const MAINNET_SUBGRAPH_URL = 'https://query-subgraph-staging.sky.money/subgraphs/name/jetstreamgg/subgraph-testnet/public';
+
+const usePrivateSubgraph = process.env.USE_PRIVATE_SUBGRAPH === 'true';
+const permission = usePrivateSubgraph ? 'private' : 'public';
+export const TENDERLY_SUBGRAPH_URL = `https://query-subgraph-staging.sky.money/${permission}/subgraphs/name/jetstreamgg/subgraph-testnet`;
+export const MAINNET_SUBGRAPH_URL = `https://query-subgraph-staging.sky.money/${permission}/private/subgraphs/name/jetstreamgg/subgraph-testnet`;
 
 export enum QueryFilterNames {
   Active = 'active',
