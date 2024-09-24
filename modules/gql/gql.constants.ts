8b80f1d2074a920ace4cad80680909fda9979981
Adam Goth <adamgoth@gmail.com>
2024-09-24 14:11:01 +0200

    Update domain

--- modules/gql/gql.constants.ts
+++ modules/gql/gql.constants.ts
@@ -8,9 +8,9 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 /* Spock URLs */
 export const LOCAL_SPOCK_URL = 'http://localhost:3001/v1';
-export const STAGING_MAINNET_SPOCK_URL = 'https://pollingdb2-mainnet-staging.makerdux.com/api/v1';
-export const MAINNET_SPOCK_URL = 'https://pollingdb2-mainnet-prod.makerdux.com/api/v1';
-export const TENDERLY_SPOCK_URL = 'https://pollingdb2-tenderly-staging.makerdux.com/api/v1';
+export const STAGING_MAINNET_SPOCK_URL = 'https://pollingdb2-mainnet-staging.makerdao.com/api/v1';
+export const MAINNET_SPOCK_URL = 'https://pollingdb2-mainnet-prod.makerdao.com/api/v1';
+export const TENDERLY_SPOCK_URL = 'https://pollingdb2-tenderly-staging.makerdao.com/api/v1';
 
 /* Subgraph URLs */
 
