10b3b145950f7232331cca3767ee11e564a553eb
Adam Goth <adamgoth@gmail.com>
2024-09-26 10:36:02 +0200

    Fix errors

--- modules/delegates/api/fetchChainDelegates.ts
+++ modules/delegates/api/fetchChainDelegates.ts
@@ -18,7 +18,7 @@ export async function fetchChainDelegates(
   network: SupportedNetworks
 ): Promise<DelegateContractInformation[]> {
   const chainId = networkNameToChainId(network);
-  const data = await gqlRequest({ 
+  const data = await gqlRequest({
     chainId,
     useSubgraph: true,
     query: allDelegates
@@ -31,7 +31,7 @@ export async function fetchChainDelegates(
       voteDelegateAddress: d.id,
       mkrDelegated: formatValue(BigNumber.from(d.totalDelegated), 'wad', 18, false),
       version: d.version,
-      lastVoteDate: Number(d.voter.lastVotedTimestamp)
-    }
+      lastVoteDate: d.voter?.lastVotedTimestamp ? Number(d.voter.lastVotedTimestamp) : null
+    };
   });
 }
