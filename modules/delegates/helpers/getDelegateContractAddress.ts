7dc625e0f2be69646b2bf3e5a6b4634f7b476808
hernandoagf <hernando.guzman93@gmail.com>
2024-10-29 14:00:00 -0300

    Use more consistent names

--- modules/delegates/helpers/getDelegateContractAddress.ts
+++ modules/delegates/helpers/getDelegateContractAddress.ts
@@ -13,11 +13,11 @@ export async function getDelegateContractAddress(
   contracts: EthSdk,
   address: string
 ): Promise<string | undefined> {
-  const voteDelegateAdress = await contracts.voteDelegateFactory.delegates(address);
-  const v1VoteDelegateAddress = await contracts.voteDelegateFactoryOld.delegates(address);
-  return v1VoteDelegateAddress !== ZERO_ADDRESS
-    ? v1VoteDelegateAddress
-    : voteDelegateAdress !== ZERO_ADDRESS
-    ? voteDelegateAdress
+  const voteDelegateAddress = await contracts.voteDelegateFactory.delegates(address);
+  const voteDelegateAddressOld = await contracts.voteDelegateFactoryOld.delegates(address);
+  return voteDelegateAddressOld !== ZERO_ADDRESS
+    ? voteDelegateAddressOld
+    : voteDelegateAddress !== ZERO_ADDRESS
+    ? voteDelegateAddress
     : undefined;
 }
