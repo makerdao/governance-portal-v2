8bfb0a2bfe6a8ddb7bdaf98327c7b69f708bb491
tyler17 <tylerya@gmail.com>
2025-03-13 12:21:29 -0700

    use spock for poll tallies past a certain date

---
+++ modules/polling/api/spock/fetchVotesByAddressWithSpock.ts
@@ -0,0 +1,45 @@
+/*
+
+SPDX-FileCopyrightText: © 2023 Dai Foundation <www.daifoundation.org>
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+
+*/
+
+import BigNumber from 'lib/bigNumberJs';
+import { SupportedNetworks } from 'modules/web3/constants/networks';
+import { PollTallyVote } from '../../types';
+import { gqlRequest } from 'modules/gql/gqlRequest';
+import { voteAddressMkrWeightsAtTime } from 'modules/gql/queries/voteAddressMkrWeightsAtTime';
+import { networkNameToChainId } from 'modules/web3/helpers/chain';
+import { parseRawOptionId } from '../../helpers/parseRawOptionId';
+import { SpockVote } from '../../types/pollTally';
+
+export async function fetchVotesByAddressForPollWithSpock(
+  pollId: number,
+  endUnix: number,
+  network: SupportedNetworks
+): Promise<PollTallyVote[]> {
+  const data = await gqlRequest({
+    chainId: networkNameToChainId(network),
+    query: voteAddressMkrWeightsAtTime,
+    variables: { argPollId: pollId, argUnix: endUnix }
+  });
+
+  const results: SpockVote[] = data.voteAddressMkrWeightsAtTime.nodes;
+
+  if (!results) return [];
+
+  const votes = results.map(vote => {
+    const ballot = parseRawOptionId(vote.optionIdRaw.toString());
+
+    return {
+      ...vote,
+      optionIdRaw: vote.optionIdRaw.toString(),
+      ballot,
+      pollId
+    };
+  });
+
+  return votes.sort((a, b) => (new BigNumber(a.mkrSupport).lt(new BigNumber(b.mkrSupport)) ? 1 : -1));
+}
