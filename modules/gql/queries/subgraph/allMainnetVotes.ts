abe409120191fb63ad297388967c40576d7992bb
tyler17 <tylerya@gmail.com>
2025-05-06 13:42:54 -0700

    update queries to filter out votes before startDate, and fetch up to 1k at once

--- modules/gql/queries/subgraph/allMainnetVotes.ts
+++ modules/gql/queries/subgraph/allMainnetVotes.ts
@@ -9,8 +9,8 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 import { gql } from 'graphql-request';
 
 export const allMainnetVotes = gql`
-  query allMainnetVotes($argAddress: String!) {
-		pollVotes(where: {voter: $argAddress}) {
+  query allMainnetVotes($argAddress: String!, $startUnix: BigInt) {
+		pollVotes(where: {voter: $argAddress, blockTime_gt: $startUnix}, first: 1000) {
       poll {
         id
       }
