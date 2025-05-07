4cdd3327949214d192ca4f4973b4e984d2b06e01
Adam Goth <adamgoth@gmail.com>
2025-05-07 09:29:15 +0200

    Refactor fetchLandingPageData and fetchPollingPageData to use SKY_PORTAL_START_DATE_MAINNET as the default start date. Clean up formatting for better readability in both functions.

--- pages/polling/[poll-hash].tsx
+++ pages/polling/[poll-hash].tsx
@@ -45,7 +45,7 @@ import { ExternalLink } from 'modules/app/components/ExternalLink';
 import usePollsStore from 'modules/polling/stores/polls';
 import { DialogOverlay, DialogContent } from 'modules/app/components/Dialog';
 import BoxWithClose from 'modules/app/components/BoxWithClose';
-import { PollOrderByEnum } from 'modules/polling/polling.constants';
+import { PollOrderByEnum, SKY_PORTAL_START_DATE_MAINNET } from 'modules/polling/polling.constants';
 import { useNetwork } from 'modules/app/hooks/useNetwork';
 import { formatEther } from 'viem';
 
@@ -473,7 +473,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     tags: null,
     status: null,
     type: null,
-    startDate: null,
+    startDate: SKY_PORTAL_START_DATE_MAINNET,
     endDate: null
   });
 
