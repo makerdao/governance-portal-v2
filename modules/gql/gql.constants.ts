4d0c7074ea8128d24c212338f12ab846029a62af
tyler17 <tylerya@gmail.com>
2025-05-18 19:00:22 -0700

    fix tenderly address

--- modules/gql/gql.constants.ts
+++ modules/gql/gql.constants.ts
@@ -22,4 +22,4 @@ export const ARBITRUM_PROD_SUBGRAPH_URL =
   'https://query-subgraph.sky.money/subgraphs/name/jetstreamgg/subgraph-arbitrum';
 
 export const stakingEngineAddressMainnet = '0xce01c90de7fd1bcfa39e237fe6d8d9f569e8a6a3';
-export const stakingEngineAddressTestnet = '0x9581c795dbcaf408e477f6f1908a41be43093122';
+export const stakingEngineAddressTestnet = '0xb1fc11f03b084fff8dae95fa08e8d69ad2547ec1';
