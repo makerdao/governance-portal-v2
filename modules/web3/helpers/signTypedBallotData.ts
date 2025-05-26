5acdbca35e2260c9487b726dea63114155ecf7b8
Adam Goth <adamgoth@gmail.com>
2025-05-26 20:46:10 +0200

    Fix gasless domain name

--- modules/web3/helpers/signTypedBallotData.ts
+++ modules/web3/helpers/signTypedBallotData.ts
@@ -50,7 +50,7 @@ export function getTypedBallotData(message: BallotDataValues, network: Supported
     },
     primaryType: 'Vote' as const,
     domain: {
-      name: 'Sky Ecosystem Polling',
+      name: 'MakerDAO Polling',
       version: 'Arbitrum.1',
       chainId,
       verifyingContract: ArbitrumPollingAddressMap[network]
