f990fa2358472fdfbbeaf080b3498e17857342e4
Adam Goth <adamgoth@gmail.com>
2025-06-11 09:56:25 +0200

    Remove unused components and dependencies related to delegation and participation, including TopDelegates, Participation, ParticipationChart, and TabsNavigation. Update package.json and pnpm-lock.yaml to reflect the removal of react-sticky.

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,24 +24,18 @@ import { ErrorBoundary } from 'modules/app/components/ErrorBoundary';
 import Skeleton from 'react-loading-skeleton';
 import { SupportedNetworks } from 'modules/web3/constants/networks';
 import useSWR, { useSWRConfig } from 'swr';
-import TopDelegates from 'modules/delegates/components/TopDelegates';
 import { ResourcesLanding } from 'modules/home/components/ResourcesLanding/ResourcesLanding';
 import { PollsOverviewLanding } from 'modules/home/components/PollsOverviewLanding';
 import { InternalLink } from 'modules/app/components/InternalLink';
 import InformationParticipateMakerGovernance from 'modules/home/components/InformationParticipateMakerGovernance/InformationParticipateMakerGovernance';
-import { useBreakpointIndex } from '@theme-ui/match-media';
 import { useAccount } from 'modules/app/hooks/useAccount';
 import { VIDEO_URLS } from 'modules/app/client/videos.constants';
-import Participation from 'modules/home/components/Participation';
-import TabsNavigation from 'modules/home/components/TabsNavigation';
-import { StickyContainer, Sticky } from 'react-sticky';
 import { useInView } from 'react-intersection-observer';
 import { useVotedProposals } from 'modules/executive/hooks/useVotedProposals';
 import { fetchLandingPageData } from 'modules/home/api/fetchLandingPageData';
 import { LandingPageData } from 'modules/home/api/fetchLandingPageData';
 import { useLandingPageDelegates } from 'modules/gql/hooks/useLandingPageDelegates';
 import { useNetwork } from 'modules/app/hooks/useNetwork';
-import { parseUnits } from 'viem';
 
 const LandingPage = ({
   proposals,
@@ -55,7 +49,6 @@ const LandingPage = ({
   hat,
   mkrInChief
 }: LandingPageData) => {
-  const bpi = useBreakpointIndex();
   const [videoOpen, setVideoOpen] = useState(false);
   const [mode] = useColorMode();
   const [backgroundImage, setBackroundImage] = useState('url(/assets/bg_medium.jpeg)');
@@ -67,14 +60,6 @@ const LandingPage = ({
   // account
   const { account, votingAccount } = useAccount();
 
-  const activeDelegates = delegatesInfo
-    .sort((a, b) => {
-      const [first] = a.combinedParticipation?.toString().split('%') || '0';
-      const [second] = b.combinedParticipation?.toString().split('%') || '0';
-      return parseFloat(second) - parseFloat(first);
-    })
-    .slice(0, 5);
-
   // executives
   const { data: votedProposals, mutate: mutateVotedProposals } = useVotedProposals();
 
@@ -82,54 +67,6 @@ const LandingPage = ({
   useEffect(() => {
     mutateVotedProposals();
   }, [votingAccount]);
-
-  // Use intersection observers to change the hash on scroll
-  const [activeTab, setActiveTab] = useState('#vote');
-
-  const { ref: voteRef, inView: voteInview } = useInView({
-    /* Optional options */
-    threshold: 0.5
-  });
-
-  const { ref: learnRef, inView: learnInview } = useInView({
-    /* Optional options */
-    threshold: 0.5
-  });
-
-  const { ref: engageRef, inView: engageInview } = useInView({
-    /* Optional options */
-    threshold: 0.5
-  });
-
-  const { ref: delegateRef, inView: delegateInview } = useInView({
-    /* Optional options */
-    threshold: 0.5
-  });
-
-  useEffect(() => {
-    if (learnInview) {
-      setActiveTab('#learn');
-    } else if (voteInview) {
-      setActiveTab('#vote');
-    } else if (engageInview) {
-      setActiveTab('#engage');
-    } else if (delegateInview) {
-      setActiveTab('#delegate');
-    }
-  }, [learnInview, voteInview, engageInview, delegateInview]);
-
-  const hashChangeHandler = useCallback(() => {
-    setActiveTab(window.location.hash);
-  }, []);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('hashchange', hashChangeHandler);
-      return () => {
-        window.removeEventListener('hashchange', hashChangeHandler);
-      };
-    }
-  }, []);
 
   return (
     <div>
@@ -154,131 +91,100 @@ const LandingPage = ({
         }}
       />
       <VideoModal isOpen={videoOpen} onDismiss={() => setVideoOpen(false)} url={VIDEO_URLS.howToVote} />
-      <StickyContainer>
-        <PrimaryLayout sx={{ maxWidth: 'page' }}>
-          <Stack gap={[5, 6]} separationType="p">
-            <section>
-              <Flex sx={{ flexDirection: ['column', 'column', 'row'], justifyContent: 'space-between' }}>
-                <Flex sx={{ p: 3, width: ['100%', '100%', '50%'], flexDirection: 'column' }}>
-                  <Heading as="h1" sx={{ color: 'text', fontSize: [7, 8] }}>
-                    Maker Governance
-                  </Heading>
-                  <Heading as="h1" sx={{ color: 'text', fontSize: [7, 8] }}>
-                    Voting Portal
-                  </Heading>
-                  <Text as="p" sx={{ fontWeight: 'semiBold', my: 3, width: ['100%', '100%', '80%'] }}>
-                    Vote with or delegate your MKR tokens to help protect the integrity of the Maker protocol
-                  </Text>
-                  <Box>
-                    <PlayButton
-                      label="How to vote"
-                      onClick={() => setVideoOpen(true)}
-                      styles={{ mr: [1, 3] }}
-                    />
-                  </Box>
+      <PrimaryLayout sx={{ maxWidth: 'page' }}>
+        <Stack gap={[5, 6]} separationType="p">
+          <section>
+            <Flex sx={{ flexDirection: ['column', 'column', 'row'], justifyContent: 'space-between' }}>
+              <Flex sx={{ p: 3, width: ['100%', '100%', '50%'], flexDirection: 'column' }}>
+                <Heading as="h1" sx={{ color: 'text', fontSize: [7, 8] }}>
+                  Maker Governance
+                </Heading>
+                <Heading as="h1" sx={{ color: 'text', fontSize: [7, 8] }}>
+                  Voting Portal
+                </Heading>
+                <Text as="p" sx={{ fontWeight: 'semiBold', my: 3, width: ['100%', '100%', '80%'] }}>
+                  Vote with or delegate your MKR tokens to help protect the integrity of the Maker protocol
+                </Text>
+                <Box>
+                  <PlayButton
+                    label="How to vote"
+                    onClick={() => setVideoOpen(true)}
+                    styles={{ mr: [1, 3] }}
+                  />
+                </Box>
+              </Flex>
+              <Flex sx={{ py: 3, px: [1, 3], width: ['100%', '100%', '50%'], flexDirection: 'column' }}>
+                <Flex sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                  <Heading>Latest Executive</Heading>
+                  <InternalLink href={'/executive'} title="Latest Executive">
+                    <ViewMore />
+                  </InternalLink>
                 </Flex>
-                <Flex sx={{ py: 3, px: [1, 3], width: ['100%', '100%', '50%'], flexDirection: 'column' }}>
-                  <Flex sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-                    <Heading>Latest Executive</Heading>
-                    <InternalLink href={'/executive'} title="Latest Executive">
-                      <ViewMore />
-                    </InternalLink>
-                  </Flex>
-                  <Flex sx={{ mt: 3 }}>
-                    <ErrorBoundary componentName="Latest Executive">
-                      {proposals ? (
-                        proposals.length > 0 ? (
-                          <ExecutiveOverviewCard
-                            votedProposals={votedProposals}
-                            account={account}
-                            isHat={hat ? hat.toLowerCase() === proposals[0].address.toLowerCase() : false}
-                            proposal={proposals[0]}
-                          />
-                        ) : (
-                          <Text>No proposals found</Text>
-                        )
+                <Flex sx={{ mt: 3 }}>
+                  <ErrorBoundary componentName="Latest Executive">
+                    {proposals ? (
+                      proposals.length > 0 ? (
+                        <ExecutiveOverviewCard
+                          votedProposals={votedProposals}
+                          account={account}
+                          isHat={hat ? hat.toLowerCase() === proposals[0].address.toLowerCase() : false}
+                          proposal={proposals[0]}
+                        />
                       ) : (
-                        <Skeleton />
-                      )}
-                    </ErrorBoundary>
-                  </Flex>
+                        <Text>No proposals found</Text>
+                      )
+                    ) : (
+                      <Skeleton />
+                    )}
+                  </ErrorBoundary>
                 </Flex>
               </Flex>
-            </section>
-
-            <section>
-              <ErrorBoundary componentName="Governance Stats">
-                <GovernanceStats
-                  pollStats={pollStats}
-                  stats={stats}
-                  mkrOnHat={mkrOnHat}
-                  mkrInChief={mkrInChief}
-                />
-              </ErrorBoundary>
-            </section>
-
-            <section id="vote">
-              <Sticky topOffset={bpi < 1 ? 1050 : 700}>
-                {({ style, isSticky }) => (
-                  <Box
-                    style={{
-                      ...style,
-                      zIndex: 100,
-                      width: isSticky ? '100%' : 'auto',
-                      left: 0,
-                      top: 66
-                    }}
-                  >
-                    <TabsNavigation activeTab={activeTab} />
-                  </Box>
-                )}
-              </Sticky>
-              <Box ref={voteRef} />
-              <Box sx={{ mt: 3 }}>
-                <PollsOverviewLanding polls={polls} activePollCount={pollStats.active} allTags={pollTags} />
-              </Box>
-              <PollCategoriesLanding pollCategories={pollTags} />
-            </section>
-
-            <section id="delegate">
-              <Box ref={delegateRef} />
-              <TopDelegates
-                topDelegates={delegates}
-                totalMKRDelegated={parseUnits((stats?.totalMKRDelegated || 0).toString(), 18)}
-              />
-            </section>
-
-            <Box as={'section'} sx={{ position: 'relative', overflowY: 'clip' }} id="learn">
-              <Box
-                sx={{
-                  background: 'onSurfaceAlt',
-                  width: '200vw',
-                  zIndex: -1,
-                  ml: '-100vw',
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  height: '1720px'
-                }}
-              />
-              <Box ref={learnRef} />
-              <InformationParticipateMakerGovernance />
-              <ResourcesLanding />
-            </Box>
-
-            <section id="engage">
-              <Box ref={engageRef} />
-              <Participation activeDelegates={activeDelegates} bpi={bpi} />
-            </section>
-            <Flex
-              onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-              sx={{ justifyContent: 'flex-end', mb: 3 }}
-            >
-              <ViewMore label="Back to the top" icon="chevron_up" />
             </Flex>
-          </Stack>
-        </PrimaryLayout>
-      </StickyContainer>
+          </section>
+
+          <section>
+            <ErrorBoundary componentName="Governance Stats">
+              <GovernanceStats
+                pollStats={pollStats}
+                stats={stats}
+                mkrOnHat={mkrOnHat}
+                mkrInChief={mkrInChief}
+              />
+            </ErrorBoundary>
+          </section>
+
+          <section id="vote">
+            <Box sx={{ mt: 3 }}>
+              <PollsOverviewLanding polls={polls} activePollCount={pollStats.active} allTags={pollTags} />
+            </Box>
+            <PollCategoriesLanding pollCategories={pollTags} />
+          </section>
+
+          <Box as={'section'} sx={{ position: 'relative', overflowY: 'clip' }} id="learn">
+            <Box
+              sx={{
+                background: 'onSurfaceAlt',
+                width: '200vw',
+                zIndex: -1,
+                ml: '-100vw',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                height: '1720px'
+              }}
+            />
+            <InformationParticipateMakerGovernance />
+            <ResourcesLanding />
+          </Box>
+
+          <Flex
+            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+            sx={{ justifyContent: 'flex-end', mb: 3 }}
+          >
+            <ViewMore label="Back to the top" icon="chevron_up" />
+          </Flex>
+        </Stack>
+      </PrimaryLayout>
     </div>
   );
 };
