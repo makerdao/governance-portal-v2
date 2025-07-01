6981cff49c002f1a0ba993aa7424392529a31d84
Adam Goth <adamgoth@gmail.com>
2025-07-01 12:09:19 +0200

    Remove SkyPollsOverviewLanding component to streamline landing page structure

--- modules/polling/components/SkyPollWinningOptionBox.tsx
+++ modules/polling/components/SkyPollWinningOptionBox.tsx
@@ -68,7 +68,7 @@ export default function SkyPollWinningOptionBox({
   const leadingOption = typeof tally.winner === 'number' ? tally.winner : tally.results[0].optionId;
   const leadingOptionName = `${
     typeof tally.winner === 'number'
-      ? tally.winningOption
+      ? tally.winningOptionName
       : tally.results[0].optionName || `Option ${tally.results[0].optionId}`
     //don't show "& n more" if isDefault since there's only ever 1 default option
   }${!isDefault && numberOfLeadingOptions > 1 ? ` & ${numberOfLeadingOptions - 1} more` : ''}`;
